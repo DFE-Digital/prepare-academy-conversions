b7079f29b7f76d1701a6a7e211a26697c43f0716
nouriach <49981579+nouriach@users.noreply.github.com>
2021-03-29 19:05:33 +0100

    Add minimum fields for data models

--- ApplyToBecomeInternal/ApplyToBecome.Data/Models/School.cs
+++ ApplyToBecomeInternal/ApplyToBecome.Data/Models/School.cs
@@ -2,6 +2,11 @@ namespace ApplyToBecome.Data.Models
 {
 	public class School
 	{
+		public int Id { get; set; }
 		public string Name { get; set; }
+		public string URN { get; set; }
+		public string LocalAuthority { get; set; }
+		public int ProjectId { get; set; }
+		public Project Project { get; set; }
 	}
 }
--- ApplyToBecomeInternal/ApplyToBecome.Data/Models/Trust.cs
+++ ApplyToBecomeInternal/ApplyToBecome.Data/Models/Trust.cs
@@ -3,5 +3,7 @@ namespace ApplyToBecome.Data.Models
 	public class Trust
 	{
 		public string Name { get; set; }
+		public int ProjectId { get; set; }
+		public Project Project { get; set; }
 	}
 }
