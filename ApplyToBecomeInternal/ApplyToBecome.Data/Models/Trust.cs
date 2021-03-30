94f53ffeed97f50fa878a809fa720a4d1ea7aed7
nouriach <49981579+nouriach@users.noreply.github.com>
2021-03-30 09:04:40 +0100

    Add ID to Trust model

--- ApplyToBecomeInternal/ApplyToBecome.Data/Models/Trust.cs
+++ ApplyToBecomeInternal/ApplyToBecome.Data/Models/Trust.cs
@@ -2,6 +2,7 @@ namespace ApplyToBecome.Data.Models
 {
 	public class Trust
 	{
+		public int Id { get; set; }
 		public string Name { get; set; }
 		public int ProjectId { get; set; }
 		public Project Project { get; set; }
