a51b5184d73f574a00a91975572d1d660eaca978
nouriach <49981579+nouriach@users.noreply.github.com>
2021-03-30 12:12:51 +0100

    Remove unnecessary data points

--- ApplyToBecomeInternal/ApplyToBecome.Data/Models/School.cs
+++ ApplyToBecomeInternal/ApplyToBecome.Data/Models/School.cs
@@ -6,7 +6,5 @@ namespace ApplyToBecome.Data.Models
 		public string Name { get; set; }
 		public string URN { get; set; }
 		public string LocalAuthority { get; set; }
-		public int ProjectId { get; set; }
-		public Project Project { get; set; }
 	}
 }
--- ApplyToBecomeInternal/ApplyToBecome.Data/Models/Trust.cs
+++ ApplyToBecomeInternal/ApplyToBecome.Data/Models/Trust.cs
@@ -4,7 +4,5 @@ namespace ApplyToBecome.Data.Models
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public int ProjectId { get; set; }
-		public Project Project { get; set; }
 	}
 }
