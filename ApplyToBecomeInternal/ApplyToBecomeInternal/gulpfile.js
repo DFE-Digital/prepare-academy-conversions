357bf03aa892129a8514b4dfd3dc91c09cdc242d
Robert Marshall <robert.marshall@madetech.com>
2021-03-30 10:13:32 +0100

    Minify CSS and Gov JS

--- ApplyToBecomeInternal/ApplyToBecomeInternal/gulpfile.js
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/gulpfile.js
@@ -4,13 +4,16 @@ var gulp = require("gulp");
 var sass = require("gulp-dart-sass");
 var async = require("async");
 var rename = require("gulp-rename");
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
 
 const buildSass = () => gulp.src("wwwroot/css/*.scss")
 	.pipe(sass().on("error", sass.logError))
+	.pipe(cleanCSS())
 	.pipe(gulp.dest("wwwroot/css"));
 
 const copyGovukAssets = () => gulp.src(["node_modules/govuk-frontend/govuk/assets/**/*"]).pipe(gulp.dest("wwwroot/assets")).on("end", () =>
-	gulp.src(["node_modules/govuk-frontend/govuk/all.js"]).pipe(rename("govuk.js")).pipe(gulp.dest("wwwroot/js/")));
+	gulp.src(["node_modules/govuk-frontend/govuk/all.js"]).pipe(uglify()).pipe(rename("govuk.js")).pipe(gulp.dest("wwwroot/js/")));
 
 gulp.task("sass", buildSass);
 
@@ -23,6 +26,6 @@ gulp.task("copy-gov-fe-assets", copyGovukAssets);
 gulp.task("build-fe", () => {
 	return async.series([
 		(next) => buildSass().on("end", next),
-		(next) => copyGovukAssets().on("end", next)
+		(next) => copyGovukAssets().on("end", next),
 	])
 });
