0cc92f2a2e638442d54622fb9b0b039c607e8a86
Robert Marshall <robert.marshall@madetech.com>
2021-04-06 12:19:31 +0100

    Update mock project data to be referenced by index (for speeeed)

--- ApplyToBecomeInternal/ApplyToBecome.Data.Mock/MockProjects.cs
+++ ApplyToBecomeInternal/ApplyToBecome.Data.Mock/MockProjects.cs
@@ -1,105 +1,54 @@
 using ApplyToBecome.Data.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace ApplyToBecome.Data.Mock
 {
 	public class MockProjects:IProjects
 	{
-		public IEnumerable<Project> GetAllProjects()
-		{
-			return new[] {
-				new Project {
-					Id = 0,
-					School = new School 
-					{
-						Id = 100,
-						Name = "St. Wilfrid's Primary School",
-						URN = "AS_102062",
-						LocalAuthority = "Warrington"
-					},
-					Trust = new Trust
-					{
-						Id = 200,
-						Name = "Dynamics Academy Trust"
-					},
-					ApplicationReceivedDate = new DateTime(2021,3,1),
-					AssignedDate = new DateTime(2021,3,3)
-				},
-				new Project {
-					Id = 1,
-					School = new School
-					{
-						Id = 101,
-						Name = "Bolton Spa Primary",
-						URN = "AS_675091",
-						LocalAuthority = "Warrington"
-					},
-					Trust = new Trust
-					{
-						Id = 201,
-						Name = "Oak Hill Trust"
-					},
-					ApplicationReceivedDate = new DateTime(2021,3,1),
-					AssignedDate = new DateTime(2021,3,3)
-				},
-				new Project {
-					Id = 2,
-					School = new School
-					{
-						Id = 102,
-						Name = "Blessed John Henry Newman Roman Catholic College",
-						URN = "AS_123920",
-						LocalAuthority = "Warrington"
-					},
-					Trust = new Trust
-					{
-						Id = 202,
-						Name = "Kingfisher learning trust"
-					},
-					ApplicationReceivedDate = new DateTime(2021,3,1),
-					AssignedDate = new DateTime(2021,3,3)
-				},
-				new Project {
-					Id = 3,
-					School = new School
-					{
-						Id = 103,
-						Name = "Bolton Spa Primary",
-						URN = "AS_112456",
-						LocalAuthority = "Bolton"
-					},
-					Trust = new Trust
-					{
-						Id = 203,
-						Name = "Oak Hill Trust"
-					},
-					ApplicationReceivedDate = new DateTime(2021,3,1),
-					AssignedDate = new DateTime(2021,3,3)
-				},
-				new Project {
-					Id = 4,
-					School = new School
-					{
-						Id = 104,
-						Name = "Ancoats Free School",
-						URN = "AS_102045",
-						LocalAuthority = "Greater Manchester"
-					},
-					Trust = new Trust
-					{
-						Id = 204,
-						Name = "United Learning Trust"
-					}
-				},
-			};
-		}
+		private readonly Project[] _projects = {
+			new Project
+			{
+				Id = 0,
+				School = new School {Id = 100, Name = "St. Wilfrid's Primary School", URN = "AS_102062", LocalAuthority = "Warrington"},
+				Trust = new Trust {Id = 200, Name = "Dynamics Academy Trust"},
+				ApplicationReceivedDate = new DateTime(2021, 3, 1),
+				AssignedDate = new DateTime(2021, 3, 3)
+			},
+			new Project
+			{
+				Id = 1,
+				School = new School {Id = 101, Name = "Bolton Spa Primary", URN = "AS_675091", LocalAuthority = "Warrington"},
+				Trust = new Trust {Id = 201, Name = "Oak Hill Trust"},
+				ApplicationReceivedDate = new DateTime(2021, 3, 1),
+				AssignedDate = new DateTime(2021, 3, 3)
+			},
+			new Project
+			{
+				Id = 2,
+				School = new School {Id = 102, Name = "Blessed John Henry Newman Roman Catholic College", URN = "AS_123920", LocalAuthority = "Warrington"},
+				Trust = new Trust {Id = 202, Name = "Kingfisher learning trust"},
+				ApplicationReceivedDate = new DateTime(2021, 3, 1),
+				AssignedDate = new DateTime(2021, 3, 3)
+			},
+			new Project
+			{
+				Id = 3,
+				School = new School {Id = 103, Name = "Bolton Spa Primary", URN = "AS_112456", LocalAuthority = "Bolton"},
+				Trust = new Trust {Id = 203, Name = "Oak Hill Trust"},
+				ApplicationReceivedDate = new DateTime(2021, 3, 1),
+				AssignedDate = new DateTime(2021, 3, 3)
+			},
+			new Project
+			{
+				Id = 4,
+				School = new School {Id = 104, Name = "Ancoats Free School", URN = "AS_102045", LocalAuthority = "Greater Manchester"},
+				Trust = new Trust {Id = 204, Name = "United Learning Trust"}
+			},
+		};
+			
+		public IEnumerable<Project> GetAllProjects() => _projects;
 
-		public Project GetProjectById(int id)
-		{
-			var projects = GetAllProjects();
-			return projects.Where(x => x.Id == id).FirstOrDefault();
-		}
+		public Project GetProjectById(int id) => _projects[id] ?? throw new Exception();
 	}
 }
