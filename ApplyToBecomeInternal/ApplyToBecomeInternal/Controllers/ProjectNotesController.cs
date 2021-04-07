5f84b02cb3882e357b7950933f9fd84b163b8952
nouriach <49981579+nouriach@users.noreply.github.com>
2021-04-07 17:30:26 +0100

    Update Controllers to pass section string to VM

---
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Controllers/ProjectNotesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ApplyToBecome.Data;
+using ApplyToBecomeInternal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplyToBecomeInternal.Controllers
+{
+	[Route("/project-notes/")]
+	public class ProjectNotesController : Controller
+	{
+		private readonly IProjects _projects;
+
+		public ProjectNotesController(IProjects projects)
+		{
+			_projects = projects;
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult Index(int id)
+		{
+			var project = _projects.GetProjectById(id);
+			var viewModel = new ProjectViewModel(project, section: "ProjectNotes");
+
+			return View(viewModel);
+		}
+	}
+}
