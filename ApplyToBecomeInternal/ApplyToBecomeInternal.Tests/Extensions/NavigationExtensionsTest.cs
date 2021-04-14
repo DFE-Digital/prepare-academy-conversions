81b367abb562f6f604a8bc9a4db3ef7b7c9704ed
nouriach <49981579+nouriach@users.noreply.github.com>
2021-04-14 14:11:57 +0100

    Write tests for new NavigationViewModel

---
+++ ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/Extensions/NavigationExtensionsTest.cs
@@ -0,0 +1,23 @@
+﻿using ApplyToBecomeInternal.Extensions;
+using ApplyToBecomeInternal.Models.Shared;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ApplyToBecomeInternal.Tests.Extensions
+{
+	public class NavigationExtensionsTest
+	{
+
+		[Theory]
+		[InlineData(NavigationContent.ProjectsList, "Back to all conversion projects", "/projectlist")]
+		public void CustomNavigationAttribute_NavigationEnum_ReturnAttributeValues(NavigationContent navigation, string expectedCopy, string expectedUrl)
+		{
+			var actual = NavigationExtensions.GetAttribute<NavigationAttribute>(navigation);
+			actual.Content.Should().Be(expectedCopy);
+			actual.Url.Should().Be(expectedUrl);
+		}
+	}
+}
--- ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/Models/ApplicationFormViewModelTests.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/Models/ApplicationFormViewModelTests.cs
@@ -16,5 +16,17 @@ namespace ApplyToBecomeInternal.Tests.Models
 			var applicationFormViewModel = new ApplicationFormViewModel(projectViewModel);
 			applicationFormViewModel.SubMenu.Page.Should().Be(SubMenuPage.ApplicationForm);
 		}
+
+		[Fact]
+		public void Constructor_WithProjectViewModel_SetsNavigationViewModelToProjectsListContentAndUrl()
+		{
+			var project = new Project { School = new School(), Trust = new Trust() };
+			var projectViewModel = new ProjectViewModel(project);
+			var applicationFormViewModel = new ApplicationFormViewModel(projectViewModel);
+			var expectedContent = "Back to all conversion projects";
+			var expectedUrl = "/projectlist";
+			applicationFormViewModel.Navigation.Content.Should().Be(expectedContent);
+			applicationFormViewModel.Navigation.Url.Should().Be(expectedUrl);
+		}
 	}
 }
