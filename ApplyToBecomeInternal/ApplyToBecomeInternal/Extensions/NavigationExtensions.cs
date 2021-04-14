1534574a561a1cf8ca45bf6284d7b0364db31701
nouriach <49981579+nouriach@users.noreply.github.com>
2021-04-14 14:13:34 +0100

    Setup NavigationViewModel
    - Add Custom Attribute class
    - Create Enum to encapsulate potential values
    - Create Extension method to return custom attribute values
    for the enum

---
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Extensions/NavigationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ApplyToBecomeInternal.Extensions
+{
+	public static class NavigationExtensions
+	{
+		public static TAttribute GetAttribute<TAttribute>(this Enum value) where TAttribute : Attribute
+		{
+			return value.GetType().GetMember(value.ToString()).First().GetCustomAttribute<TAttribute>();
+		}
+	}
+}
---
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Models/Shared/NavigationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ApplyToBecomeInternal.Extensions;
+
+namespace ApplyToBecomeInternal.Models.Shared
+{
+	public class NavigationViewModel
+	{
+		public NavigationViewModel(NavigationContent content)
+		{
+			Content = NavigationExtensions.GetAttribute<NavigationAttribute>(content).Content;
+			Url = NavigationExtensions.GetAttribute<NavigationAttribute>(content).Url;
+		}
+
+		public string Content { get; }
+		public string Url { get; set; }
+	}
+}
