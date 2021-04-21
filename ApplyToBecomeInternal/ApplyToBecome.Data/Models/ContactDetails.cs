73a17f666b4e90f1b866ac80bb43e75a859a6366
nouriach <49981579+nouriach@users.noreply.github.com>
2021-04-21 13:42:49 +0100

    Create ApplicationConversion model & tests

---
+++ ApplyToBecomeInternal/ApplyToBecome.Data/Models/ContactDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplyToBecome.Data.Models
+{
+	public class ContactDetails
+	{
+		public string Name { get; set; }
+		public string EmailAddress { get; set; }
+		public string TelephoneNumber { get; set; }
+
+	}
+}
