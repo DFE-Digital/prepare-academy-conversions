cdcd72a9f8f7d7ad25b4cee4b6cd0e5887ade8a7
nouriach <49981579+nouriach@users.noreply.github.com>
2021-04-22 11:52:28 +0100

    Create LandAndBuildings model & pass test

---
+++ ApplyToBecomeInternal/ApplyToBecome.Data/Models/Application/LandAndBuildings.cs
@@ -0,0 +1,13 @@
+﻿namespace ApplyToBecome.Data.Models.Application
+{
+	public class LandAndBuildings
+	{
+		public string BuildingAndLandOwner { get; set; }
+		public bool HasCurrentPlannedBuildingWorks { get; set; }
+		public bool HasSharedFacilitiesOnSite { get; set; }
+		public bool HasSchoolGrants { get; set; }
+		public bool HasPrivateFinanceInitiativeScheme { get; set; }
+		public bool IsInPrioritySchoolBuildingProgramme { get; set; }
+		public bool IsInBuildingSchoolsForFutureProgramme { get; set; }
+	}
+}
