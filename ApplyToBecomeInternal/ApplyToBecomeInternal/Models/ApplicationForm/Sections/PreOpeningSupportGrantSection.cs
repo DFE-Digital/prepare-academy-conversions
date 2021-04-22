5a16d189883585b656e47b9ab2d3a965091660ed
nouriach <49981579+nouriach@users.noreply.github.com>
2021-04-22 12:11:48 +0100

    Add model and pass test for PreOpeningSupportGrant

--- ApplyToBecomeInternal/ApplyToBecomeInternal/Models/ApplicationForm/Sections/PreOpeningSupportGrantSection.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Models/ApplicationForm/Sections/PreOpeningSupportGrantSection.cs
@@ -1,11 +1,20 @@
 using ApplyToBecome.Data.Models.Application;
+using System.Collections.Generic;
 
 namespace ApplyToBecomeInternal.Models.ApplicationForm.Sections
 {
 	public class PreOpeningSupportGrantSection : BaseFormSection
 	{
-		public PreOpeningSupportGrantSection(Application application) : base("Pre opening support grant")
+		public PreOpeningSupportGrantSection(Application application) : base("Pre-opening support grant")
 		{
+			SubSections = new[]
+{
+				new FormSubSection("Details", GenerateDetailsFields(application))
+			};
 		}
+		private static IEnumerable<FormField> GenerateDetailsFields(Application application) =>
+			new[] {
+				new FormField("Do you want these funds paid to the school or the trust?", application.FundsPaidToSchoolOrTrust)
+			};
 	}
 }
