dbd0ff1f53e34a50022ce8d07f6df097309b1d48
Robert Marshall <robert.marshall@madetech.com>
2021-04-22 09:16:56 +0100

    Populate missing mock data

--- ApplyToBecomeInternal/ApplyToBecome.Data.Mock/MockApplications.cs
+++ ApplyToBecomeInternal/ApplyToBecome.Data.Mock/MockApplications.cs
@@ -1,5 +1,6 @@
 using ApplyToBecome.Data.Models;
 using ApplyToBecome.Data.Models.Application;
+using System;
 
 namespace ApplyToBecome.Data.Mock
 {
@@ -19,6 +20,48 @@ namespace ApplyToBecome.Data.Mock
 					EvidenceDocument = new Link("consent_dynamics.docx", "#"),
 					ChangesToGovernance = false,
 					ChangesAtLocalLevel = true
+				},
+				HeadTeacher = new ContactDetails
+				{
+					Name = "Garth Brown",
+					EmailAddress = "garth.brown@stwilfridsprimary.edu.uk",
+					TelephoneNumber = "09876 64547563"
+				},
+				GoverningBodyChair = new ContactDetails
+				{
+					Name = "Arna Siggurdottier",
+					EmailAddress = "arna.siggurdottier@dynamicstrust.co.uk",
+					TelephoneNumber = "0972 345 119"
+				},
+				Approver = new ContactDetails
+				{
+					Name = "Garth Brown",
+					EmailAddress = "garth.brown@stwilfridsprimary.edu.uk",
+				},
+				DateForConversion = new DateForConversion
+				{
+					HasPreferredDate = true,
+					PreferredDate = new DateTime(2021,04,20)
+				},
+				SchoolToTrustRationale = "This is a rationale",
+				WillSchoolChangeName = true,
+				FurtherInformation = new FurtherInformation
+				{
+					WhatWillSchoolBringToTrust = "the school will bring these things to the trust",
+					HasUnpublishedOfstedInspection = false,
+					HasSafeguardingInvestigations = false,
+					IsPartOfLocalAuthorityReorganisation = false,
+					IsPartOfLocalAuthorityClosurePlans = false,
+					IsLinkedToDiocese = true,
+					NameOfDiocese = "Diocese of Warrington",
+					DioceseLetterOfConsent = new Link("consent-from-diocese.docx", "#"),
+					IsPartOfFederation = false,
+					IsSupportedByFoundationTrustOrOtherBody = true,
+					HasSACREChristianWorshipExcemption = false,
+					MainFeederSchools = "n/a as we are a primary school",
+					SchoolConsent = new Link("consent.docx", "#"),
+					EqualitiesImpactAssessmentResult = "Considered unlikely",
+					AdditionalInformation = null
 				}
 			};
 		}
