0da97bb24841ca4699a711306a7cb002c9fc8697
nouriach <49981579+nouriach@users.noreply.github.com>
2021-04-27 15:04:24 +0100

    Create endpoint for New Note
    - Add new navigation enum
    - Create new ViewModel for page
    -

--- ApplyToBecomeInternal/ApplyToBecomeInternal/Models/Shared/NavigationViewModel.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Models/Shared/NavigationViewModel.cs
@@ -1,9 +1,17 @@
 ﻿using ApplyToBecomeInternal.Extensions;
+using System;
 
 namespace ApplyToBecomeInternal.Models.Shared
 {
 	public class NavigationViewModel
 	{
+		public NavigationViewModel(NavigationTarget target, string id)
+		{
+			var (content, url) = target.GetContent();
+			Content = content;
+			Url = BuildUrlWithId(url, id);
+		}
+
 		public NavigationViewModel(NavigationTarget target)
 		{
 			var (content, url) = target.GetContent();
@@ -13,5 +21,9 @@ namespace ApplyToBecomeInternal.Models.Shared
 
 		public string Content { get; }
 		public string Url { get; }
+		private string BuildUrlWithId(string url, string id)
+		{
+			return url.Replace("{id}", id);
+		}
 	}
 }
