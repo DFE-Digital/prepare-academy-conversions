0c914d400556d7de4851101de831b5623c238bb6
nouriach <49981579+nouriach@users.noreply.github.com>
2021-04-27 15:04:24 +0100

    Create endpoint for New Note
    - Add new navigation enum
    - Create new ViewModel for page
    -

--- ApplyToBecomeInternal/ApplyToBecomeInternal/Models/Shared/NavigationTarget.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Models/Shared/NavigationTarget.cs
@@ -3,6 +3,8 @@
 	public enum NavigationTarget
 	{
 		[Navigation(content: "Back to all conversion projects", url: "/projectlist")]
-		ProjectsList
+		ProjectsList,
+		[Navigation(content: "Back", url: "/project-notes/{id}")]
+		ProjectNotes
 	}
 }
