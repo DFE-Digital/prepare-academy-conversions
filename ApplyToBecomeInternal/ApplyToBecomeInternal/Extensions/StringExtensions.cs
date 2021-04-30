17c2cb190d8e6ed4e24019b7a25b2106c3cae02e
Robert Marshall <robert.marshall@madetech.com>
2021-04-30 12:37:09 +0100

    Move index aggregation to extension method.

---
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace ApplyToBecomeInternal.Extensions
+{
+	public static class StringExtensions
+	{
+		public static IEnumerable<int> AllIndicesOf(this string str, string value)
+		{
+			for (int index = 0;; index += value.Length)
+			{
+				index = str.IndexOf(value, index);
+				if (index == -1)
+					break;
+				yield return index;
+			}
+		}
+	}
+}
