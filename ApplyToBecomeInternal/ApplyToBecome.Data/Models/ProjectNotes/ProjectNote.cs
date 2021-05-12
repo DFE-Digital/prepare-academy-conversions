6eddfef5e78b4629b214f328fa7ee7272be43a9c
nouriach <49981579+nouriach@users.noreply.github.com>
2021-05-12 17:37:33 +0100

    Create ProjectNote model & add to Project

---
+++ ApplyToBecomeInternal/ApplyToBecome.Data/Models/ProjectNotes/ProjectNote.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace ApplyToBecome.Data.Models.ProjectNotes
+{
+	public class ProjectNote
+	{
+		public ProjectNote(string title, string body)
+		{
+			Title = title;
+			Body = body;
+			Author = "Test Name";
+			DateAndTime = FormatDateAndTime(DateTime.Now);
+		}
+
+		private string FormatDateAndTime(DateTime date)
+		{
+			return $"{date.Day} {date.ToString("Y")} at {date.ToString("t")}";
+		}
+
+		public string Title { get; set; }
+		public string Body { get; set; }
+		public string Author { get; set; }
+		public string DateAndTime { get; set; }
+
+	}
+}
