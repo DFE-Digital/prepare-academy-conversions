303024264b6bc6bd0eef1adade8ee57c5aff2549
nouriach <49981579+nouriach@users.noreply.github.com>
2021-05-17 13:35:24 +0100

    Update new/existing VMs and return in Controller

---
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/ViewModels/GenerateHTBTemplateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ApplyToBecomeInternal.Models.Navigation;
+
+namespace ApplyToBecomeInternal.ViewModels
+{
+	public class GenerateHTBTemplateViewModel
+	{
+		public GenerateHTBTemplateViewModel(ProjectViewModel project)
+		{
+			Project = project;
+			Navigation = new NavigationViewModel(NavigationTarget.PreviewHTBTemplate);
+		}
+		public ProjectViewModel Project { get; set; }
+		public NavigationViewModel Navigation { get; set; }
+	}
+}
