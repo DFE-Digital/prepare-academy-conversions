a8ef6f8972891e14b5696d82dae2fe10e40e2a47
nouriach <49981579+nouriach@users.noreply.github.com>
2021-05-17 13:34:09 +0100

    Update models to hold placeholder document info

---
+++ ApplyToBecomeInternal/ApplyToBecome.Data/Models/GenerateHTBTemplate/DocumentDetails.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ApplyToBecome.Data.Models.GenerateHTBTemplate
+{
+	public class DocumentDetails
+	{
+		public string Name { get; set; }
+		public string Type { get; set; }
+		public string Size { get; set; }
+
+	}
+}
