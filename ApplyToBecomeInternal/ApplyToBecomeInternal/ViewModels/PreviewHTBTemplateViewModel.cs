964690b78a8c7790dea73e8208fd9e610dc27049
nouriach <49981579+nouriach@users.noreply.github.com>
2021-05-18 15:55:36 +0100

    Remove unnecessary using statements

--- ApplyToBecomeInternal/ApplyToBecomeInternal/ViewModels/PreviewHTBTemplateViewModel.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/ViewModels/PreviewHTBTemplateViewModel.cs
@@ -1,8 +1,4 @@
 ﻿using ApplyToBecomeInternal.Models.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ApplyToBecomeInternal.ViewModels
 {
