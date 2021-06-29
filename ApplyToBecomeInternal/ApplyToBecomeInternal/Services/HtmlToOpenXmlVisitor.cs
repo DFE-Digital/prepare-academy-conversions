e526fdb5a596c8506e42ceb1e3c70f45154bd6cd
Vivian <vivian.roberts@madetech.com>
2021-06-29 17:46:35 +0100

    Fix lists not displaying in word doc

--- ApplyToBecomeInternal/ApplyToBecomeInternal/Services/HtmlToOpenXmlVisitor.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Services/HtmlToOpenXmlVisitor.cs
@@ -14,8 +14,9 @@ namespace ApplyToBecomeInternal.Services
 		private readonly MainDocumentPart _mainDocumentPart;
 		private readonly ParagraphProperties _paragraphProperties;
 		private readonly RunProperties _runProperties;
+		private readonly List<AbstractNum> _abstractNums = new List<AbstractNum>();
+		private readonly List<NumberingInstance> _numberingInstances = new List<NumberingInstance>();
 		private Stack<Run> _runs = new Stack<Run>();
-		private int _currentNumberingId = 0;
 		private static string[] _tags = new[] { "ol", "ul", "p" };
 
 		public HtmlToOpenXmlVisitor(MainDocumentPart mainDocumentPart, ParagraphProperties paragraphProperties, RunProperties runProperties)
@@ -37,6 +38,15 @@ namespace ApplyToBecomeInternal.Services
 			ReconstuctDocumentBody(document, groupedElements);
 
 			VisitCore(document.Body);
+
+			foreach (var abstractNum in _abstractNums)
+			{
+				_mainDocumentPart.NumberingDefinitionsPart.Numbering.Append(abstractNum);
+			}
+			foreach (var numberingInstance in _numberingInstances)
+			{
+				_mainDocumentPart.NumberingDefinitionsPart.Numbering.Append(numberingInstance);
+			}
 		}
 
 		private static List<List<INode>> GroupElements(IHtmlDocument document)
@@ -117,53 +127,25 @@ namespace ApplyToBecomeInternal.Services
 			}
 		}
 
-		public void VisitOrderedList(INode orderedList)
+		private void VisitOrderedList(INode orderedList)
 		{
-			_currentNumberingId++;
-			var numbering = _mainDocumentPart.NumberingDefinitionsPart.Numbering;
-			numbering.Append(
-				new AbstractNum(
-					new Level(
-						new StartNumberingValue { Val = 1 },
-						new NumberingFormat { Val = NumberFormatValues.Decimal }) 
-					{ LevelIndex = 0 })
-				{ AbstractNumberId = _currentNumberingId });
-			numbering.Append(
-				new NumberingInstance(
-					new AbstractNumId() { Val = _currentNumberingId }
-				)
-				{ NumberID = _currentNumberingId });
+			AddNumberingDefinition(NumberFormatValues.Decimal, "%1.");
 			VisitCore(orderedList);
 		}
 
-		public void VisitUnorderedList(INode unorderedList)
+		private void VisitUnorderedList(INode unorderedList)
 		{
-			_currentNumberingId++;
-			var numbering = _mainDocumentPart.NumberingDefinitionsPart.Numbering;
-			numbering.Append(
-				new AbstractNum(
-					new Level(
-						new NumberingFormat() { Val = NumberFormatValues.Bullet },
-						new LevelText() { Val = "•" })
-					{ LevelIndex = 0 })
-				{ AbstractNumberId = _currentNumberingId });
-			numbering.Append(
-				new NumberingInstance(
-					new AbstractNumId() { Val = _currentNumberingId }
-				)
-				{ NumberID = _currentNumberingId });
-
+			AddNumberingDefinition(NumberFormatValues.Bullet, "•");
 			VisitCore(unorderedList);
 		}
 
-		public void VisitListItem(INode listItem)
+		private void VisitListItem(INode listItem)
 		{
-			_runs.Push(new Run(_runProperties.CloneNode(true)));
 			var properties = _paragraphProperties.CloneNode(true);
 			properties.AppendChild(new NumberingProperties(
 				new NumberingLevelReference() { Val = 0 },
-				new NumberingId() { Val = _currentNumberingId }));
-			OpenXmlElements.Add(new Paragraph(properties, _runs.Peek()));
+				new NumberingId() { Val = _abstractNums.Count }));
+			OpenXmlElements.Add(new Paragraph(properties));
 			VisitCore(listItem);
 		}
 
@@ -173,72 +155,78 @@ namespace ApplyToBecomeInternal.Services
 			VisitCore(paragraph);
 		}
 
-		public void VisitBold(INode bold, bool hasRun)
+		private void VisitBold(INode bold, bool hasRun)
 		{
-			if (!hasRun)
+			UseRun((run) =>
 			{
-				AddRun();
-			}
-			_runs.Peek().RunProperties.Bold = new Bold();
-			VisitCore(bold, true);
-			if (!hasRun)
-			{
-				_runs.Pop();
-			}
+				run.RunProperties.Bold = new Bold();
+				VisitCore(bold, true);
+			}, hasRun);
 		}
 
-		public void VisitItalic(INode italic, bool hasRun)
+		private void VisitItalic(INode italic, bool hasRun)
 		{
-			if (!hasRun)
+			UseRun((run) =>
 			{
-				AddRun();
-			}
-			_runs.Peek().RunProperties.Italic = new Italic();
-			VisitCore(italic, true);
-			if (!hasRun)
-			{
-				_runs.Pop();
-			}
+				run.RunProperties.Italic = new Italic();
+				VisitCore(italic, true);
+			}, hasRun);
 		}
 
-		public void VisitUnderline(INode underline, bool hasRun)
+		private void VisitUnderline(INode underline, bool hasRun)
 		{
-			if (!hasRun)
+			UseRun((run) =>
 			{
-				AddRun();
-			}
-			_runs.Peek().RunProperties.Underline = new Underline
-			{
-				Val = UnderlineValues.Single
-			};
-			VisitCore(underline, true);
-			if (!hasRun)
-			{
-				_runs.Pop();
-			}
+				run.RunProperties.Underline = new Underline
+				{
+					Val = UnderlineValues.Single
+				};
+				VisitCore(underline, true);
+			}, hasRun);
 		}
 
 		private void VisitText(IText text, bool hasRun)
 		{
-			if (!hasRun)
+			UseRun((run) =>
 			{
-				AddRun();
-			}
-			_runs.Peek().Append(new Text
+				run.Append(new Text
 				{
 					Text = text.TextContent,
 					Space = SpaceProcessingModeValues.Preserve
 				});
+			}, hasRun);
+		}
+
+		private void AddNumberingDefinition(NumberFormatValues numberFormatValues, string levelText)
+		{
+			_abstractNums.Add(
+				new AbstractNum(
+					new Level(
+						new StartNumberingValue { Val = 1 },
+						new NumberingFormat() { Val = numberFormatValues },
+						new LevelJustification { Val = LevelJustificationValues.Left },
+						new LevelText() { Val = levelText })
+					{ LevelIndex = 0 })
+				{ AbstractNumberId = _abstractNums.Count + 1 });
+			_numberingInstances.Add(
+				new NumberingInstance(
+					new AbstractNumId() { Val = _numberingInstances.Count + 1 }
+				)
+				{ NumberID = _numberingInstances.Count + 1 });
+		}
+
+		private void UseRun(Action<Run> action, bool hasRun)
+		{
+			if (!hasRun)
+			{
+				_runs.Push(new Run(_runProperties.CloneNode(true)));
+				OpenXmlElements.Last().Append(_runs.Peek());
+			}
+			action(_runs.Peek());
 			if (!hasRun)
 			{
 				_runs.Pop();
 			}
-		}
-
-		private void AddRun()
-		{
-			_runs.Push(new Run(_runProperties.CloneNode(true)));
-			OpenXmlElements.Last().Append(_runs.Peek());
 		}
 	}
 }
