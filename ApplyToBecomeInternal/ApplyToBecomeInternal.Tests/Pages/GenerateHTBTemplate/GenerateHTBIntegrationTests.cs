5ac8107ed10d332fead1968a9eafee4607503697
rf-mt <rebecca@madetech.com>
2021-07-14 09:27:38 +0100

    Add integration tests for generate htb template button

---
+++ ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/Pages/GenerateHTBTemplate/GenerateHTBIntegrationTests.cs
@@ -0,0 +1,56 @@
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using FluentAssertions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ApplyToBecomeInternal.Tests.Pages.GenerateHTBTemplate
+{
+	public class GenerateHTBIntegrationTests : BaseIntegrationTests
+	{
+		public GenerateHTBIntegrationTests(IntegrationTestingWebApplicationFactory factory) : base(factory) { }
+
+		[Fact]
+		public async Task Should_navigate_between_task_list_and_generate_htb_template()
+		{
+			var project = AddGetProject();
+
+			await OpenUrlAsync($"/task-list/{project.Id}");
+
+			await Document.QuerySelector<IHtmlFormElement>("form").SubmitAsync();
+			Document.Url.Should().BeUrl($"/task-list/{project.Id}/generate-headteacher-board-template");
+
+			await NavigateAsync("Back to task list");
+			Document.Url.Should().BeUrl($"/task-list/{project.Id}");
+		}
+
+		[Fact]
+		public async Task Should_navigate_between_preview_htb_template_and_generate_htb_template()
+		{
+			var project = AddGetProject();
+
+			await OpenUrlAsync($"/task-list/{project.Id}/preview-headteacher-board-template");
+
+			await NavigateAsync("Generate HTB document");
+			Document.Url.Should().BeUrl($"/task-list/{project.Id}/generate-headteacher-board-template");
+
+			await NavigateAsync("Back to preview");
+			Document.Url.Should().BeUrl($"/task-list/{project.Id}/preview-headteacher-board-template");
+		}
+
+		[Fact]
+		public async Task Should_display_error_summary_on_task_list_when_generate_button_clicked_if_no_htb_date_set()
+		{
+			var project = AddGetProject(p => p.HeadTeacherBoardDate = null);
+
+			await OpenUrlAsync($"/task-list/{project.Id}");
+
+			await Document.QuerySelector<IHtmlFormElement>("form").SubmitAsync();
+
+			Document.Url.Should().BeUrl($"/task-list/{project.Id}");
+
+			Document.QuerySelector(".govuk-error-summary").Should().NotBeNull();
+			Document.QuerySelector(".govuk-error-summary").TextContent.Should().Contain("Set an HTB date");
+		}
+	}
+}
