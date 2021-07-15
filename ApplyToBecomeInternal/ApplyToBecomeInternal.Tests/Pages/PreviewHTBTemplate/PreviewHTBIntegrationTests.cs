f3e4f10acc4da1b07f372ce76f6735da29064d1b
rf-mt <rebecca@madetech.com>
2021-07-15 15:49:10 +0100

    Display error if generate htb doc button clicked without htb date set

---
+++ ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/Pages/PreviewHTBTemplate/PreviewHTBIntegrationTests.cs
@@ -0,0 +1,44 @@
+using FluentAssertions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ApplyToBecomeInternal.Tests.Pages.PreviewHTBTemplate
+{
+	public class PreviewHTBIntegrationTests : BaseIntegrationTests
+	{
+		public PreviewHTBIntegrationTests(IntegrationTestingWebApplicationFactory factory) : base(factory) { }
+
+		[Fact]
+		public async Task Should_navigate_between_task_list_and_preview_htb_template()
+		{
+			var project = AddGetProject();
+
+			await OpenUrlAsync($"/task-list/{project.Id}");
+
+			await NavigateAsync("Preview HTB template");
+			Document.Url.Should().BeUrl($"/task-list/{project.Id}/preview-headteacher-board-template");
+
+			await NavigateAsync("Back to task list");
+			Document.Url.Should().BeUrl($"/task-list/{project.Id}");
+		}
+
+		[Fact]
+		public async Task Should_display_error_summary_on_preview_htb_template_when_generate_button_clicked_if_no_htb_date_set()
+		{
+			var project = AddGetProject(p => p.HeadTeacherBoardDate = null);
+
+			await OpenUrlAsync($"/task-list/{project.Id}/preview-headteacher-board-template");
+
+			await NavigateAsync("Generate HTB document");
+
+			Document.Url.Should().BeUrl($"/task-list/{project.Id}");
+
+			Document.QuerySelector(".govuk-error-summary").Should().NotBeNull();
+			Document.QuerySelector(".govuk-error-summary").TextContent.Should().Contain("Set an HTB date");
+
+			await NavigateAsync("Set an HTB date before you generate your document");
+
+			Document.Url.Should().BeUrl($"/task-list/{project.Id}/confirm-school-trust-information-project-dates#head-teacher-board-date");
+		}
+	}
+}
