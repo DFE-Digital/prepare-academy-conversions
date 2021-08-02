081ed3da939492402cfa7174250dd069da397d2d
rebecca <rebecca@madetech.com>
2021-08-02 14:05:13 +0100

    Update test name to reflect opening date display logic tweak

--- ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/Pages/ProjectList/ProjectListIntegrationTests.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/Pages/ProjectList/ProjectListIntegrationTests.cs
@@ -57,7 +57,7 @@ namespace ApplyToBecomeInternal.Tests.Pages.ProjectList
 		}
 
 		[Fact]
-		public async Task Should_display_assigned_to_me_date_when_no_opening_date_set()
+		public async Task Should_display_assigned_to_me_date_when_no_htb_date_set()
 		{
 			var projects = AddGetProjects(p => p.HeadTeacherBoardDate = null);
 
