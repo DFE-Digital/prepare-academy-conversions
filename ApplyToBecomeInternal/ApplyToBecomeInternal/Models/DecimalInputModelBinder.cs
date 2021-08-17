8eb84a33c854ee7ea98d111660265a632eeed14b
rebecca <rebecca@madetech.com>
2021-08-17 09:17:27 +0100

    Add custom validation error message for monetary inputs

--- ApplyToBecomeInternal/ApplyToBecomeInternal/Models/DecimalInputModelBinder.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Models/DecimalInputModelBinder.cs
@@ -37,7 +37,15 @@ namespace ApplyToBecomeInternal.Models
 				return Task.CompletedTask;
 			}
 
-			return (new DecimalModelBinder(NumberStyles.Any, _loggerFactory)).BindModelAsync(bindingContext);
+			(new DecimalModelBinder(NumberStyles.Any, _loggerFactory)).BindModelAsync(bindingContext);
+
+			if (bindingContext.ModelState.TryGetValue(bindingContext.ModelName, out var entry) && entry.Errors.Count > 0)
+			{
+				var displayName = bindingContext.ModelMetadata.DisplayName ?? bindingContext.ModelName;
+				entry.Errors.Add($"{displayName} must be written in the correct format, like 5,000.00");
+			}
+
+			return Task.CompletedTask;
 		}
 	}
 }
