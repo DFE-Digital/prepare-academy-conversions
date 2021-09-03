c7d5ca56252c2af051c426c004b58c602024af09
Rebecca <40759410+rf-mt@users.noreply.github.com>
2021-09-03 16:45:08 +0100

    Revert "KS content tweaks"

--- ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/Pages/TaskList/KeyStagePerformance/KeyStage2PerformanceIntegrationTests.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/Pages/TaskList/KeyStagePerformance/KeyStage2PerformanceIntegrationTests.cs
@@ -47,10 +47,10 @@ namespace ApplyToBecomeInternal.Tests.Pages.KeyStagePerformance
 
 				Document.QuerySelector($"#na-percentage-meeting-expected-in-rwm-{i}").TextContent.Trim().Should()
 					.Be(
-						$"{response.NationalAveragePercentageMeetingExpectedStdInRWM.NotDisadvantaged}<br>(disadvantaged pupils {response.NationalAveragePercentageMeetingExpectedStdInRWM.Disadvantaged})");
+						$"{response.NationalAveragePercentageMeetingExpectedStdInRWM.NotDisadvantaged}\n(disadvantaged pupils {response.NationalAveragePercentageMeetingExpectedStdInRWM.Disadvantaged})");
 				Document.QuerySelector($"#na-percentage-achieving-higher-in-rwm-{i}").TextContent.Should()
 					.Be(
-						$"{response.NationalAveragePercentageAchievingHigherStdInRWM.NotDisadvantaged}<br>(disadvantaged pupils {response.NationalAveragePercentageAchievingHigherStdInRWM.Disadvantaged})");
+						$"{response.NationalAveragePercentageAchievingHigherStdInRWM.NotDisadvantaged}\n(disadvantaged pupils {response.NationalAveragePercentageAchievingHigherStdInRWM.Disadvantaged})");
 				Document.QuerySelector($"#na-reading-progress-score-{i}").TextContent.Should().Be(response.NationalAverageReadingProgressScore.NotDisadvantaged);
 				Document.QuerySelector($"#na-writing-progress-score-{i}").TextContent.Should().Be(response.NationalAverageWritingProgressScore.NotDisadvantaged);
 				Document.QuerySelector($"#na-maths-progress-score-{i}").TextContent.Should().Be(response.NationalAverageMathsProgressScore.NotDisadvantaged);
@@ -95,7 +95,7 @@ namespace ApplyToBecomeInternal.Tests.Pages.KeyStagePerformance
 			Document.QuerySelector("#percentage-meeting-expected-in-rwm-0").TextContent.Should().Be("no data");
 
 			Document.QuerySelector($"#na-percentage-meeting-expected-in-rwm-0").TextContent.Trim().Should()
-				.Be($"{ks2ResponseOrderedByYear.First().NationalAveragePercentageMeetingExpectedStdInRWM.NotDisadvantaged}<br>(disadvantaged pupils no data)");
+				.Be($"{ks2ResponseOrderedByYear.First().NationalAveragePercentageMeetingExpectedStdInRWM.NotDisadvantaged}\n(disadvantaged pupils no data)");
 
 			await NavigateAsync("Confirm and continue");
 			Document.Url.Should().BeUrl($"/task-list/{project.Id}");
