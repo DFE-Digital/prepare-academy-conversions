941266f65e4425e881b12306e09407d989f6b567
Daniel Burnley <dan@danops.uk>
2021-09-10 14:16:14 +0100

    Set content to HTML strings

--- ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/Pages/TaskList/PreviewHTBTemplate/PreviewHTBIntegrationTests.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/Pages/TaskList/PreviewHTBTemplate/PreviewHTBIntegrationTests.cs
@@ -268,10 +268,10 @@ namespace ApplyToBecomeInternal.Tests.Pages.PreviewHTBTemplate
 
 				Document.QuerySelector($"#na-percentage-meeting-expected-in-rwm-{i}").TextContent.Trim().Should()
 					.Be(
-						$"{response.NationalAveragePercentageMeetingExpectedStdInRWM.NotDisadvantaged}<br>(disadvantaged pupils {response.NationalAveragePercentageMeetingExpectedStdInRWM.Disadvantaged})");
+						$"{response.NationalAveragePercentageMeetingExpectedStdInRWM.NotDisadvantaged}(disadvantaged pupils {response.NationalAveragePercentageMeetingExpectedStdInRWM.Disadvantaged})");
 				Document.QuerySelector($"#na-percentage-achieving-higher-in-rwm-{i}").TextContent.Should()
 					.Be(
-						$"{response.NationalAveragePercentageAchievingHigherStdInRWM.NotDisadvantaged}<br>(disadvantaged pupils {response.NationalAveragePercentageAchievingHigherStdInRWM.Disadvantaged})");
+						$"{response.NationalAveragePercentageAchievingHigherStdInRWM.NotDisadvantaged}(disadvantaged pupils {response.NationalAveragePercentageAchievingHigherStdInRWM.Disadvantaged})");
 				Document.QuerySelector($"#na-reading-progress-score-{i}").TextContent.Should().Be(response.NationalAverageReadingProgressScore.NotDisadvantaged);
 				Document.QuerySelector($"#na-writing-progress-score-{i}").TextContent.Should().Be(response.NationalAverageWritingProgressScore.NotDisadvantaged);
 				Document.QuerySelector($"#na-maths-progress-score-{i}").TextContent.Should().Be(response.NationalAverageMathsProgressScore.NotDisadvantaged);
