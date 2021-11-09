41024b2e7a035f25eefcf87e745ff6ccdf204884
Daniel Burnley <dan@danops.uk>
2021-11-09 12:55:25 +0000

    Check against existing headteacher board date

--- ApplyToBecomeInternal/ApplyToBecomeInternal/Pages/UpdateAcademyConversionProjectPageModel.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Pages/UpdateAcademyConversionProjectPageModel.cs
@@ -36,14 +36,10 @@ namespace ApplyToBecomeInternal.Pages
 
 		public virtual async Task<IActionResult> OnPostAsync(int id)
 		{
-			//if (AcademyConversionProject.HeadTeacherBoardDate.HasValue == false)
-			//{
-			//	_errorService.AddError($"/task-list/{id}/confirm-school-trust-information-project-dates/head-teacher-board-date?return=%2FTaskList%2FSchoolAndTrustInformation/ConfirmSchoolAndTrustInformation&fragment=head-teacher-board-date",
-			//		"Set an Advisory Board date before you generate your project template");
-			//}
-
+			await SetProject(id);
+			
 			bool schoolAndTrustInformationSectionComplete = AcademyConversionProject.SchoolAndTrustInformationSectionComplete != null && AcademyConversionProject.SchoolAndTrustInformationSectionComplete.Value;
-			if (schoolAndTrustInformationSectionComplete && !AcademyConversionProject.HeadTeacherBoardDate.HasValue)
+			if (schoolAndTrustInformationSectionComplete && !Project.HeadTeacherBoardDate.HasValue)
 			{
 				_errorService.AddError($"/task-list/{id}/confirm-school-trust-information-project-dates/head-teacher-board-date?return=%2FTaskList%2FSchoolAndTrustInformation/ConfirmSchoolAndTrustInformation&fragment=head-teacher-board-date",
 					"Set an Advisory Board date before you generate your project template");
@@ -52,7 +48,6 @@ namespace ApplyToBecomeInternal.Pages
 			_errorService.AddErrors(Request.Form.Keys, ModelState);
 			if (_errorService.HasErrors())
 			{
-				await SetProject(id);
 				return Page();
 			}
 
@@ -61,7 +56,6 @@ namespace ApplyToBecomeInternal.Pages
 			if (!response.Success)
 			{
 				_errorService.AddTramsError();
-				await SetProject(id);
 				return Page();
 			}
 
