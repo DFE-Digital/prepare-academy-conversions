bc9dd73643bbbee8c05ec133b691f4ad9ff64107
DALLY <Michael.DALLY@EDUCATION.GOV.UK>
2021-11-12 23:23:32 +0000

    pushing initial cypress interactions

---
+++ cypress/integration/route-tests/routes-test.js
@@ -0,0 +1,14 @@
+
+describe('Task list should be displayed', () => {
+  before(function(){
+      // runs before all tests inside descript
+  })
+    it('Should login successfully',()=>{
+      cy.login('academy', 'conversions');
+    })
+
+//     it('Should just work',()=>{
+// })
+
+
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,34 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.clearCookies()
+	cy.clearLocalStorage()
+	cy.visit('https://apply-to-become-an-academy-internal-dev.london.cloudapps.digital/login')
+	cy.get('#username').type(username)
+	cy.get('#password').type(password)
+	cy.get('.govuk-grid-column-one-half .govuk-button--primary').click()
+});
