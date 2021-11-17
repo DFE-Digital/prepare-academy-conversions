b1c654a4e812607b967ddfc40c68197c05190138
Daniel Burnley <dan@danops.uk>
2021-11-17 11:07:40 +0000

    Add secure headers

--- ApplyToBecomeInternal/ApplyToBecomeInternal/Startup.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Startup.cs
@@ -1,5 +1,6 @@
 using ApplyToBecome.Data.Services;
 using ApplyToBecomeInternal.Configuration;
+using ApplyToBecomeInternal.Security;
 using ApplyToBecomeInternal.Services;
 using ApplyToBecomeInternal.Services.WordDocument;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -100,6 +101,9 @@ namespace ApplyToBecomeInternal
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
+
+			app.UseSecurityHeaders(
+				SecureHeadersDefinitions.SecurityHeadersDefinitions.GetHeaderPolicyCollection(env.IsDevelopment()));
 
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
