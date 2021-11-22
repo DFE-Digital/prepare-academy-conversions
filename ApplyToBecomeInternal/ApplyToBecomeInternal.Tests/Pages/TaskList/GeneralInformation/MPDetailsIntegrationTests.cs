56f3664da1539ce1ab546b9197b25e694ec6bddc
Catherine LAWLOR <Cath.LAWLOR@gmail.com>
2021-11-22 12:05:27 +0000

    reminder to add tests

---
+++ ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/Pages/TaskList/GeneralInformation/MPDetailsIntegrationTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ApplyToBecomeInternal.Tests.Pages.TaskList.GeneralInformation
+{
+	public class MPDetailsIntegrationTests
+	{
+		[Fact]
+		public void NotStarted()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
