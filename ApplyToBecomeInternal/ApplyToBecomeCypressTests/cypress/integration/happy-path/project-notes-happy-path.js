71094c5a898d19ef57d7b2df1b0505e8c5b00928
Mike Dally <michael.dally@education.gov.uk>
2021-11-22 11:38:12 +0000

    added project notes happy path test

---
+++ ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/happy-path/project-notes-happy-path.js
@@ -0,0 +1,26 @@
+describe('Submit and view project notes', () => {
+
+    before(function () {
+        cy.login();
+    });
+    
+    after(function () {
+        cy.clearLocalStorage();
+    });
+    
+    it('Should display the project notes input', () => {
+        
+    });
+
+    it('Should correctly render the school application form', () => {
+        
+    });
+
+    it('Should all the user to add a note', () => {
+        
+    });
+
+    it('Should allow the user to navigate back to the task list with updates displayed', () => {
+        
+    });
+})
