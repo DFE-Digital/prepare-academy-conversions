3fbab9d2c120a174c668b0855c9b8b5c917e1f61
Mike Dally <michael.dally@education.gov.uk>
2021-11-23 16:19:27 +0000

    pushed completed test to add project note

--- ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/happy-path/project-notes-happy-path.js
+++ ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/happy-path/project-notes-happy-path.js
@@ -23,19 +23,8 @@ describe('Submit and view project notes', () => {
         cy.get('#project-note-subject').type('New subject added at: ' + dateText)
         cy.get('#project-note-body').type('New body added at: ' + dateText)
         cy.get('[type=submit]').click()
-        cy.pause()
         cy.get('#project-note-added').should('contain.text', 'Note added')
         cy.get('#project-note-subject-0').should('have.text', 'New subject added at: '+dateText)
         cy.get('#project-note-body-0').should('have.text', 'New body added at: '+dateText)
     });
-
-    it('Should correctly render the school application form', () => {
-        
-    });
-
-    it('Should allow the user to navigate back to the task list with updates displayed', () => {
-        
-    });
-
-    it
 })
