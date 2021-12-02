ce94fd5a98092a3a1546e9b553ed9e267ee4fab8
Mike Dally <michael.dally@education.gov.uk>
2021-12-02 15:34:42 +0000

    pushing fixed-up test

--- ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/url-routing/advisory-board-urls.js
+++ ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/url-routing/advisory-board-urls.js
@@ -1,25 +1,28 @@
 describe('validate advisory board urls', () => {
+    let selectedSchool = '';
+
     afterEach(() => {
 		cy.storeSessionData();
-        //cy.visit(Cypress.env('url')+ '/task-list/4')
 	});
 
-    before(function () {
+    beforeEach(function () {
         cy.login();
         cy.selectSchoolListing(2)
+        cy.url().then(url =>{
+            selectedSchool = url
+        })
      });
     
    after(function () {
         cy.clearLocalStorage();
     });  
 
+
     it('should correctly render a page when user visits /confirm-school-trust-information-project-dates/advisory-board-date', () => {
-        cy.url().then(url =>{
             //Changes the current URL
-            let modifiedUrl = url + "/confirm-school-trust-information-project-dates/advisory-board-date"
-            cy.visit(modifiedUrl)
+            cy.visit(selectedSchool + "/confirm-school-trust-information-project-dates/advisory-board-date"            )
             cy.get('h1').contains('Set the Advisory Board date')
-        });
+        
     });
 
     /* it('should correctly render a page when user visits /task-list', () => {
@@ -31,15 +34,8 @@ describe('validate advisory board urls', () => {
         }); 
     }); */
     it('should correctly render a page when user visits /confirm-school-trust-information-project-dates/previous-advisory-board-date', () => {
-        cy.url().then(url =>{
             //Changes the current URL
-            let modifiedUrl = url.replace(url,"/confirm-school-trust-information-project-date/previous-advisory-board-date")
-            cy.visit(modifiedUrl)
+            cy.visit(selectedSchool+"/confirm-school-trust-information-project-date/previous-advisory-board-date")
             cy.get('h1').contains('Set the Advisory Board date')
-        });
-    });
-
-    it('', () => {
-        
     });
 });
