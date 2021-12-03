b5429b5cc4ede11c88ea42dc53aaf1e64740b874
Aisha JILANI <Aisha.JILANI@EDUCATION.GOV.UK>
2021-12-03 16:58:52 +0000

    Cypress tests: all 5 passed

--- ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/url-routing/advisory-board-urls.js
+++ ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/url-routing/advisory-board-urls.js
@@ -29,16 +29,22 @@ describe('validate advisory board urls', () => {
             cy.get('h1').contains('Date of previous Advisory Board')
     });
 
-    it('should correctly render a page when user visits /confirm-school-trust-information-project-dates/previous-advisory', () => {
+    it('should correctly render a page when user visits /confirm-school-trust-information-project-dates/previous-advisory-board', () => {
         //Changes the current URL
-        cy.visit(selectedSchool+"/confirm-school-trust-information-project-dates/previous-advisory")
+        cy.visit(selectedSchool+"/confirm-school-trust-information-project-dates/previous-advisory-board")
         cy.get('h1').contains('Has this project been to a previous Advisory Board?')
     });
 
-    it('should correctly render a page when user visits /confirm-school-trust-information-project-dates/previous-advisory-board-date', () => {
+    it('should correctly render a page when user visits /preview-project-template', () => {
         //Changes the current URL
-        cy.visit(selectedSchool+"/confirm-school-trust-information-project-dates/previous-advisory-board-date")
-        cy.get('h1').contains('Date of previous Advisory Board')
+        cy.visit(selectedSchool+"/preview-project-template")
+        cy.get('h1').contains('Preview project template')
+    });
+
+    it('should correctly render a page when user visits /download-project-template', () => {
+        //Changes the current URL
+        cy.visit(selectedSchool+"/download-project-template")
+        cy.get('h1').contains('Download project template')
     });
 
 });
