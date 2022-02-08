997aaa802234500c2b3978110aae1d18c72085f6
Lisa Sau <lisasau85@gmail.com>
2022-02-08 12:38:27 +0000

    feature/cypress-TL-school-trust-check-mark
    - Includes checkmark preconditions

--- ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/84457_TL_school-and-trust-information-proj-dates/86296_error-handling_checkmark.js
+++ ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/84457_TL_school-and-trust-information-proj-dates/86296_error-handling_checkmark.js
@@ -9,6 +9,23 @@ describe("86296 Check mark should reflect status correctly", () => {
         cy.selectSchoolListing(1)
     });
 
+    it("TC: Precondition checkbox status", () => {
+        cy.get('[id=school-and-trust-information-status]').should("be.visible")
+        .invoke("text")
+        .then((text) => {
+            // text == "Completed" || "In Progress"
+            if (text.includes("Completed")) {
+                cy.log("Completed")
+                return
+            }
+            else {
+                cy.get('*[href*="/confirm-school-trust-information-project-dates"]').click();
+                cy.get('[id="school-and-trust-information-complete"]').click();
+                cy.get("#confirm-and-continue-button").click();
+            }
+        })
+    })
+
     it("TC01: Unchecked and returns as 'In Progress", () => {
         cy.get('*[href*="/confirm-school-trust-information-project-dates"]').click();
         cy.get('[id="school-and-trust-information-complete"]').click()
@@ -22,8 +39,8 @@ describe("86296 Check mark should reflect status correctly", () => {
         cy.get("#confirm-and-continue-button").click();
         cy.get('[id=school-and-trust-information-status]').contains('Completed').should('not.contain', 'In Progress')
     });
-});
 
-after(function () {
-    cy.clearLocalStorage();
+    after(function () {
+        cy.clearLocalStorage();
+    });
 });
