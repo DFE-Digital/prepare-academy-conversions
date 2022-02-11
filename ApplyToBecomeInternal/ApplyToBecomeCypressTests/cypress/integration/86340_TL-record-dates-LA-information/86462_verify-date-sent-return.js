9d6ed28904735c8271cb3bd11aba21e0512ee99c
Lisa Sau <lisasau85@gmail.com>
2022-02-11 17:10:50 +0000

    Task: 86462 LA info template date preview
    - date sent
    - date return

---
+++ ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/86340_TL-record-dates-LA-information/86462_verify-date-sent-return.js
@@ -0,0 +1,41 @@
+/// <reference types ="Cypress"/>
+
+describe("86462'Date you sent/return the template' are reflected in preview", () => {
+    afterEach(() => {
+        cy.storeSessionData()
+    });
+
+    before(() => {
+        cy.login()
+        cy.selectSchoolListing(2)
+        cy.url().then(url => {
+            //changes the current URL
+            let modifiedUrl = url + "/confirm-local-authority-information-template-dates"
+            cy.visit(modifiedUrl)
+        });
+    });
+    
+    after(() => {
+        cy.clearLocalStorage()
+    });
+
+    it("TC01: Verifies 'Date you sent the template' on LA information preview", () => {
+        cy.get('[data-test="change-la-info-template-sent-date"]').click()
+        cy.submitDateLaInfoSent(20, 2, 2022)
+        cy.get('[data-module="govuk-button"]').click()
+        cy.get('[id="la-info-template-sent-date"]').invoke("text")
+        .then((text) => {
+            expect(text).to.match(/^([0-9]){2}\s[a-zA-Z]{1,}\s[0-9]{4}$/)
+        });
+    });
+
+    it("TC02: Verifies 'Date you want the template returned' on LA information preview", () => {
+        cy.get('[data-test="change-la-info-template-returned-date"]').click()
+        cy.submitDateLaInfoReturn(20, 2, 2020)
+        cy.get('[data-module="govuk-button"]').click()
+        cy.get('[id="la-info-template-returned-date"]').invoke("text")
+        .then((text) => {
+            expect(text).to.match(/^([0-9]){2}\s[a-zA-Z]{1,}\s[0-9]{4}$/)
+        });
+    });
+});
