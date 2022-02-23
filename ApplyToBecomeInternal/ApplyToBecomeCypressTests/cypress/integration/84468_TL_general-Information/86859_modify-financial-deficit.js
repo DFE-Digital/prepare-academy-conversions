b6517affb36043bed6f51f958716489cf5419808
Lisa Sau <lisasau85@gmail.com>
2022-02-23 19:24:05 +0000

    refactor/cypress-TL-conversions
    Added:
    - Commands
    - Adopted single quotation
    
    Removed:
    - Unnecessary semicolons
    - Double quotations

--- ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/84468_TL_general-Information/86859_modify-financial-deficit.js
+++ ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/84468_TL_general-Information/86859_modify-financial-deficit.js
@@ -1,37 +1,39 @@
-/// <reference types ="Cypress"/>
+/// <reference types ='Cypress'/>
 
 describe('86859 Modify viability fields', () => {
     afterEach(() => {
-        cy.storeSessionData();
+        cy.storeSessionData()
     });
 
     before(() => {
-        cy.login();
+        cy.login()
         cy.selectSchoolListing(2)
         cy.url().then(url => {
-            let modifiedUrl = url + "/confirm-general-information"
+            let modifiedUrl = url + '/confirm-general-information'
             cy.visit(modifiedUrl)
         });
     });
 
     after(() => {
-        cy.clearLocalStorage();
+        cy.clearLocalStorage()
     });
 
-    it("TC01: Navigates to Financial deficit fields and modifies fields 'Yes'", () => {
+    it('TC01: Navigates to Financial deficit fields and modifies fields "Yes"', () => {
         cy.get('[data-test="change-financial-deficit"]').click()
         cy.get('[id="financial-deficit"]').click()
-        cy.get('[id="confirm-and-continue-button"]').click()
-        cy.get('[id="financial-deficit"]').should('contain', 'Yes')
+        cy.saveContinueBtn().click()
+        cy.get('[id="financial-deficit"]')
+        .should('contain', 'Yes')
         .should('not.contain', 'No')
         .should('not.contain', 'Empty')
     });
 
-    it("TC02: Navigates to Financial deficit fields and modifies fields 'No'", () => {
+    it('TC02: Navigates to Financial deficit fields and modifies fields "No"', () => {
         cy.get('[data-test="change-financial-deficit"]').click()
         cy.get('[id="financial-deficit-2"]').click()
-        cy.get('[id="confirm-and-continue-button"]').click()
-        cy.get('[id="financial-deficit"]').should('contain', 'No')
+        cy.saveContinueBtn().click()
+        cy.get('[id="financial-deficit"]')
+        .should('contain', 'No')
         .should('not.contain', 'Yes')
         .should('not.contain', 'Empty')
     });
