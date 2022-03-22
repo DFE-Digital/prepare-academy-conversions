1bf4477e9699577c6ecd45fa3d38c73bb4824dce
Lisa Sau <lisasau85@gmail.com>
2022-03-22 10:52:26 +0000

    Task: 88238 Update error message
    - Unskip test to reflect changes made on error message prompt.

--- ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/91521_SCAPP_apply-to-become/91489_Apply-to-become-get-application types.js
+++ ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/91521_SCAPP_apply-to-become/91489_Apply-to-become-get-application types.js
@@ -1,6 +1,8 @@
 /// <reference types ='Cypress'/>
 
 describe('91489: Apply-to-become GET application types', () => {
+    let url = Cypress.env('url')
+
     afterEach(() => {
         cy.storeSessionData()
     });
@@ -14,14 +16,15 @@ describe('91489: Apply-to-become GET application types', () => {
         cy.clearLocalStorage()
     });
 
+
     it('TC01: Error Message Page not found if no existing ID', () => {
-        cy.visit(Cypress.env('url') + '/school-application-form/666', {failOnStatusCode: false})
+        cy.visit(url+'/school-application-form/666', {failOnStatusCode: false})
         cy.get('[id="error-heading"]').should('contain.text', 'Page not found')
     });
 
 
     it('TC02: FormMAT type should not be valid and should display "Not Implemented"', () => {
-        cy.visit(Cypress.env('url') + '/school-application-form/521', {failOnStatusCode: false})
+        cy.visit( url+'/school-application-form/521', {failOnStatusCode: false})
         cy.get('[id="error-heading"]').should('contain.text', 'Not implemented')
     })
 })
