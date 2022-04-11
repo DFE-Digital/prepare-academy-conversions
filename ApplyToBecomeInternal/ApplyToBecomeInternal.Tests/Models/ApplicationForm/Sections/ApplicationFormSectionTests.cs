ddd3a7b6ca34bb3d85d8965f313c224a0f4a6d3a
sureshbabukuruva <100155371+sureshbabukuruva@users.noreply.github.com>
2022-04-11 11:28:13 +0100

    83618- add follow up answers to questions in the aplication form (#271)
    * 83618- add follow up answers to questions in the application form
    Co-authored-by: Catherine LAWLOR <Cath.LAWLOR@gmail.com>
    Co-authored-by: Catherine Lawlor <55924986+CathLass@users.noreply.github.com>

--- ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/Models/ApplicationForm/Sections/ApplicationFormSectionTests.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/Models/ApplicationForm/Sections/ApplicationFormSectionTests.cs
@@ -12,19 +12,8 @@ namespace ApplyToBecomeInternal.Tests.Models.ApplicationForm.Sections
 {
 	public class ApplicationFormSectionTests
 	{
-		[Fact(Skip = "complete when missng fields are implemented")]
-		public void Constructor_WithApplication_SetsFields()
-		{
-			var fixture = new Fixture();
-			var application = fixture.Create<Application>();
-			application.ChangesToLaGovernance = true;
-			application.ChangesToLaGovernance = true;
-
-			throw new NotImplementedException();
-		}
-
 		[Fact]
-		public void Constructor_Doesnt_Set_Conditional_Rows_Following_No_Answers()
+		public void Constructor_Doesnt_Include_Conditional_Rows_Following_No_Answers()
 		{
 			var fixture = new Fixture();
 			var application = fixture.Create<Application>();
@@ -41,9 +30,39 @@ namespace ApplyToBecomeInternal.Tests.Models.ApplicationForm.Sections
 
 			var expectedSubSectionFields = new[] {
 				new FormField("Trust name", application.Name),
-				new LinkFormField("Upload evidence that the trust consents to the school joining", "MadeUpDocName", application.TrustConsentEvidenceDocumentLink),
 				new FormField("Will there be any changes to the governance of the trust due to the school joining?", "No"),
 				new FormField("Will there be any changes at a local level due to this school joining?", "No"),
+			};
+
+			formSection.Heading.Should().Be("Overview");
+			formSection.Fields.Should().BeEquivalentTo(expectedFields);
+			formSection.SubSections.Should().HaveCount(1);
+			formSection.SubSections.First().Heading.Should().Be("Details");
+			formSection.SubSections.First().Fields.Should().BeEquivalentTo(expectedSubSectionFields);
+		}
+
+		[Fact]
+		public void Constructor_Includes_Conditional_Rows_Following_Yes_Answers()
+		{
+			var fixture = new Fixture();
+			var application = fixture.Create<Application>();
+			application.ChangesToLaGovernance = true;
+			application.ChangesToTrust = true;
+			var formSection = new ApplicationFormSection(application);
+
+			var expectedFields = new[]
+			{
+				new FormField("Application to join", $"{application.Name} with {application.ApplyingSchools.First().SchoolName}"),
+				new FormField("Application reference", application.ApplicationId),
+				new FormField("Lead applicant", application.ApplicationLeadAuthorName),
+			};
+
+			var expectedSubSectionFields = new[] {
+				new FormField("Trust name", application.Name),
+				new FormField("Will there be any changes to the governance of the trust due to the school joining?", "Yes"),
+				new FormField("What are the changes?", application.ChangesToTrustExplained),
+				new FormField("Will there be any changes at a local level due to this school joining?", "Yes"),
+				new FormField("What are the changes and how do they fit into the current lines of accountability in the trust?", application.ChangesToLaGovernanceExplained)
 			};
 
 			formSection.Heading.Should().Be("Overview");
--- ApplyToBecomeInternal/ApplyToBecomeInternal/Models/ApplicationForm/Sections/ApplicationFormSection.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Models/ApplicationForm/Sections/ApplicationFormSection.cs
@@ -25,12 +25,11 @@ namespace ApplyToBecomeInternal.Models.ApplicationForm.Sections
 		{
 			var formFields = new List<FormField> {
 				new FormField("Trust name", application.Name),
-				new LinkFormField("Upload evidence that the trust consents to the school joining", "MadeUpDocName", application.TrustConsentEvidenceDocumentLink)
 			};
 			formFields.Add(new FormField("Will there be any changes to the governance of the trust due to the school joining?", application.ChangesToTrust.ToYesNoString()));
 			if ( application.ChangesToTrust == true ) formFields.Add(new FormField("What are the changes?", application.ChangesToTrustExplained) );
 			formFields.Add(new FormField("Will there be any changes at a local level due to this school joining?", application.ChangesToLaGovernance.ToYesNoString()));
-			if (application.ChangesToLaGovernance == true) formFields.Add(new FormField("What are the changes and how do they fit into the current lines of accountability in the trust", application.ChangesToLaGovernanceExplained));
+			if (application.ChangesToLaGovernance == true) formFields.Add(new FormField("What are the changes and how do they fit into the current lines of accountability in the trust?", application.ChangesToLaGovernanceExplained));
 
 			return formFields;
 		}
