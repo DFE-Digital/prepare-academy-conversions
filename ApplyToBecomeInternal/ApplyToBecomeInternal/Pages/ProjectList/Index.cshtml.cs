b8280cdb3124abdafa2fdefce08612d5b83eb9cb
Aisha JILANI <Aisha.JILANI@EDUCATION.GOV.UK>
2022-07-18 15:21:11 +0100

    Task 99970: Add paging to the UI

--- ApplyToBecomeInternal/ApplyToBecomeInternal/Pages/ProjectList/Index.cshtml.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Pages/ProjectList/Index.cshtml.cs
@@ -5,16 +5,27 @@ using ApplyToBecome.Data.Models;
 using ApplyToBecome.Data.Services;
 using ApplyToBecomeInternal.Extensions;
 using ApplyToBecomeInternal.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ApplyToBecomeInternal.Pages.ProjectList
 {
 	public class IndexModel : PageModel
     {
+		private readonly int _pageSize = 10;
+
 		public IEnumerable<ProjectListViewModel> Projects { get; set; }
 		public int ProjectCount => Projects.Count();
 
 		private readonly IAcademyConversionProjectRepository _repository;
+
+		public int StartingPage { get; private set; } = 1;
+		public bool HasPreviousPage => CurrentPage > 1;
+		public bool HasNextPage => Projects.ToList().Count == _pageSize;
+		public int PreviousPage => CurrentPage - 1;
+		public int NextPage => CurrentPage + 1;
+
+		[BindProperty(SupportsGet = true)] public int CurrentPage { get; set; } = 1;
 
 		public IndexModel(IAcademyConversionProjectRepository repository)
 		{
@@ -23,13 +34,18 @@ namespace ApplyToBecomeInternal.Pages.ProjectList
 
 		public async Task OnGetAsync()
         {
-			var response = await _repository.GetAllProjects(1, 50);
+			var response = await _repository.GetAllProjects(CurrentPage, _pageSize);
 			if (!response.Success)
 			{
 				// 500 maybe?
 			}
 
 			Projects = response.Body.Select(Build).ToList();
+
+			if (CurrentPage - 5 > 1)
+			{
+				StartingPage = CurrentPage - 5;
+			}
 		}
 
 		private ProjectListViewModel Build(AcademyConversionProject academyConversionProject)
