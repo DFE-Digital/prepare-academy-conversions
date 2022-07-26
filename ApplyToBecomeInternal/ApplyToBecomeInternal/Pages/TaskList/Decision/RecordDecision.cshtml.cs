1054888f61a6533ca259b037622c5853bd82d90a
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-07-26 16:12:12 +0100

    AB#101442 record decision - create decision base class

--- ApplyToBecomeInternal/ApplyToBecomeInternal/Pages/TaskList/Decision/RecordDecision.cshtml.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Pages/TaskList/Decision/RecordDecision.cshtml.cs
@@ -1,48 +1,36 @@
 using ApplyToBecome.Data.Services;
-using ApplyToBecomeInternal.Extensions;
 using ApplyToBecomeInternal.Models;
+using ApplyToBecomeInternal.Pages.TaskList;
 using ApplyToBecomeInternal.Pages.TaskList.Decision.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Threading.Tasks;
-using static ApplyToBecomeInternal.Pages.TaskList.Decision.DecisionConstants;
 
 namespace ApplyToBecomeInternal.Pages.Decision
 {
-	public class RecordDecisionModel : PageModel
-	{
-		private readonly IAcademyConversionProjectRepository _repository;
-		private readonly ISession _session;		
+	public class RecordDecisionModel : DecisionBaseModel
+	{		
 
-		public RecordDecisionModel(IAcademyConversionProjectRepository repository, ISession session)
-		{
-			_repository = repository;
-			_session = session;
+		public RecordDecisionModel(IAcademyConversionProjectRepository repository, ISession session) : base(repository, session)
+		{		
 		}
-				
-		public string SchoolName { get; set; }
-		public int Id { get; set; }
+					
 		[BindProperty]
 		public AdvisoryBoardDecisions AdvisoryBoardDecision { get; set; }
 
 		public async Task<IActionResult> OnGetAsync(int id)
 		{
-			Id = id;
-			AdvisoryBoardDecision = 
-				_session.Get<AdvisoryBoardDecision>(DECISION_SESSION_KEY)?.Decision ?? AdvisoryBoardDecisions.Approved;
-
-			var project = await _repository.GetProjectById(id);
-			SchoolName = project.Body.SchoolName;			
+			await SetDefaults(id);
+			AdvisoryBoardDecision = GetDecisionFromSession()?.Decision ?? AdvisoryBoardDecisions.Approved;						
 
 			return Page();
 		}
 
 		public IActionResult OnPostAsync(int id)
 		{
-			var decision = _session.Get<AdvisoryBoardDecision>(DECISION_SESSION_KEY) ?? new AdvisoryBoardDecision();
+			var decision = GetDecisionFromSession() ?? new AdvisoryBoardDecision();
 			decision.Decision = AdvisoryBoardDecision;
-			_session.Set(DECISION_SESSION_KEY, decision);
+			SetDecisionInSession(decision);
 
 			return RedirectToPage(Links.Decision.WhoDecided.Page, new { id });
 		}
