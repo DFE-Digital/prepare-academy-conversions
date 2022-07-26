2a7109da883e5e7bc68816914365bc105ec4e2b8
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-07-26 15:05:03 +0100

    AB#101442 record decision - add who decided form

--- ApplyToBecomeInternal/ApplyToBecomeInternal/Pages/TaskList/Decision/Models/AdvisoryBoardDecision.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Pages/TaskList/Decision/Models/AdvisoryBoardDecision.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ApplyToBecomeInternal.Pages.TaskList.Decision.Models
 {
@@ -19,10 +20,15 @@ namespace ApplyToBecomeInternal.Pages.TaskList.Decision.Models
 
 	public enum DecisionMadeBy
 	{
+		[Description("Regional Director for the region")]
 		RegionalDirectorForRegion = 0,
+		[Description("A different Regional Director")]
 		OtherRegionalDirector = 1,
+		[Description("Minister")]
 		Minister = 2,
+		[Description("Director General")]
 		DirectorGeneral = 3,
+		[Description("Other")]
 		None = 4
 	}
 
