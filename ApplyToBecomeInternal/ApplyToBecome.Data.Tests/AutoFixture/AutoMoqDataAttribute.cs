76d2f34d7c7c82f2531a3f3f1cbe6d7132f22dd6
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-07-28 13:03:45 +0100

    AB#101442 record decision - tidy pages and new unit tests

---
+++ ApplyToBecomeInternal/ApplyToBecome.Data.Tests/AutoFixture/AutoMoqDataAttribute.cs
@@ -0,0 +1,17 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+
+namespace ApplyToBecome.Data.Tests.AutoFixture
+{
+	public class AutoMoqDataAttribute : AutoDataAttribute
+	{
+		public AutoMoqDataAttribute()
+			: base(() =>
+			{
+				return new Fixture().Customize(new CompositeCustomization(new AutoMoqCustomization()));
+			})
+		{
+		}
+	}
+}
