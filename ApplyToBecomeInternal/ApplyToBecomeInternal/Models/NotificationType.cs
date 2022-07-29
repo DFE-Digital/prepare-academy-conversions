512d4a02077cb878929056c2c2547152f5f396e2
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-07-29 09:09:48 +0100

    AB#101442 record decision - adding success notification

---
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Models/NotificationType.cs
@@ -0,0 +1,9 @@
+﻿namespace ApplyToBecomeInternal.Models
+{
+	public enum NotificationType
+	{
+		Success,
+		Error,
+		Warning
+	}
+}
--- ApplyToBecomeInternal/ApplyToBecomeInternal/Pages/TaskList/Decision/Summary.cshtml.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Pages/TaskList/Decision/Summary.cshtml.cs
@@ -1,9 +1,11 @@
 using ApplyToBecome.Data.Models.AdvisoryBoardDecision;
 using ApplyToBecome.Data.Services;
 using ApplyToBecome.Data.Services.Interfaces;
+using ApplyToBecomeInternal.Extensions;
 using ApplyToBecomeInternal.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using System.Threading.Tasks;
 
 namespace ApplyToBecomeInternal.Pages.TaskList.Decision
@@ -27,7 +29,7 @@ namespace ApplyToBecomeInternal.Pages.TaskList.Decision
 			{
 				{ Decision: AdvisoryBoardDecisions.Approved, ApprovedConditionsSet: true } => "APPROVED WITH CONDITIONS",
 				_ => Decision?.Decision.ToString().ToUpper()
-			};						
+			};
 		}
 
 		public async Task<IActionResult> OnGetAsync(int id)
@@ -40,7 +42,12 @@ namespace ApplyToBecomeInternal.Pages.TaskList.Decision
 
 		public async Task<IActionResult> OnPostAsync(int id)
 		{
+			if (!ModelState.IsValid) return await OnGetAsync(id);
+
 			await _advisoryBoardDecisionRepository.Create(GetDecisionFromSession());
+			SetDecisionInSession(null);
+
+			TempData.SetNotification(NotificationType.Success, "Done", "Decision recorded");
 
 			return RedirectToPage(Links.ProjectList.Index.Page);
 		}
