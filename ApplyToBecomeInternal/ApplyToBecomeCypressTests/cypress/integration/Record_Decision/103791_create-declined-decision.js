3d91b385ef820f7171ed8e5456c8e030d1e79dd3
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-08-30 17:20:42 +0100

    test: added additional tests for recording a decicion

---
+++ ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/Record_Decision/103791_create-declined-decision.js
@@ -0,0 +1,57 @@
+/// <reference types ='Cypress'/>
+
+// uri to be updated once academisation API is integrated
+let url = Cypress.env('url') + '/task-list/2054?rd=true'
+let projecList = Cypress.env('url') + '/project-list'
+
+describe('103791 Create Declined journey', () => {
+    beforeEach(() => {
+        // delete decision
+        cy.sqlServer('DELETE FROM [academisation].[ConversionAdvisoryBoardDecision] WHERE ConversionProjectId = 2054')
+        cy.clearCookies()
+        cy.visit(url)
+    })
+
+    // Edit Approval Path - Regional Director, Finance 
+    it('TC01: J2 Create a recorded decision Declined - Reg Director Region, Finance', () => {
+        // Click on change your decision button 
+        cy.changeDecision().should('contain.text', 'Record a decision').click()
+         //select iniital decision
+        cy.declineRadioBtn().click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        cy.get('[id="regionaldirectorforregion-radio"]').click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        cy.declineFinancebox().click()
+            .then(() => cy.declineFinancText().clear().type('finance details'))
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // date entry
+        cy.recordDecisionDate(10, 8, 2022)
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // Change condition
+        cy.reasonchangeLink().click()
+        cy.declineFinancebox()
+        .invoke('attr', 'aria-expanded')
+        cy.declineFinancText().clear().type('finance details 2nd test')
+        cy.continueBtn().click()
+        cy.continueBtn().click()
+        // preview answers before submit
+        cy.decision().should('contain.text', 'Declined')
+        cy.decisionMadeBy().should('contain.text', 'Regional Director for the region')        
+        // check reasons
+        var declinedReasons = cy.get('[id="decline-reasons"]')
+        declinedReasons.should('contain.text', 'Finance:')
+        declinedReasons.should('contain.text', 'finance details 2nd test')
+        // check date                
+        cy.decisionDate().should('contain.text', '10 August 2022')
+        // clicks on the record a decision button to submit
+        cy.recordThisDecision().click()
+        // recorded decision confirmation
+        cy.recordnoteMsg().should('contain.text', 'Decision recorded')
+        cy.visit(projecList)
+        cy.projectStateId().should('contain.text', 'DECLINED')
+    })
+})
