ccae432f713fe65d2a978933f5dee83b6e05278d
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-08-30 17:20:42 +0100

    test: added additional tests for recording a decicion

---
+++ ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/integration/Record_Decision/103195_create-approved-decision.js
@@ -0,0 +1,53 @@
+/// <reference types ='Cypress'/>
+
+// uri to be updated once academisation API is integrated
+let url = Cypress.env('url') + '/task-list/2054?rd=true'
+
+describe('103195 Record new Approved decision', ()=> {
+
+    beforeEach(() => {
+        // delete decision
+        cy.sqlServer('DELETE FROM [academisation].[ConversionAdvisoryBoardDecision] WHERE ConversionProjectId = 2054')
+        cy.clearCookies()
+        cy.visit(url)
+    })
+
+    // Edit Approval Path - Regional Director, No/Yes conditions set
+    it('TC01: J1 Create a new recorded decision Approval - Reg Director Region, No Conditions', () => {
+        // Click on change your decision button
+        cy.get('[id="record-decision-link"]').should('contain.text', 'Record a decision').click()
+         //select iniital decision
+        cy.get('[id="approved-radio"]').click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // selects regional director button
+        cy.get('[id="regionaldirectorforregion-radio"]').click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // selects 'no' on conditions met
+        cy.NoRadioBtn().click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // date entry
+        cy.recordDecisionDate(10, 8, 2022)
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // Change condition
+        cy.ChangeConditionsLink().click()
+        cy.YesRadioBtn().click()
+        cy.continueBtn().click()
+        cy.YesTextBox().clear().type('This is a test')
+        cy.continueBtn().click()
+        cy.continueBtn().click()
+        // preview answers before submit
+        cy.ApprovedDecisionPreview().should('contain.text', 'APPROVED WITH CONDITIONS')
+        cy.ApprovedMadeByPreview().should('contain.text', 'Regional Director for the region')
+        cy.AprrovedConditionsSet().should('contain.text', 'Yes')
+        cy.ApprovedConditionDetails().should('contain.text', 'This is a test')
+        cy.ApprovedDecisionDate().should('contain.text', '10 August 2022')
+        // clicks on the record a decision button to submit
+        cy.recordThisDecision().click()
+        // recorded decision confirmation
+        cy.ApprovedMessageBanner().should('contain.text', 'Decision recorded')
+    })
+})
