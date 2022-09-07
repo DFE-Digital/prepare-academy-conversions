11410289568337e7c277fdbeec484b136b1b07d1
MartinWheelerMT <martin.wheeler@madetech.com>
2022-09-07 17:42:25 +0100

    AB#105769 - Implement Landing Page Conversions
    Add configuration setting for transfers url
    change launchSettings.json to open at '/'
    Added IOptions for ServiceLink
    Added ServiceLinkOptions configuration to Startup

--- ApplyToBecomeInternal/ApplyToBecomeInternal/Startup.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Startup.cs
@@ -2,6 +2,7 @@ using ApplyToBecome.Data.Services;
 using ApplyToBecome.Data.Services.Interfaces;
 using ApplyToBecomeInternal.Authorization;
 using ApplyToBecomeInternal.Configuration;
+using ApplyToBecomeInternal.Options;
 using ApplyToBecomeInternal.Security;
 using ApplyToBecomeInternal.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -94,7 +95,9 @@ namespace ApplyToBecomeInternal
 				client.BaseAddress = new Uri(apiOptions.BaseUrl);
 				client.DefaultRequestHeaders.Add("x-api-key", apiOptions.ApiKey);
 			});
-
+			
+			services.Configure<ServiceLinkOptions>(Configuration.GetSection(ServiceLinkOptions.Name));
+			
 			services.AddScoped<ErrorService>();
 			services.AddScoped<IGetEstablishment, EstablishmentService>();
 			services.Decorate<IGetEstablishment, GetEstablishmentItemCacheDecorator>();
