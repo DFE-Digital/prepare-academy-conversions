78d555ea643634b7d117c3aeb193797e417cec3e
Dominic Need <dominic.need@nimbleapproach.com>
2022-09-27 14:54:45 +0100

    New string extension for splitting PascalCase

---
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace ApplyToBecomeInternal.Extensions
+{
+	public static class StringExtensions
+	{
+		public static string SplitPascalCase<T>(this T source)
+		{
+			return source == null ? string.Empty : Regex.Replace(source.ToString(), "[A-Z]", " $0").Trim().ToString();
+		}
+	}
+}
