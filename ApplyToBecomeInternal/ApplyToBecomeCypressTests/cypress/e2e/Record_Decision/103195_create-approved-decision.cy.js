ee04b357724e66c726369654e823b1312b8205c4
Lisa Sau <lisasau85@gmail.com>
2022-10-06 15:56:55 +0100

    Task 108196: Cypress Tags
    Cypress grep:
    - Cypress 10.0^ dependency
    - Grep Module import to /plugins/index.js & /support/e2e.js
    - Tags in use: @dev,@ stage, @integration
    - For more recipes: https://github.com/cypress-io/cypress-grep
    
    Cypress upgrade:
    - Cypress package version: 10.9.0
    - Folder where tests live is renamed: from 'integration', to 'e2e'
    - Test files renamed: from '.spec.js', now '.cy.js'
    - Support files renamed from: /support/index.js, now support/e2e.js
    - Cypress config files changed: from cypress.json, now cypress.config.ts
    - Latest changes: https://docs.cypress.io/guides/references/changelog
    
    Cypress 10.9.0 known issues:
    - Unable to run all tests at once via Cypress GUI: https://github.com/cypress-io/cypress/discussions/21628
    - Error Log each time tests run in terminal: https://github.com/cypress-io/cypress/issues/22273
    
    README:
    - grep run command
    - grep recipes

---
+++ ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/e2e/Record_Decision/103195_create-approved-decision.cy.js
@@ -0,0 +1,67 @@
+/// <reference types ='Cypress'/>
+
+// uri to be updated once academisation API is integrated
+import RecordDecision from '../../pages/recordDecision'
+
+describe('103195 Record new Approved decision', { tags: '@dev'}, () => {
+
+    beforeEach(() => {
+        RecordDecision.selectProject().then(id => {
+            // delete decision
+            cy.sqlServer(`DELETE FROM [academisation].[ConversionAdvisoryBoardDecision] WHERE ConversionProjectId = ${id}`)
+            cy.clearCookies()
+            cy.url().then(url => cy.visit(`${url}?rd=true`))
+        })
+    })
+
+    // Edit Approval Path - Regional Director, No/Yes conditions set
+    it('TC01: J1 Create a new recorded decision Approval - Reg Director Region, No Conditions', () => {
+        // Click on change your decision button
+        cy.get('[id="record-decision-link"]').should('contain.text', 'Record a decision').click()
+        //select iniital decision
+        cy.get('[id="approved-radio"]').click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // selects regional director button
+        cy.get('[id="regionaldirectorforregion-radio"]').click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // selects 'no' on conditions met
+        cy.NoRadioBtn().click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // date entry
+        cy.recordDecisionDate(10, 8, 2022)
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // Change condition
+        cy.ChangeConditionsLink().click()
+        cy.YesRadioBtn().click()
+        cy.YesTextBox().clear().type('This is a test')
+        cy.continueBtn().click()
+        cy.continueBtn().click()
+        // preview answers before submit
+        checkSummary()
+        cy.continueBtn().click()
+        // recorded decision confirmation
+        cy.ApprovedMessageBanner().should('contain.text', 'Decision recorded')
+        checkSummary()
+        // confirm project status has been updated
+        cy.url().then(url => {
+            const id = RecordDecision.getIdFromUrl(url)
+            cy.visit(Cypress.env('url') + '/project-list')
+            cy.get(`[id="project-status-${id}"]`).should('contain.text', 'APPROVED WITH CONDITIONS')
+        })
+    })
+
+    // Displayed at the end of the journey
+    function checkSummary() {
+        cy.ApprovedDecisionPreview().should('contain.text', 'APPROVED WITH CONDITIONS')
+        cy.ApprovedMadeByPreview().should('contain.text', 'Regional Director for the region')
+        cy.AprrovedConditionsSet().should('contain.text', 'Yes')
+        cy.AprrovedConditionsSet().should('contain.text', 'This is a test')
+        cy.ApprovedDecisionDate().should('contain.text', '10 August 2022')
+
+    }
+
+})
---
+++ ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/e2e/Record_Decision/103788_new-plus-edit-deferred-decision.cy.js
@@ -0,0 +1,243 @@
+/// <reference types ='Cypress'/>
+// uri to be updated once academisation API is integrated
+let projectList = Cypress.env('url') + '/project-list'
+import RecordDecision from '../../pages/recordDecision'
+
+describe('Edit Deferred journey', {tags: '@dev'}, () => {
+    beforeEach(() => {    
+        RecordDecision.selectProject().then(id => {
+            // delete deferred reasons
+            cy.sqlServer(`
+            delete from 
+                academisation.ConversionAdvisoryBoardDecisionDeferredReason 
+            where 
+                AdvisoryBoardDecisionId = (select Id from academisation.ConversionAdvisoryBoardDecision where ConversionProjectId = ${id})`) 
+            cy.sqlServer(`delete from academisation.ConversionAdvisoryBoardDecision where ConversionProjectId = ${id}`)
+            cy.sqlServer(`insert into academisation.ConversionAdvisoryBoardDecision values (${id}, \'Deferred\', null, null, getdate(), \'None\', getdate(), getdate())`)
+            cy.clearCookies()
+            cy.url().then(url => cy.visit(`${url}?rd=true`))
+        })
+    })
+
+    // Edit Deferred Path - Regional Director, Additional information needed 
+    it('TC01: J3 Edit a recorded decision Deferred - Reg Director Region, Finance', () => {
+        // Click on change your decision button 
+        cy.changeDecision().should('contain.text', 'Change your decision').click()
+        //select iniital decision
+        cy.deferredRadioBtn().click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        cy.get('[id="regionaldirectorforregion-radio"]').click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        cy.addInfoNeededBox().click()
+            .then(() => cy.addInfoNeededText().clear().type('Additional info'))
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // date entry
+        cy.recordDecisionDate(10, 8, 2022)
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // give deferred reason
+        cy.deferredReasonChangeLink().click()
+        // set details
+        cy.addInfoNeededText().clear().type('Additional info 2nd time')
+        cy.continueBtn().click()
+        cy.continueBtn().click()
+        // preview answers before submit
+        cy.deferredDecision().should('contain.text', 'Deferred')
+        cy.deferredDecisionMadeBy().should('contain.text', 'Regional Director for the region')
+        // check reasons
+        var deferredReasons = cy.get('[id="deferred-reasons"]')
+        deferredReasons.should('contain.text', 'Additional information needed:')
+        deferredReasons.should('contain.text', 'Additional info 2nd time')
+        cy.deferredDecisionDate().should('contain.text', '10 August 2022')
+        // clicks on the record a decision button to submit
+        cy.continueBtn().click()
+        // recorded decision confirmation
+        cy.deferredProjectStateId().should('contain.text', 'Decision recorded')
+        cy.url().then(url => {
+            const id = RecordDecision.getIdFromUrl(url)
+            cy.visit(projectList)
+            cy.get(`[id="project-status-${id}"]`).should('contain.text', 'DEFERRED')
+        })
+    })
+
+    // Edit Deferred Path - A different Regional Director, ofsted report 
+    it('TC02: J3 Edit a recorded decision Deferred - Different Reg Director, ofsted report', () => {
+        // Click on change your decision button 
+        cy.changeDecision().should('contain.text', 'Change your decision').click()
+        //select iniital decision
+        cy.deferredRadioBtn().click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // selects regional director button
+        cy.get('[id="otherregionaldirector-radio"]').click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        cy.awaitOfstedReportBox().click()
+            .then(() => cy.awaitOfstedReportText().clear().type('awaiting ofsted'))
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // date entry
+        cy.recordDecisionDate(10, 8, 2022)
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // Change condition
+        cy.deferredReasonChangeLink().click()
+        cy.awaitOfstedReportText().clear().type('awaiting ofsted 2nd time')
+        cy.continueBtn().click()
+        cy.continueBtn().click()
+        // preview answers before submit
+        cy.deferredDecision().should('contain.text', 'Deferred')
+        cy.deferredDecisionMadeBy().should('contain.text', 'A different Regional Director')
+        // check reasons
+        var deferredReasons = cy.get('[id="deferred-reasons"]')
+        deferredReasons.should('contain.text', 'Awaiting next ofsted report:')
+        deferredReasons.should('contain.text', 'awaiting ofsted 2nd time')
+        // check date
+        cy.deferredDecisionDate().should('contain.text', '10 August 2022')
+        // clicks on the record a decision button to submit
+        cy.continueBtn().click()
+        // recorded decision confirmation
+        cy.deferredProjectStateId().should('contain.text', 'Decision recorded')
+        cy.url().then(url => {
+            const id = RecordDecision.getIdFromUrl(url)
+            cy.visit(projectList)
+            cy.get(`[id="project-status-${id}"]`).should('contain.text', 'DEFERRED')
+        })
+    })
+
+    // Edit Deferred Path - Director General, Performance concerns
+    it('TC03: J3 Edit a recorded decision Deferred - Director General, Performance', () => {
+        // Click on change your decision button 
+        cy.changeDecision().should('contain.text', 'Change your decision').click()
+        //select iniital decision
+        cy.deferredRadioBtn().click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // selects regional director button
+        cy.get('[id="directorgeneral-radio"]').click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        cy.performanceCheckBox().click()
+            .then(() => cy.performanceCheckText().clear().type('performance details'))
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // date entry
+        cy.recordDecisionDate(10, 8, 2022)
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // Change condition
+        cy.deferredReasonChangeLink().click()        
+        cy.performanceCheckText().clear().type('performance details 2nd time')
+        cy.continueBtn().click()
+        cy.continueBtn().click()
+        // preview answers before submit
+        cy.deferredDecision().should('contain.text', 'Deferred')
+        cy.deferredDecisionMadeBy().should('contain.text', 'Director General')
+        // check reasons
+        var deferredReasons = cy.get('[id="deferred-reasons"]')
+        deferredReasons.should('contain.text', 'Performance concerns:')
+        deferredReasons.should('contain.text', 'performance details 2nd time')
+        // check date
+        cy.deferredDecisionDate().should('contain.text', '10 August 2022')
+        // clicks on the record a decision button to submit
+        cy.continueBtn().click()
+        // recorded decision confirmation
+        cy.deferredProjectStateId().should('contain.text', 'Decision recorded')
+        cy.url().then(url => {
+            const id = RecordDecision.getIdFromUrl(url)
+            cy.visit(projectList)
+            cy.get(`[id="project-status-${id}"]`).should('contain.text', 'DEFERRED')
+        })
+    })
+
+    // Edit Deferred Path - Minister, Other
+    it('TC04: J3 Edit a recorded decision Deferred - Minister, Other', () => {
+        // Click on change your decision button 
+        cy.changeDecision().should('contain.text', 'Change your decision').click()
+        //select iniital decision
+        cy.deferredRadioBtn().click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // selects regional director button
+        cy.get('[id="minister-radio"]').click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        cy.OtherCheckBox().click()            
+            .then(() => cy.OtherCheckText().clear().type('other details'))
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // date entry
+        cy.recordDecisionDate(10, 8, 2022)
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // Change condition
+        cy.deferredReasonChangeLink().click()
+        cy.OtherCheckText().clear().type('other details 2nd time')
+        cy.continueBtn().click()
+        cy.continueBtn().click()
+        // preview answers before submit
+        cy.deferredDecision().should('contain.text', 'Deferred')
+        cy.deferredDecisionMadeBy().should('contain.text', 'Minister')
+        // check reasons
+        var deferredReasons = cy.get('[id="deferred-reasons"]')
+        deferredReasons.should('contain.text', 'Other:')
+        deferredReasons.should('contain.text', 'other details 2nd time')
+        cy.deferredDecisionDate().should('contain.text', '10 August 2022')
+        // clicks on the record a decision button to submit
+        cy.continueBtn().click()
+        // recorded decision confirmation
+        cy.deferredProjectStateId().should('contain.text', 'Decision recorded')
+        cy.url().then(url => {
+            const id = RecordDecision.getIdFromUrl(url)
+            cy.visit(projectList)
+            cy.get(`[id="project-status-${id}"]`).should('contain.text', 'DEFERRED')
+        })
+    })
+
+    // Edit Deferred Path - None, Other
+    it('TC05: J3 Edit a recorded decision Deferred - None, Other', () => {
+        // Click on change your decision button 
+        cy.changeDecision().should('contain.text', 'Change your decision').click()
+        //select iniital decision
+        cy.deferredRadioBtn().click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // selects regional director button
+        cy.get('[id="none-radio"]').click()
+        // clicks on the continue button
+        cy.continueBtn().click()
+        cy.OtherCheckBox().click()
+            .then(() => cy.OtherCheckText().clear().type('other details'))
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // date entry
+        cy.recordDecisionDate(10, 8, 2022)
+        // clicks on the continue button
+        cy.continueBtn().click()
+        // Change condition
+        cy.deferredReasonChangeLink().click()
+        cy.OtherCheckText().clear().type('other details 2nd time')
+        cy.continueBtn().click()
+        cy.continueBtn().click()
+        // preview answers before submit
+        cy.deferredDecision().should('contain.text', 'Deferred')
+        cy.deferredDecisionMadeBy().should('contain.text', 'None')
+         // check reasons
+         var deferredReasons = cy.get('[id="deferred-reasons"]')
+         deferredReasons.should('contain.text', 'Other:')
+         deferredReasons.should('contain.text', 'other details 2nd time')
+        cy.deferredDecisionDate().should('contain.text', '10 August 2022')
+        // clicks on the record a decision button to submit
+        cy.continueBtn().click()
+        // recorded decision confirmation
+        cy.deferredProjectStateId().should('contain.text', 'Decision recorded')
+        cy.url().then(url => {
+            const id = RecordDecision.getIdFromUrl(url)
+            cy.visit(projectList)
+            cy.get(`[id="project-status-${id}"]`).should('contain.text', 'DEFERRED')
+        })
+    })
+})
