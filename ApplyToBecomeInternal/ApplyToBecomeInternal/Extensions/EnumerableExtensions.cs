61e5db49677ad74038ab23fdaa4e6f6ab1e9d74c
WILDE <Tim.WILDE@EDUCATION.GOV.UK>
2022-11-10 10:57:11 +0000

    Projects updated to .net 6 and nuget packages updated.

--- ApplyToBecomeInternal/ApplyToBecomeInternal/Extensions/EnumerableExtensions.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Extensions/EnumerableExtensions.cs
@@ -4,16 +4,17 @@ using System.Linq;
 
 namespace ApplyToBecomeInternal.Extensions
 {
-	public static class EnumerableExtensions
-	{
-		public static IEnumerable<TSource> DistinctBy<TSource, TKey>
-			(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
-		{
-			HashSet<TKey> seenKeys = new HashSet<TKey>();
-
-			return from TSource element in source
-				   where seenKeys.Add(keySelector(element))
-				   select element;
-		}
-	}
+	 // TODO: Can this be removed in lieu of the .net 6 impl?
+	// public static class EnumerableExtensions
+	// {
+	// 	public static IEnumerable<TSource> DistinctBy<TSource, TKey>
+	// 		(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+	// 	{
+	// 		HashSet<TKey> seenKeys = new HashSet<TKey>();
+	//
+	// 		return from TSource element in source
+	// 			   where seenKeys.Add(keySelector(element))
+	// 			   select element;
+	// 	}
+	// }
 }
