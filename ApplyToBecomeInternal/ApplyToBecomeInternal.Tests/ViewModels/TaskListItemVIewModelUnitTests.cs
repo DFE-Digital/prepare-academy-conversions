e3c56c9c081e1ec3376b086a8b2442879df3accf
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-11-30 15:52:38 +0000

    Adding more tests for TaskListItemViewModel

--- ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/ViewModels/TaskListItemVIewModelUnitTests.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal.Tests/ViewModels/TaskListItemVIewModelUnitTests.cs
@@ -3,13 +3,13 @@ using Xunit;
 
 namespace ApplyToBecomeInternal.Tests.ViewModels
 {
-	public class TaskListItemVIewModelUnitTests
+	public class TaskListItemViewModelUnitTests
 	{
 		[Fact]
 		public void Should_return_true_when_equivalent()
 		{
 			Assert.Multiple(
-				() => Assert.True(TaskListItemViewModel.NotStarted.Equals((object) TaskListItemViewModel.NotStarted)),
+				() => Assert.True(TaskListItemViewModel.NotStarted.Equals((object)TaskListItemViewModel.NotStarted)),
 				() => Assert.True(TaskListItemViewModel.InProgress.Equals((object)TaskListItemViewModel.InProgress)),
 				() => Assert.True(TaskListItemViewModel.Completed.Equals((object)TaskListItemViewModel.Completed))
 			);
@@ -18,10 +18,27 @@ namespace ApplyToBecomeInternal.Tests.ViewModels
 		[Fact]
 		public void Should_return_false_when_not_equivalent()
 		{
-			Assert.Multiple(
-				() => Assert.False(TaskListItemViewModel.NotStarted.Equals((object)TaskListItemViewModel.Completed)),
-				() => Assert.False(TaskListItemViewModel.InProgress.Equals(null))
-			);
+			Assert.False(TaskListItemViewModel.NotStarted.Equals((object)TaskListItemViewModel.Completed));
+		}
+
+		[Fact]
+		public void Should_return_false_when_not_null()
+		{
+			Assert.False(TaskListItemViewModel.InProgress.Equals(null));
+		}
+
+		[Fact]
+		public void Should_return_true_when_equal()
+		{
+			var model = TaskListItemViewModel.NotStarted;
+
+			Assert.True(model.Equals(model));
+		}
+
+		[Fact]
+		public void GetHashCode_should_return_the_same_hashcode_when_objects_are_equivalent()
+		{
+			Assert.Equal(TaskListItemViewModel.NotStarted.GetHashCode(), TaskListItemViewModel.NotStarted.GetHashCode());
 		}
 	}
 }
