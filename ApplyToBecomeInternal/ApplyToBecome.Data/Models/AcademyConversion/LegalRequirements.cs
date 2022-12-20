056438da8ee848b041784e3377e5be288f4c0bb3
dneed-nimble <Dominic.NEED@nimbleapproach.com>
2022-12-20 11:33:58 +0000

    Ensuring the return is able to be parsed to enum

--- ApplyToBecomeInternal/ApplyToBecome.Data/Models/AcademyConversion/LegalRequirements.cs
+++ ApplyToBecomeInternal/ApplyToBecome.Data/Models/AcademyConversion/LegalRequirements.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApplyToBecome.Data.Extensions;
+using System;
 
 namespace ApplyToBecome.Data.Models.AcademyConversion;
 
@@ -13,10 +14,12 @@ public class LegalRequirements
    public static LegalRequirements From(AcademyConversionProject project)
    {
       LegalRequirements legalRequirements = new() { IsComplete = project.LegalRequirementsSectionComplete ?? false };
-      if (Enum.TryParse(project.GoverningBodyResolution, out ThreeOptions governingBodyApproved)) legalRequirements.GoverningBodyApproved = governingBodyApproved;
-      if (Enum.TryParse(project.Consultation, out ThreeOptions consultationDone)) legalRequirements.ConsultationDone = consultationDone;
-      if (Enum.TryParse(project.FoundationConsent, out ThreeOptions foundationConsentDone)) legalRequirements.FoundationConsent = foundationConsentDone;
-      if (Enum.TryParse(project.DiocesanConsent, out ThreeOptions diocesanConsentDone)) legalRequirements.DiocesanConsent = diocesanConsentDone;
+      if (Enum.TryParse(project.GoverningBodyResolution.ToFirstUpper(), out ThreeOptions governingBodyApproved)) legalRequirements.GoverningBodyApproved = governingBodyApproved;
+      if (Enum.TryParse(project.Consultation.ToFirstUpper(), out ThreeOptions consultationDone)) legalRequirements.ConsultationDone = consultationDone;
+      if (Enum.TryParse(project.FoundationConsent.ToFirstUpper(), out ThreeOptions foundationConsentDone)) legalRequirements.FoundationConsent = foundationConsentDone;
+      if (Enum.TryParse(project.DiocesanConsent.ToFirstUpper(), out ThreeOptions diocesanConsentDone)) legalRequirements.DiocesanConsent = diocesanConsentDone;
       return legalRequirements;
    }
+
+
 }
