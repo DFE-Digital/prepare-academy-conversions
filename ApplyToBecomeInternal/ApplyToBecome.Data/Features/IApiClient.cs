62f661fb47ca7569f1564783b1f637e6fc7fad39
dneed-nimble <Dominic.NEED@nimbleapproach.com>
2023-01-05 16:12:46 +0000

    Updated IApiClient to new region approach

--- ApplyToBecomeInternal/ApplyToBecome.Data/Features/IApiClient.cs
+++ ApplyToBecomeInternal/ApplyToBecome.Data/Features/IApiClient.cs
@@ -8,7 +8,6 @@ namespace ApplyToBecome.Data.Features;
 public interface IApiClient
 {
    Task<HttpResponseMessage> GetAllProjectsAsync(AcademyConversionSearchModel searchModel);
-   Task<HttpResponseMessage> GetSelectedRegionsAsync(IEnumerable<string> regions);
    Task<HttpResponseMessage> GetProjectByIdAsync(int id);
    Task<HttpResponseMessage> UpdateProjectAsync(int id, UpdateAcademyConversionProject updateProject);
    Task<HttpResponseMessage> GetFilterParametersAsync();
