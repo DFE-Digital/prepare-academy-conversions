b4fc431184d99e3eb290f539c0ea44490062a5dd
WILDE <Tim.WILDE@EDUCATION.GOV.UK>
2023-01-09 16:56:56 +0000

    Mechanism for the back link tag helper to lookup links provided as overrides.

--- ApplyToBecomeInternal/ApplyToBecomeInternal/Models/Links.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Models/Links.cs
@@ -1,147 +1,162 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace ApplyToBecomeInternal.Models
 {
 	public static class Links
 	{
-		public static class ApplicationForm
-		{
-			public static readonly LinkItem Index = new LinkItem { Page = "/ApplicationForm/Index" };
-		}
+      private static readonly List<LinkItem> _links = new();
+
+      private static LinkItem AddLinkItem(string page, string backText = "Back")
+      {
+         LinkItem item = new() { Page = page, BackText = backText };
+         _links.Add(item);
+         return item;
+      }
+
+      public static LinkItem ByPage(string page) => _links.FirstOrDefault(x => string.Equals(page, x.Page, StringComparison.InvariantCultureIgnoreCase));
+
+      public static class ApplicationForm
+      {
+         public static readonly LinkItem Index = AddLinkItem(page: "/ApplicationForm/Index");
+      }
 
 		public static class ProjectType
-		{
-			public static readonly LinkItem Index = new LinkItem {BackText = "Back to project type", Page = "/ProjectType/Index"};
-		}
+      {
+         public static readonly LinkItem Index = AddLinkItem(backText: "Back to project type", page: "/ProjectType/Index");
+      }
 		public static class ProjectAssignment
-		{
-			public static readonly LinkItem Index = new LinkItem { BackText = "Back to all conversion projects", Page = "/ProjectAssignment/Index" };
-		}
+      {
+         public static readonly LinkItem Index = AddLinkItem(backText: "Back to all conversion projects", page: "/ProjectAssignment/Index");
+      }
 		public static class ProjectList
-		{
-			public static readonly LinkItem Index = new LinkItem { BackText = "Back to all conversion projects", Page = "/ProjectList/Index" };
-		}
+      {
+         public static readonly LinkItem Index = AddLinkItem(backText: "Back to all conversion projects", page: "/ProjectList/Index");
+      }
 
 		public static class ProjectNotes
-		{
-			public static readonly LinkItem Index = new LinkItem { Page = "/ProjectNotes/Index" };
-			public static readonly LinkItem NewNote = new LinkItem { Page = "/ProjectNotes/NewNote" };
-		}
+      {
+         public static readonly LinkItem Index = AddLinkItem(page: "/ProjectNotes/Index");
+         public static readonly LinkItem NewNote = AddLinkItem(page: "/ProjectNotes/NewNote");
+      }
 
 		public static class TaskList
-		{
-			public static readonly LinkItem Index = new LinkItem { BackText = "Back to task list", Page = "/TaskList/Index" };
-			public static readonly LinkItem PreviewHTBTemplate = new LinkItem { BackText = "Back to preview", Page = "/TaskList/PreviewProjectTemplate" };
-			public static readonly LinkItem GenerateHTBTemplate = new LinkItem { Page = "/TaskList/DownloadProjectTemplate" };
-		}
+      {
+         public static readonly LinkItem Index = AddLinkItem(backText: "Back to task list", page: "/TaskList/Index");
+         public static readonly LinkItem PreviewHTBTemplate = AddLinkItem(backText: "Back to preview", page: "/TaskList/PreviewProjectTemplate");
+         public static readonly LinkItem GenerateHTBTemplate = AddLinkItem(page: "/TaskList/DownloadProjectTemplate");
+      }
 
 		public static class SchoolPerformance
-		{
-			public static readonly LinkItem ConfirmSchoolPerformance = new LinkItem { Page = "/TaskList/SchoolPerformance/ConfirmSchoolPerformance" };
-			public static readonly LinkItem AdditionalInformation = new LinkItem { Page = "/TaskList/SchoolPerformance/AdditionalInformation" };
-		}
+      {
+         public static readonly LinkItem ConfirmSchoolPerformance = AddLinkItem(page: "/TaskList/SchoolPerformance/ConfirmSchoolPerformance");
+         public static readonly LinkItem AdditionalInformation = AddLinkItem(page: "/TaskList/SchoolPerformance/AdditionalInformation");
+      }
 
 		public static class SchoolApplicationForm
-		{
-			public static readonly LinkItem Index = new LinkItem { Page = "/ApplicationForm/Index" };
-			public static readonly LinkItem SchoolApplicationTab = new LinkItem { Page = "/ApplicationForm/SchoolApplicationTab" };
-		}
+      {
+         public static readonly LinkItem Index = AddLinkItem(page: "/ApplicationForm/Index");
+         public static readonly LinkItem SchoolApplicationTab = AddLinkItem(page: "/ApplicationForm/SchoolApplicationTab");
+      }
 
 		public static class LocalAuthorityInformationTemplateSection
 		{
-			public static readonly LinkItem ConfirmLocalAuthorityInformationTemplateDates = new LinkItem { Page = "/TaskList/LocalAuthorityInformationTemplate/ConfirmLocalAuthorityInformationTemplateDates" };
-			public static readonly LinkItem RecordLocalAuthorityInformationTemplateDates = new LinkItem { Page = "/TaskList/LocalAuthorityInformationTemplate/RecordLocalAuthorityInformationTemplateDates" };
+			public static readonly LinkItem ConfirmLocalAuthorityInformationTemplateDates = AddLinkItem (page: "/TaskList/LocalAuthorityInformationTemplate/ConfirmLocalAuthorityInformationTemplateDates" );
+			public static readonly LinkItem RecordLocalAuthorityInformationTemplateDates = AddLinkItem (page: "/TaskList/LocalAuthorityInformationTemplate/RecordLocalAuthorityInformationTemplateDates");
 		}
 
 		public static class SchoolAndTrustInformationSection
-		{
-			public static readonly LinkItem ConfirmSchoolAndTrustInformation = new LinkItem { Page = "/TaskList/SchoolAndTrustInformation/ConfirmSchoolAndTrustInformation" };
-			public static readonly LinkItem ProjectRecommendation = new LinkItem { Page = "/TaskList/SchoolAndTrustInformation/ProjectRecommendation" };
-			public static readonly LinkItem Author = new LinkItem { Page = "/TaskList/SchoolAndTrustInformation/Author" };
-			public static readonly LinkItem ClearedBy = new LinkItem { Page = "/TaskList/SchoolAndTrustInformation/ClearedBy" };
-			public static readonly LinkItem AcademyOrderRequired = new LinkItem { Page = "/TaskList/SchoolAndTrustInformation/AcademyOrderRequired" };
-			public static readonly LinkItem HeadTeacherBoardDate = new LinkItem { Page = "/TaskList/SchoolAndTrustInformation/AdvisoryBoardDate" };
-			public static readonly LinkItem PreviousHeadTeacherBoardDateQuestion = new LinkItem { Page = "/TaskList/SchoolAndTrustInformation/PreviousAdvisoryBoard" };
-			public static readonly LinkItem PreviousHeadTeacherBoardDate = new LinkItem { Page = "/TaskList/SchoolAndTrustInformation/PreviousAdvisoryBoardDate" };
-			public static readonly LinkItem ProposedAcademyOpeningDate = new LinkItem { Page = "/TaskList/SchoolAndTrustInformation/ProposedAcademyOpeningDate" };
-			public static readonly LinkItem RouteAndGrant = new LinkItem { Page = "/TaskList/SchoolAndTrustInformation/RouteAndGrant" };
-		}
+      {
+         public static readonly LinkItem ConfirmSchoolAndTrustInformation = AddLinkItem(page: "/TaskList/SchoolAndTrustInformation/ConfirmSchoolAndTrustInformation");
+         public static readonly LinkItem ProjectRecommendation = AddLinkItem(page: "/TaskList/SchoolAndTrustInformation/ProjectRecommendation");
+         public static readonly LinkItem Author = AddLinkItem(page: "/TaskList/SchoolAndTrustInformation/Author");
+         public static readonly LinkItem ClearedBy = AddLinkItem(page: "/TaskList/SchoolAndTrustInformation/ClearedBy");
+         public static readonly LinkItem AcademyOrderRequired = AddLinkItem(page: "/TaskList/SchoolAndTrustInformation/AcademyOrderRequired");
+         public static readonly LinkItem HeadTeacherBoardDate = AddLinkItem(page: "/TaskList/SchoolAndTrustInformation/AdvisoryBoardDate");
+         public static readonly LinkItem PreviousHeadTeacherBoardDateQuestion = AddLinkItem(page: "/TaskList/SchoolAndTrustInformation/PreviousAdvisoryBoard");
+         public static readonly LinkItem PreviousHeadTeacherBoardDate = AddLinkItem(page: "/TaskList/SchoolAndTrustInformation/PreviousAdvisoryBoardDate");
+         public static readonly LinkItem ProposedAcademyOpeningDate = AddLinkItem(page: "/TaskList/SchoolAndTrustInformation/ProposedAcademyOpeningDate");
+         public static readonly LinkItem RouteAndGrant = AddLinkItem(page: "/TaskList/SchoolAndTrustInformation/RouteAndGrant");
+      }
 
 		public static class GeneralInformationSection
-		{
-			public static readonly LinkItem ConfirmGeneralInformation = new LinkItem { Page = "/TaskList/GeneralInformation/ConfirmGeneralInformation" };
-			public static readonly LinkItem PublishedAdmissionNumber = new LinkItem { Page = "/TaskList/GeneralInformation/PublishedAdmissionNumber" };
-			public static readonly LinkItem ViabilityIssues = new LinkItem { Page = "/TaskList/GeneralInformation/ViabilityIssues" };
-			public static readonly LinkItem FinancialDeficit = new LinkItem { Page = "/TaskList/GeneralInformation/FinancialDeficit" };
-			public static readonly LinkItem DistanceFromTrustHeadquarters = new LinkItem { Page = "/TaskList/GeneralInformation/DistanceFromTrustHeadquarters" };
-			public static readonly LinkItem MPDetails = new LinkItem { Page = "/TaskList/GeneralInformation/MPDetails" };
-		}
+      {
+         public static readonly LinkItem ConfirmGeneralInformation = AddLinkItem(page: "/TaskList/GeneralInformation/ConfirmGeneralInformation");
+         public static readonly LinkItem PublishedAdmissionNumber = AddLinkItem(page: "/TaskList/GeneralInformation/PublishedAdmissionNumber");
+         public static readonly LinkItem ViabilityIssues = AddLinkItem(page: "/TaskList/GeneralInformation/ViabilityIssues");
+         public static readonly LinkItem FinancialDeficit = AddLinkItem(page: "/TaskList/GeneralInformation/FinancialDeficit");
+         public static readonly LinkItem DistanceFromTrustHeadquarters = AddLinkItem(page: "/TaskList/GeneralInformation/DistanceFromTrustHeadquarters");
+         public static readonly LinkItem MPDetails = AddLinkItem(page: "/TaskList/GeneralInformation/MPDetails");
+      }
 
 		public static class RationaleSection
-		{
-			public static readonly LinkItem ConfirmProjectAndTrustRationale = new LinkItem { Page = "/TaskList/Rationale/ConfirmProjectAndTrustRationale" };
-			public static readonly LinkItem RationaleForProject = new LinkItem { Page = "/TaskList/Rationale/RationaleForProject" };
-			public static readonly LinkItem RationaleForTrust = new LinkItem { Page = "/TaskList/Rationale/RationaleForTrust" };
-		}
+      {
+         public static readonly LinkItem ConfirmProjectAndTrustRationale = AddLinkItem(page: "/TaskList/Rationale/ConfirmProjectAndTrustRationale");
+         public static readonly LinkItem RationaleForProject = AddLinkItem(page: "/TaskList/Rationale/RationaleForProject");
+         public static readonly LinkItem RationaleForTrust = AddLinkItem(page: "/TaskList/Rationale/RationaleForTrust");
+      }
 
 		public static class RisksAndIssuesSection
-		{
-			public static readonly LinkItem ConfirmRisksAndIssues = new LinkItem { Page = "/TaskList/RisksAndIssues/ConfirmRisksAndIssues" };
-			public static readonly LinkItem RisksAndIssues = new LinkItem { Page = "/TaskList/RisksAndIssues/RisksAndIssues" };
-		}
+      {
+         public static readonly LinkItem ConfirmRisksAndIssues = AddLinkItem(page: "/TaskList/RisksAndIssues/ConfirmRisksAndIssues");
+         public static readonly LinkItem RisksAndIssues = AddLinkItem(page: "/TaskList/RisksAndIssues/RisksAndIssues");
+      }
 
 		public static class LegalRequirements
-		{
-			public static readonly LinkItem Summary = new LinkItem { Page = "/TaskList/LegalRequirements/LegalSummary" };
-			public static readonly LinkItem GoverningBodyResolution = new LinkItem { Page = "/TaskList/LegalRequirements/LegalGoverningBody" };
-			public static readonly LinkItem Consultation = new LinkItem { Page = "/TaskList/LegalRequirements/LegalConsultation" };
-			public static readonly LinkItem DiocesanConsent = new LinkItem { Page = "/TaskList/LegalRequirements/LegalDiocesanConsent" };
-			public static readonly LinkItem FoundationConsent = new LinkItem { Page = "/TaskList/LegalRequirements/LegalFoundationConsent" };
-		}
+      {
+         public static readonly LinkItem Summary = AddLinkItem(page: "/TaskList/LegalRequirements/LegalSummary");
+         public static readonly LinkItem GoverningBodyResolution = AddLinkItem(page: "/TaskList/LegalRequirements/LegalGoverningBody");
+         public static readonly LinkItem Consultation = AddLinkItem(page: "/TaskList/LegalRequirements/LegalConsultation");
+         public static readonly LinkItem DiocesanConsent = AddLinkItem(page: "/TaskList/LegalRequirements/LegalDiocesanConsent");
+         public static readonly LinkItem FoundationConsent = AddLinkItem(page: "/TaskList/LegalRequirements/LegalFoundationConsent");
+      }
 
 		public static class SchoolBudgetInformationSection
-		{
-			public static readonly LinkItem ConfirmSchoolBudgetInformation = new LinkItem { Page = "/TaskList/SchoolBudgetInformation/ConfirmSchoolBudgetInformation" };
-			public static readonly LinkItem UpdateSchoolBudgetInformation = new LinkItem { Page = "/TaskList/SchoolBudgetInformation/UpdateSchoolBudgetInformation" };
-			public static readonly LinkItem AdditionalInformation = new LinkItem { Page = "/TaskList/SchoolBudgetInformation/AdditionalInformation" };
-		}
+      {
+         public static readonly LinkItem ConfirmSchoolBudgetInformation = AddLinkItem(page: "/TaskList/SchoolBudgetInformation/ConfirmSchoolBudgetInformation");
+         public static readonly LinkItem UpdateSchoolBudgetInformation = AddLinkItem(page: "/TaskList/SchoolBudgetInformation/UpdateSchoolBudgetInformation");
+         public static readonly LinkItem AdditionalInformation = AddLinkItem(page: "/TaskList/SchoolBudgetInformation/AdditionalInformation");
+      }
 
 		public static class SchoolPupilForecastsSection
-		{
-			public static readonly LinkItem ConfirmSchoolPupilForecasts = new LinkItem { Page = "/TaskList/SchoolPupilForecasts/ConfirmSchoolPupilForecasts" };
-			public static readonly LinkItem AdditionalInformation = new LinkItem { Page = "/TaskList/SchoolPupilForecasts/AdditionalInformation" };
-		}
+      {
+         public static readonly LinkItem ConfirmSchoolPupilForecasts = AddLinkItem(page: "/TaskList/SchoolPupilForecasts/ConfirmSchoolPupilForecasts");
+         public static readonly LinkItem AdditionalInformation = AddLinkItem(page: "/TaskList/SchoolPupilForecasts/AdditionalInformation");
+      }
 
 		public static class KeyStagePerformanceSection
-		{
-			public static readonly LinkItem KeyStage2PerformanceTables = new LinkItem { Page = "/TaskList/KeyStagePerformance/KeyStage2PerformanceTables" };
-			public static readonly LinkItem KeyStage2PerformanceTablesAdditionalInformation = new LinkItem { Page = "/TaskList/KeyStagePerformance/KeyStage2PerformanceTablesAdditionalInformation" };
-			public static readonly LinkItem KeyStage4PerformanceTables = new LinkItem { Page = "/TaskList/KeyStagePerformance/KeyStage4PerformanceTables" };
-			public static readonly LinkItem KeyStage4PerformanceTablesAdditionalInformation = new LinkItem { Page = "/TaskList/KeyStagePerformance/KeyStage4PerformanceTablesAdditionalInformation" };
-			public static readonly LinkItem KeyStage5PerformanceTables = new LinkItem { Page = "/TaskList/KeyStagePerformance/KeyStage5PerformanceTables" };
-			public static readonly LinkItem KeyStage5PerformanceTablesAdditionalInformation = new LinkItem { Page = "/TaskList/KeyStagePerformance/KeyStage5PerformanceTablesAdditionalInformation" };
+      {
+         public static readonly LinkItem KeyStage2PerformanceTables = AddLinkItem(page: "/TaskList/KeyStagePerformance/KeyStage2PerformanceTables");
+			public static readonly LinkItem KeyStage2PerformanceTablesAdditionalInformation = AddLinkItem (page: "/TaskList/KeyStagePerformance/KeyStage2PerformanceTablesAdditionalInformation");
+         public static readonly LinkItem KeyStage4PerformanceTables = AddLinkItem(page: "/TaskList/KeyStagePerformance/KeyStage4PerformanceTables");
+			public static readonly LinkItem KeyStage4PerformanceTablesAdditionalInformation = AddLinkItem (page: "/TaskList/KeyStagePerformance/KeyStage4PerformanceTablesAdditionalInformation");
+         public static readonly LinkItem KeyStage5PerformanceTables = AddLinkItem(page: "/TaskList/KeyStagePerformance/KeyStage5PerformanceTables");
+			public static readonly LinkItem KeyStage5PerformanceTablesAdditionalInformation = AddLinkItem (page: "/TaskList/KeyStagePerformance/KeyStage5PerformanceTablesAdditionalInformation");
 		}
 
 		public static class Decision
-		{
-			public static readonly LinkItem RecordDecision = new LinkItem { BackText = "Back", Page = "/TaskList/Decision/RecordDecision" };
-			public static readonly LinkItem WhoDecided = new LinkItem { BackText = "Back", Page = "/TaskList/Decision/WhoDecided" };
-			public static readonly LinkItem DeclineReason = new LinkItem { BackText = "Back", Page = "/TaskList/Decision/DeclineReason" };
-			public static readonly LinkItem AnyConditions = new LinkItem { BackText = "Back", Page = "/TaskList/Decision/AnyConditions" };
-			public static readonly LinkItem DecisionDate = new LinkItem { BackText = "Back", Page = "/TaskList/Decision/DecisionDate" };
-			public static readonly LinkItem WhyDeferred = new LinkItem { BackText = "Back", Page = "/TaskList/Decision/WhyDeferred" };
-			public static readonly LinkItem Summary = new LinkItem { BackText = "Back", Page = "/TaskList/Decision/Summary" };
-		}
+      {
+         public static readonly LinkItem RecordDecision = AddLinkItem(backText: "Back", page: "/TaskList/Decision/RecordDecision");
+         public static readonly LinkItem WhoDecided = AddLinkItem(backText: "Back", page: "/TaskList/Decision/WhoDecided");
+         public static readonly LinkItem DeclineReason = AddLinkItem(backText: "Back", page: "/TaskList/Decision/DeclineReason");
+         public static readonly LinkItem AnyConditions = AddLinkItem(backText: "Back", page: "/TaskList/Decision/AnyConditions");
+         public static readonly LinkItem DecisionDate = AddLinkItem(backText: "Back", page: "/TaskList/Decision/DecisionDate");
+         public static readonly LinkItem WhyDeferred = AddLinkItem(backText: "Back", page: "/TaskList/Decision/WhyDeferred");
+         public static readonly LinkItem Summary = AddLinkItem(backText: "Back", page: "/TaskList/Decision/Summary");
+      }
 
 		public static class TrustTemplate
-		{
-			public static readonly LinkItem TrustTemplateGuidance = new LinkItem { Page = "/TaskList/TrustTemplate/TrustTemplateGuidance" };
-		}
+      {
+         public static readonly LinkItem TrustTemplateGuidance = AddLinkItem(page: "/TaskList/TrustTemplate/TrustTemplateGuidance");
+      }
 
 		public static class Public
-		{
-			public static readonly LinkItem Accessibility = new LinkItem { Page = "/Public/AccessibilityStatement" };
-			public static readonly LinkItem CookiePreferences = new LinkItem { Page = "/Public/CookiePreferences" };
-		}
-	}
+      {
+         public static readonly LinkItem Accessibility = AddLinkItem(page: "/Public/AccessibilityStatement");
+         public static readonly LinkItem CookiePreferences = AddLinkItem(page: "/Public/CookiePreferences");
+      }
+      }
 
 	public class LinkItem
 	{
