e507fbdba0e9a4648001f7e219efbc2f7492741a
rdogra-nimble <richika.dogra@nimbleapproach.com>
2023-01-18 13:57:37 +0000

    added more page for accessibility check

--- ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/e2e/Accessibility-check/accessibility-check.cy.js
+++ ApplyToBecomeInternal/ApplyToBecomeCypressTests/cypress/e2e/Accessibility-check/accessibility-check.cy.js
@@ -10,11 +10,17 @@ Cypress._.each(['ipad-mini'], (viewport) => {
     describe('Check accessibility of the different pages', function () {
         conversionLinks.forEach((link) => {
 
-            it('Validate accessibility on different pages', function () {
+            it('Validate accessibility on different pages on Dev env.', function () {
                 let url = Cypress.env('url')
-                cy.visit(url)
-                cy.visit(url + link)
-                cy.excuteAccessibilityTests(wcagStandards, continueOnFail, impactLevel)
+                if (url.toString().includes('dev')) {
+                    cy.visit(url)
+                    cy.visit(url + link)
+                    cy.excuteAccessibilityTests(wcagStandards, continueOnFail, impactLevel)
+                }
+                else {
+                    cy.log('Staging env.') //Accessibility check only at Dev. env. atm
+                    Cypress.runner.stop()
+                }
             })
         })
     })
