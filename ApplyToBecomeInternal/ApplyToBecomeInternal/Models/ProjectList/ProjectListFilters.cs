af6f9afc6c2872498e5c98e777c69fc9a91e968a
WILDE <Tim.WILDE@EDUCATION.GOV.UK>
2023-01-24 11:30:59 +0000

    Specific tests for ProjectListFilters.

--- ApplyToBecomeInternal/ApplyToBecomeInternal/Models/ProjectList/ProjectListFilters.cs
+++ ApplyToBecomeInternal/ApplyToBecomeInternal/Models/ProjectList/ProjectListFilters.cs
@@ -1,7 +1,7 @@
 ﻿#nullable enable
 
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +12,7 @@ public class ProjectListFilters
 {
    private const string FilterTitle = nameof(FilterTitle);
    private const string FilterStatuses = nameof(FilterStatuses);
-   private const string FilterOffices = nameof(FilterOffices);
+   private const string FilterOfficers = nameof(FilterOfficers);
    private const string FilterRegions = nameof(FilterRegions);
 
    private IDictionary<string, object?> _store = null!;
@@ -27,9 +27,9 @@ public class ProjectListFilters
    [BindProperty] public string[] SelectedRegions { get; set; } = Array.Empty<string>();
 
    public bool IsVisible => string.IsNullOrWhiteSpace(Title) is false ||
-                            (SelectedStatuses.Length > 0) ||
-                            (SelectedOfficers.Length > 0) ||
-                            (SelectedRegions.Length > 0);
+                            SelectedStatuses.Length > 0 ||
+                            SelectedOfficers.Length > 0 ||
+                            SelectedRegions.Length > 0;
 
    public ProjectListFilters PersistUsing(IDictionary<string, object?> store)
    {
@@ -37,7 +37,7 @@ public class ProjectListFilters
 
       Title = Get(FilterTitle).FirstOrDefault()?.Trim();
       SelectedStatuses = Get(FilterStatuses);
-      SelectedOfficers = Get(FilterOffices);
+      SelectedOfficers = Get(FilterOfficers);
       SelectedRegions = Get(FilterRegions);
 
       return this;
@@ -55,7 +55,7 @@ public class ProjectListFilters
 
    private string[] Cache(string key, string[]? value)
    {
-      if (Equals(value, default))
+      if (value is null || value.Length == 0)
          _store.Remove(key);
       else
          _store[key] = value;
@@ -63,9 +63,11 @@ public class ProjectListFilters
       return value ?? Array.Empty<string>();
    }
 
-   public void PopulateFrom(IQueryCollection requestQuery)
+   public void PopulateFrom(IEnumerable<KeyValuePair<string, StringValues>> requestQuery)
    {
-      if (requestQuery.ContainsKey("clear"))
+      Dictionary<string, StringValues>? query = new(requestQuery, StringComparer.OrdinalIgnoreCase);
+
+      if (query.ContainsKey("clear"))
       {
          Title = default;
          SelectedStatuses = Array.Empty<string>();
@@ -75,29 +77,29 @@ public class ProjectListFilters
          return;
       }
 
-      bool activeFilterChanges = requestQuery.ContainsKey(nameof(Title)) ||
-                                 requestQuery.ContainsKey(nameof(SelectedStatuses)) ||
-                                 requestQuery.ContainsKey(nameof(SelectedOfficers)) ||
-                                 requestQuery.ContainsKey(nameof(SelectedRegions));
+      bool activeFilterChanges = query.ContainsKey(nameof(Title)) ||
+                                 query.ContainsKey(nameof(SelectedStatuses)) ||
+                                 query.ContainsKey(nameof(SelectedOfficers)) ||
+                                 query.ContainsKey(nameof(SelectedRegions));
 
       if (activeFilterChanges)
       {
-         Title = Cache(FilterTitle, GetFromQuery(nameof(Title)))?.FirstOrDefault()?.Trim();
+         Title = Cache(FilterTitle, GetFromQuery(nameof(Title))).FirstOrDefault()?.Trim();
          SelectedStatuses = Cache(FilterStatuses, GetFromQuery(nameof(SelectedStatuses)));
-         SelectedOfficers = Cache(FilterOffices, GetFromQuery(nameof(SelectedOfficers)));
+         SelectedOfficers = Cache(FilterOfficers, GetFromQuery(nameof(SelectedOfficers)));
          SelectedRegions = Cache(FilterRegions, GetFromQuery(nameof(SelectedRegions)));
       }
       else
       {
-         Title = Get(FilterTitle, persist: true).FirstOrDefault()?.Trim();
-         SelectedStatuses = Get(FilterStatuses, persist: true);
-         SelectedOfficers = Get(FilterOffices, persist: true);
-         SelectedRegions = Get(FilterRegions, persist: true);
+         Title = Get(FilterTitle, true).FirstOrDefault()?.Trim();
+         SelectedStatuses = Get(FilterStatuses, true);
+         SelectedOfficers = Get(FilterOfficers, true);
+         SelectedRegions = Get(FilterRegions, true);
       }
 
       string[] GetFromQuery(string key)
       {
-         return requestQuery.ContainsKey(key) ? requestQuery[key]! : Array.Empty<string>();
+         return query.ContainsKey(key) ? query[key]! : Array.Empty<string>();
       }
    }
 }
