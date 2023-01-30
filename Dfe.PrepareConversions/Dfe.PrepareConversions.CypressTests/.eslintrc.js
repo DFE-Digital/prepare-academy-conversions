f5b30cab24ff69ee258f5e11de29b18ac3317646
Jordan Beard <jordan.beard@nimbleapproach.com>
2023-01-30 17:27:52 +0000

    adding fix to workflow file

--- Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/.eslintrc.js
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
         "no-mixed-spaces-and-tabs": 0, // disable rule
         "no-constant-condition": 0,
         "no-unused-vars": 0,
-        "no-undef": 0,
+        "no-undef": 1,
         "no-useless-escape": 0,
         "cypress/no-assigning-return-values": 0
       }
