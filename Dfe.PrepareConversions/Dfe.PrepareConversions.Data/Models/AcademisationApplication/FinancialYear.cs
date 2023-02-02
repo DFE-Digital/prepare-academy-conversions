ca5b2abefb3180e022e4f30220577bb9506b957e
dneed-nimble <Dominic.NEED@nimbleapproach.com>
2023-02-02 10:19:20 +0000

    Moved classes out of master class

---
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.Data/Models/AcademisationApplication/FinancialYear.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ApplyToBecome.Data.Models.AcademisationApplication;
+
+public class FinancialYear
+{
+   public DateTime FinancialYearEndDate { get; set; }
+   public double Revenue { get; set; }
+   public string RevenueStatus { get; set; }
+   public string RevenueStatusExplained { get; set; }
+   public string RevenueStatusFileLink { get; set; }
+   public double CapitalCarryForward { get; set; }
+   public string CapitalCarryForwardStatus { get; set; }
+   public string CapitalCarryForwardExplained { get; set; }
+}
---
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.Data/Models/AcademisationApplication/JoinTrustDetails.cs
@@ -0,0 +1,16 @@
+﻿namespace ApplyToBecome.Data.Models.AcademisationApplication;
+
+public class JoinTrustDetails
+{
+   public int UKPRN { get; set; }
+
+   public string TrustName { get; set; }
+
+   public string ChangesToTrust { get; set; }
+
+   public string ChangesToTrustExplained { get; set; }
+
+   public bool? ChangesToLaGovernance { get; set; }
+
+   public string ChangesToLaGovernanceExplained { get; set; }
+}
