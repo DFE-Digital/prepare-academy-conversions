172ba64301c155ed15b78976fb356165b50e5a95
Jordan Beard <jordan.beard@nimbleapproach.com>
2023-02-15 09:25:11 +0000

    remove commented out code

--- Dfe.PrepareConversions/Dfe.PrepareConversions/Pages/Shared/_BackLink.cshtml.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/Pages/Shared/_BackLink.cshtml.cs
@@ -1,10 +1,8 @@
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Dfe.PrepareConversions.Pages.TaskList.Decision
 {
-	public class BackLink
+   public class BackLink
 	{
 		public BackLink(string linkPage, Dictionary<string, string> linkRouteValues, string linkText = "Back")
 		{
@@ -16,23 +14,5 @@ namespace Dfe.PrepareConversions.Pages.TaskList.Decision
 		public string LinkPage { get; set; }
 		public Dictionary<string, string> LinkRouteValues { get; set; }
 		public string LinkText { get; set; }
-
-		//public string ToQueryString(string path)
-		//{
-		//	var sb = new StringBuilder(path);
-
-		//	for (int i = 0; i < LinkRouteValues.Count; i++)
-		//	{
-		//		var routeValue = LinkRouteValues.ElementAt(i);
-		//		if (i == 0) sb.Append("?");
-		//		else
-		//		{
-		//			sb.Append("&");
-		//		}
-
-		//		sb.Append($"{routeValue.Key}={routeValue.Value}");
-		//	}
-		//	return sb.ToString();
-		//}
 	}
 }
