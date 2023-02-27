eaae4fcd9e38ac205dbf2e56bc5530293e8ab426
dneed-nimble <Dominic.NEED@nimbleapproach.com>
2023-02-27 11:32:22 +0000

    WasForm7Received added to TrustVM

--- Dfe.PrepareConversions/Dfe.PrepareConversions/ViewModels/SchoolAndTrustInformationViewModel.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/ViewModels/SchoolAndTrustInformationViewModel.cs
@@ -26,6 +26,6 @@ public class SchoolAndTrustInformationViewModel
    public string Form7Received { get; set; }
    public string Form7ReceivedDate { get; set; }
     public bool IsDao { get; set; }
-    public bool WasForm7Recieved { get; set; }
+    public bool WasForm7Received { get; set; }
 
 }
