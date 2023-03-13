27f81dc45e393399ea54cf46e6dc477384fc930a
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2023-03-13 15:38:56 +0000

    Add DNS Zone deployment option

--- terraform.gpaas-azure-migration/container-apps-hosting.tf
+++ terraform.gpaas-azure-migration/container-apps-hosting.tf
@@ -10,6 +10,9 @@ module "azure_container_apps_hosting" {
 
   enable_container_registry = local.enable_container_registry
 
+  enable_dns_zone      = local.enable_dns_zone
+  dns_zone_domain_name = local.dns_zone_domain_name
+
   enable_event_hub = local.enable_event_hub
 
   image_name                             = local.image_name
