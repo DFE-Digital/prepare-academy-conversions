eff74ab181004c4a528ccbbd578529a45a42f2b5
rdogra-nimble <richika.dogra@nimbleapproach.com>
2023-03-17 14:39:09 +0000

    create and verify Form a MAT project on FE

--- Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/Form-a-MAT/add-form-a-mat-project.cy.js
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/Form-a-MAT/add-form-a-mat-project.cy.js
@@ -1,43 +1,86 @@
-let APPLICATION_ID = 105;
+let APPLICATION_ID = 31;
 let URN_ID = null;
 
-describe('Add Form-a-MAT application using cypress-plugin-api', () => {
+describe('Add Form-a-MAT application through API request and verify on the frontend', () => {
+    beforeEach(() => {
+		cy.sqlServer(`UPDATE [academisation].[ConversionApplication]
+		SET [academisation].[ConversionApplication].ApplicationStatus = 'InProgress'
+		SELECT TOP (1000) [Id]
+			  ,[ApplicationType]
+			  ,[CreatedOn]
+			  ,[LastModifiedOn]
+			  ,[ApplicationStatus]
+			  ,[FormTrustId]
+			  ,[JoinTrustId]
+			  ,[ApplicationSubmittedDate]
+			  ,[DynamicsApplicationId]
+			  ,[ApplicationReference]
+		  FROM [academisation].[ConversionApplication]
+		  WHERE [academisation].[ConversionApplication].ApplicationReference = 'A2B_31'`);
+	  });
+
 	it('TC01: should create a Form-a-MAT project using Api', () => {
-		cy.api({
+		cy.request({
 			method: 'POST',
 			url: `application/${APPLICATION_ID}/submit`,
+		response: [],
 		}).then((response) => {
+			console.log(response.body)
+            assert.equal(response.status, 201)
+            expect(response.body).to.not.be.null
+            console.log(response)
+            expect(response.body).to.be.a('array')
             expect(response.status).to.equal(201);
-			expect(response.body.academyTypeAndRoute).to.equal('Form a Mat');
-			expect(response.body).to.have.property('schoolName');
-            expect(response.body).to.have.property('urn');
-            URN_ID = response.body.urn;
-            expect(response.body).to.have.property('projectStatus');
-            expect(response.body).to.have.property('nameOfTrust');
+			expect(response.body[0]).to.have.property('academyTypeAndRoute');
+			expect(response.body[0].academyTypeAndRoute).to.equal('Form a Mat');
+			expect(response.body[0]).to.have.property('schoolName');
+            expect(response.body[0]).to.have.property('urn');
+            URN_ID = response.body[0].urn;
+			console.log(URN_ID)
+            expect(response.body[0]).to.have.property('projectStatus');
+           // expect(response.body[0]).to.have.property('nameOfTrust');
+		   expect(response.body[1]).to.have.property('academyTypeAndRoute');
+		   expect(response.body[1].academyTypeAndRoute).to.equal('Form a Mat');
+		   expect(response.body[1]).to.have.property('schoolName');
+		   expect(response.body[1]).to.have.property('urn');
+		});
+
+        //TC02: should NOT create a Form a MAT project if it's NOT in Progress to submit
+		cy.api({
+			method: 'POST',
+			url: `application/${APPLICATION_ID}/submit`, failOnStatusCode: false,
+			response: [],
+		}).then((response) => {
+            expect(response.status).to.equal(400);
+			expect(response.body[0]).to.have.property('errorMessage');
+			expect(response.body[0].errorMessage).to.equal('Application must be In Progress to submit');
 		});
 	});
 
-	it('TC02: should create a Form a MAT project on the FE', () => {
+	it('TC03: should create a Form a MAT project on the FE', () => {
         cy.login()
         cy.get('[data-cy="select-projectlist-filter-row"]').first().should('be.visible')
         .invoke('text')
         .then((text) => {
           if (text.includes('Route: Form a MAT')) {
-            cy.get('#urn-0').should('contain', `URN: '${URN_ID}'`)
+		   cy.get('#urn-0').invoke('text')
+			 .should('match', /\D+\d+/);
+		   cy.get('#urn-0').contains('URN: ')
+		   cy.get('#application-to-join-trust-0').contains('Application to join a trust: ')
+		   cy.get('#application-received-date-0').contains('Project created date: ')
+		   cy.get('#opening-date-0').contains('Opening date: ')
+		   cy.get('#school-name-0').click()
+		   cy.url().should('include', 'form-a-mat')
+		   //check school application form
+		   cy.get('[class="moj-sub-navigation__link"]').contains('School application form')
+			 . click()
+		   //check Other schools in this MAT
+		   cy.get('[class="moj-sub-navigation__link"]').contains('Other schools in this MAT')
+		   . click()
           }
           else {
-            cy.log('this is not involuntary project')
+            cy.log('this is not Form a MAT project')
           }
         });
-	});
-
-	it('TC03: should give an error when submitted same application twice', () => {
-		cy.api({
-			method: 'POST',
-			url: `application/${APPLICATION_ID}/submit`,
-		}).then((response) => {
-            expect(response.status).to.equal(400);
-			expect(response.body.errorMessage).to.equal('Form Application must be In Progress to submit Mat');
-		});
 	});
 });
