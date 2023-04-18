ec7ed29962dae1429bd2e374494ad12153a8fbe7
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2023-04-18 13:45:19 +0100

    Add option to override Front Door CDN Origin

--- terraform.gpaas-azure-migration/locals.tf
+++ terraform.gpaas-azure-migration/locals.tf
@@ -15,6 +15,7 @@ locals {
   dns_ns_records                               = var.dns_ns_records
   dns_txt_records                              = var.dns_txt_records
   enable_cdn_frontdoor                         = var.enable_cdn_frontdoor
+  cdn_frontdoor_origin_fqdn_override           = var.cdn_frontdoor_origin_fqdn_override
   cdn_frontdoor_enable_rate_limiting           = var.cdn_frontdoor_enable_rate_limiting
   cdn_frontdoor_host_add_response_headers      = var.cdn_frontdoor_host_add_response_headers
   cdn_frontdoor_custom_domains                 = var.cdn_frontdoor_custom_domains
