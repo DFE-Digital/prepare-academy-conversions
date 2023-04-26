89411bae2250f5c24b55d6c5a2d0a47b3e0350eb
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2023-04-26 15:25:56 +0100

    Disassociate logit from app

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,41 +1,41 @@
 resource cloudfoundry_app worker_app {
   name               = local.web_app_name
   space              = data.cloudfoundry_space.space.id
-	docker_image			 = local.docker_image
-	strategy           = "blue-green-v3"
+  docker_image			 = local.docker_image
+  strategy           = "blue-green-v3"
 
-  service_binding {
-    service_instance = cloudfoundry_user_provided_service.logit.id
+  # service_binding {
+  #   service_instance = cloudfoundry_user_provided_service.logit.id
+  # }
+
+  routes {
+    route = cloudfoundry_route.web_app_cloudapp_digital_route.id
   }
 
-	routes {
-		route = cloudfoundry_route.web_app_cloudapp_digital_route.id
-	}
-
-	environment = {
-		"ASPNETCORE_ENVIRONMENT"              = var.aspnetcore_environment
-		"ASPNETCORE_URLS"                     = "http://+:8080"
-		"TramsApi__Endpoint"                  = var.app_trams_api_endpoint
-		"TramsApi__ApiKey"                    = var.app_trams_api_key
-		"TramsApi__ApiKey"                    = var.app_trams_api_key
-		"AcademisationApi__BaseUrl"           = var.app_academisation_api_baseurl
-		"AcademisationApi__ApiKey"            = var.app_academisation_api_apikey
-		"ServiceLink__TransfersUrl"           = var.app_servicelink_transfersurl
-		"GoogleAnalytics__Enable"             = var.enable_google_analytics
-		"FeedbackLink"                        = var.app_feedback_link
-		"SupportEmail"                        = var.app_support_email
-		"SENTRY_RELEASE"                      = "a2b-internal:${var.cf_app_image_tag}"
-		"AzureAd__ClientSecret"               = var.app_azuread_clientsecret
-		"AzureAd__ClientId"                   = var.app_azuread_clientid
-		"AzureAd__TenantId"                   = var.app_azuread_tenantid
-		"AzureAd__GroupId"                    = var.app_azuread_groupid
-		"CypressTestSecret"                   = var.app_cypresstest_secret
-      "FeatureManagement__UseAcademisation" = var.app_feature_use_academisation
-      "FeatureManagement__UseAcademisationApplication" = var.app_feature_use_academisation_application
-	  "FeatureManagement__ShowDirectedAcademyOrders" = var.app_feature_show_directedacademyorders
-      "AccessRequest"                       = var.app_accessrequest
-      "TemplateFeedback"                    = var.app_templatefeedback
-	}
+  environment = {
+    "ASPNETCORE_ENVIRONMENT"              = var.aspnetcore_environment
+    "ASPNETCORE_URLS"                     = "http://+:8080"
+    "TramsApi__Endpoint"                  = var.app_trams_api_endpoint
+    "TramsApi__ApiKey"                    = var.app_trams_api_key
+    "TramsApi__ApiKey"                    = var.app_trams_api_key
+    "AcademisationApi__BaseUrl"           = var.app_academisation_api_baseurl
+    "AcademisationApi__ApiKey"            = var.app_academisation_api_apikey
+    "ServiceLink__TransfersUrl"           = var.app_servicelink_transfersurl
+    "GoogleAnalytics__Enable"             = var.enable_google_analytics
+    "FeedbackLink"                        = var.app_feedback_link
+    "SupportEmail"                        = var.app_support_email
+    "SENTRY_RELEASE"                      = "a2b-internal:${var.cf_app_image_tag}"
+    "AzureAd__ClientSecret"               = var.app_azuread_clientsecret
+    "AzureAd__ClientId"                   = var.app_azuread_clientid
+    "AzureAd__TenantId"                   = var.app_azuread_tenantid
+    "AzureAd__GroupId"                    = var.app_azuread_groupid
+    "CypressTestSecret"                   = var.app_cypresstest_secret
+    "FeatureManagement__UseAcademisation" = var.app_feature_use_academisation
+    "FeatureManagement__UseAcademisationApplication" = var.app_feature_use_academisation_application
+    "FeatureManagement__ShowDirectedAcademyOrders" = var.app_feature_show_directedacademyorders
+    "AccessRequest"                       = var.app_accessrequest
+    "TemplateFeedback"                    = var.app_templatefeedback
+  }
 }
 
 resource cloudfoundry_route web_app_cloudapp_digital_route {
