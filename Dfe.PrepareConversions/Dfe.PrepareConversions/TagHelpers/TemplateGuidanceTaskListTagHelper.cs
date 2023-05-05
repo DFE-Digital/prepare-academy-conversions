575e777ef98dc2b6642b637fbc324e0d5fead8a1
dneed-nimble <Dominic.NEED@nimbleapproach.com>
2023-05-05 16:22:01 +0100

    Refactored TagHelper for generic html

--- Dfe.PrepareConversions/Dfe.PrepareConversions/TagHelpers/TemplateGuidanceTaskListTagHelper.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/TagHelpers/TemplateGuidanceTaskListTagHelper.cs
@@ -11,40 +11,27 @@ namespace Dfe.PrepareConversions.TagHelpers
       public override void Process(TagHelperContext context, TagHelperOutput output)
       {
          string urlSlug = $"{ProjectId}/template-guidance";
-         if (IsSponsored)
-         {
-            output.Content.SetHtmlContent($@"
-                <h3 class='app-task-list__section govuk-!-margin-top-8'>Sponsor template guidance</h3>
+         output.Content.SetHtmlContent(IsSponsored
+            ? GetHtmlContent("Sponsor template guidance", urlSlug, "Prepare your sponsor template")
+            : GetHtmlContent("Trust template guidance", urlSlug, "Prepare your trust template"));
+      }
+
+      private static string GetHtmlContent(string title, string urlSlug, string linkText)
+      {
+         return $@"
+                <h3 class='app-task-list__section govuk-!-margin-top-8'>{title}</h3>
                 <div class='app-task-list'>
                     <ul class='app-task-list__items govuk-!-padding-left-0'>
                         <li class='app-task-list__item'>
                             <span class='app-task-list__task-name'>
                                 <a class='govuk-link' href='{urlSlug}' aria-describedby='trust-template-status'>
-                                    Prepare your sponsor template
+                                    {linkText}
                                 </a>
                             </span>
                             <strong class='govuk-tag govuk-tag--grey app-task-list__tag' id='trust-template-status'>Reference only</strong>
                         </li>
                     </ul>
-                </div>");
-         }
-         else
-         {
-            output.Content.SetHtmlContent($@"
-                <h3 class='app-task-list__section govuk-!-margin-top-8'>Trust template guidance</h3>
-                <div class='app-task-list'>
-                    <ul class='app-task-list__items govuk-!-padding-left-0'>
-                        <li class='app-task-list__item'>
-                            <span class='app-task-list__task-name'>
-                                <a class='govuk-link' href='{urlSlug}' aria-describedby='trust-template-status'>
-                                    Prepare your trust template
-                                </a>
-                            </span>
-                            <strong class='govuk-tag govuk-tag--grey app-task-list__tag' id='trust-template-status'>Reference only</strong>
-                        </li>
-                    </ul>
-                </div>");
-         }
+                </div>";
       }
    }
 }
