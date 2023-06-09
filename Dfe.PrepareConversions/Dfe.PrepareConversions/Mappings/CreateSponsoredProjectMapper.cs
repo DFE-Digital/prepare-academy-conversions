ee2468bfa0f113edb9344e303b533f6c7c32b98f
Chris Dexter <109349584+chrisdexnimble@users.noreply.github.com>
2023-06-09 16:55:21 +0100

    Added local authority and region to the model sent from conversions when creating an involuntary conversion. This will store the two values in the database immediately, and if not null then the background enrichment task in the academisation api will skip over them.

---
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/Mappings/CreateSponsoredProjectMapper.cs
@@ -0,0 +1,28 @@
+﻿using Dfe.PrepareConversions.Data.Models.Establishment;
+using Dfe.PrepareConversions.Data.Models.SponsoredProject;
+using Dfe.PrepareConversions.Data.Models.Trust;
+using System;
+
+namespace Dfe.PrepareConversions.Mappings;
+
+public class CreateSponsoredProjectMapper
+{
+   public static CreateSponsoredProject MapToDto(EstablishmentResponse establishment, TrustDetail trust)
+   {
+      var partOfPfiScheme = !string.IsNullOrWhiteSpace(establishment.ViewAcademyConversion?.Pfi)
+                            && establishment.ViewAcademyConversion?.Pfi.Equals("No", StringComparison.InvariantCultureIgnoreCase) == false;
+      
+      SponsoredProjectSchool createSchool = new(
+         establishment.EstablishmentName,
+         establishment.Urn,
+         partOfPfiScheme,
+         establishment.LocalAuthorityName,
+         establishment.Gor.Name);
+         
+      SponsoredProjectTrust createTrust = new(
+         trust.GiasData.GroupName,
+         trust.GiasData.GroupId);
+
+      return new CreateSponsoredProject(createSchool, createTrust);
+   }
+}
