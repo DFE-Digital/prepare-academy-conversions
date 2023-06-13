bf5d7fe5ab0985aa72944c64f713667058a775e5
Chris Dexter <109349584+chrisdexnimble@users.noreply.github.com>
2023-06-13 11:59:22 +0100

    put string extension back to old code now pipline test done

--- Dfe.PrepareConversions/Dfe.PrepareConversions.Data/Extensions/StringExtensions.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.Data/Extensions/StringExtensions.cs
@@ -1,11 +1,9 @@
 ﻿namespace Dfe.PrepareConversions.Data.Extensions;
-using Dfe.Academisation.ExtensionMethods;
 public static class StringExtensions
 {
    public static string ToFirstUpper(this string input)
    {
-      // Intentionally just passing on the method call so that connectivity to the nuget server can be proven
-
-      return Dfe.Academisation.ExtensionMethods.StringExtensions.ToFirstUpper(input);
+      string lowered = input.ToLower();
+      return $"{char.ToUpper(lowered[0])}{lowered[1..]}";
    }
 }
