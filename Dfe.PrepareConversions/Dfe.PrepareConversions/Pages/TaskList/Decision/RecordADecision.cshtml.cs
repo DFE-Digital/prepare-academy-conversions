e4bea8128b4b6829d1ce0a1be66a5e8cebb2b763
Dominic Need <dominic.need@nimbleapproach.com>
2023-07-21 10:22:04 +0100

    UI Elements of 134597 complete

---
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/Pages/TaskList/Decision/RecordADecision.cshtml.cs
@@ -0,0 +1,13 @@
+using Dfe.PrepareConversions.Data.Services;
+
+namespace Dfe.PrepareConversions.Pages.TaskList.Decision
+{
+   public class IndexModel : BaseAcademyConversionProjectPageModel
+   {
+      public IndexModel(IAcademyConversionProjectRepository repository) : base(repository)
+      {
+      }
+   }
+}
+
+
