1922e5c535a2ebfb03fb5f620347a2fcda0e6394
GOOD <Dan.GOOD@EDUCATION.GOV.UK>
2023-08-02 16:30:28 +0100

    Adding page-objects directory above pages directory to make that consistent with A2B

---
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/page-objects/pages/legacy/projectList.js
@@ -0,0 +1,22 @@
+class ProjectList {
+
+    selectProject() {
+        cy.login({titleFilter: 'Gloucester school'});
+        cy.get('[id="school-name-0"]').click();
+
+        return cy.url().then(url => this.getIdFromUrl(url));
+    };
+
+    getIdFromUrl(url) {
+        const urlSplit = url.toString().split('/');
+        for (let i = urlSplit.length - 1; i > 0; i--) {
+            const potentialId = parseInt(urlSplit[i]);
+
+            if (!isNaN(potentialId)) return potentialId;
+        }
+
+        return '';
+    };
+};
+
+export default new ProjectList();
