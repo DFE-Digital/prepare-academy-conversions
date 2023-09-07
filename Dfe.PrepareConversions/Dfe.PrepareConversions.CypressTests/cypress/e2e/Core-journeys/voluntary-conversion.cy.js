2714804809337b3ba969aa4f132f6fd3fea192a8
GOOD <Dan.GOOD@EDUCATION.GOV.UK>
2023-09-07 11:36:06 +0100

    In beforeEach() block in Voluntary change the way of accessing the project to be Page-Object-friendly with the existing projectList page object as Nick has done

--- Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/Core-journeys/voluntary-conversion.cy.js
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/Core-journeys/voluntary-conversion.cy.js
@@ -1,17 +1,18 @@
 /// <reference types ='Cypress'/>
+import projectList from '../../pages/projectList'
+
+const projectName = 'Voluntary Cypress Project'
 
 describe('Voluntary conversion', { tags: ['@dev', '@stage'] }, () => {
    beforeEach(() => {
       cy.callAcademisationApi('POST', `cypress-data/add-voluntary-project.cy`, "{}")
          .then(() => {
-            cy.login({ titleFilter: 'Cypress Project' })
-               .then(() => {
-                  cy.get('[id="school-name-0"]').click();                  
-               });
+            projectList.selectProject(projectName)
          });
    })
 
    it('TC01: Voluntary conversion journey ', () => {
     // Go wild Dan!
+
    })
 })
