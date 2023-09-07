a44c6f9a1b1e766362b878da34811f13da3a8b9f
GOOD <Dan.GOOD@EDUCATION.GOV.UK>
2023-09-07 11:59:59 +0100

    change function call in beforeEach() block in voluntary-conversion.cy.js spec to projectList.selectVoluntaryProject(projectName) so correct voluntary project is being selected

--- Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/Core-journeys/voluntary-conversion.cy.js
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/Core-journeys/voluntary-conversion.cy.js
@@ -1,18 +1,19 @@
 /// <reference types ='Cypress'/>
 import projectList from '../../pages/projectList'
 
-const projectName = 'Voluntary Cypress Project'
+var projectName = 'Voluntary Cypress Project'
 
 describe('Voluntary conversion', { tags: ['@dev', '@stage'] }, () => {
    beforeEach(() => {
       cy.callAcademisationApi('POST', `cypress-data/add-voluntary-project.cy`, "{}")
          .then(() => {
-            projectList.selectProject(projectName)
+            projectList.selectVoluntaryProject(projectName)
          });
    })
 
    it('TC01: Voluntary conversion journey ', () => {
     // Go wild Dan!
+
 
    })
 })
