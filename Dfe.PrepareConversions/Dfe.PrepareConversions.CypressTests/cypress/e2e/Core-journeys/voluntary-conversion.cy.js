f0bd3d77e97431bded688e4eef8781dffee83f40
GOOD <Dan.GOOD@EDUCATION.GOV.UK>
2023-09-07 17:51:08 +0100

    Attempted to change call from selectVoluntaryProject to selectProject to use Nicks function

--- Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/Core-journeys/voluntary-conversion.cy.js
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/Core-journeys/voluntary-conversion.cy.js
@@ -8,7 +8,7 @@ describe('Voluntary conversion', { tags: ['@dev', '@stage'] }, () => {
    beforeEach(() => {
       cy.callAcademisationApi('POST', `cypress-data/add-voluntary-project.cy`, "{}")
          .then(() => {
-            projectList.selectVoluntaryProject(projectName)
+            projectList.selectProject(projectName)
          });
    })
 
