92f9919e91bff0e30f70f19049f4ec08efea9dde
GOOD <Dan.GOOD@EDUCATION.GOV.UK>
2023-09-07 12:28:27 +0100

    Started to add detail to voluntaryProjectElementsVisible() function in voluntaryProjectTaskList page object

--- Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/pages/voluntaryProjectTaskList.js
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/pages/voluntaryProjectTaskList.js
@@ -1,8 +1,24 @@
+var projectName = 'Voluntary Cypress Project'
+var routeType = 'Voluntary conversion'
+var projectOwner = ''
+var projectStatus = 'Approved with Conditions'
+
 class VoluntaryProjectTaskList {
-    var projectName = 'Voluntary Cypress Project'
+    
     static voluntaryProjectElementsVisible()
     {
+        // Check Header section of main Project Page
+        cy.get('h1[data-cy]').contains(projectName)
+        cy.get('p[class="govuk-body govuk-!-margin-bottom-1"]').contains('Route: ' + routeType)
+        cy.get('p[class="govuk-body govuk-!-margin-bottom-5"]').contains('Project owner: ' + projectOwner)
+        cy.get('.empty').contains('Empty')
+        cy.get('a[class="govuk-link govuk-!-padding-left-50"]').contains('Change')
+        cy.get('#project-status-28470').contains(projectStatus)
+
+        // Check Project Tabs Section of main Project Page
+        cy.get('ul[class="moj-sub-navigation__list govuk-!-margin-top-6"]').should('have.length',5)
         
+
     }
 }
 
