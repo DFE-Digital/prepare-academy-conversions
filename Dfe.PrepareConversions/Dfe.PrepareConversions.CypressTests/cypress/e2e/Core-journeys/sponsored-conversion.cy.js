ac38d9a0b9e25d2ee35afe50b80fda3b01faba55
Nick Warms <Nicholas.WARMS@education.gov.uk>
2023-09-07 16:24:50 +0100

    Start the school overview tests

--- Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/Core-journeys/sponsored-conversion.cy.js
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/Core-journeys/sponsored-conversion.cy.js
@@ -3,6 +3,7 @@
 import projectList from "../../pages/projectList";
 import projectTaskList from "../../pages/projectTaskList";
 import projectAssignment from "../../pages/projectAssignment";
+import schoolOverview from "../../pages/schoolOverview";
 
 const projectName = 'Sponsored Cypress Project';
 const deliveryOfficer = 'Richika Dogra';
@@ -16,13 +17,28 @@ describe('Sponsored conversion', { tags: ['@dev', '@stage'] }, () => {
    })
 
    it('TC01: Sponsored conversion journey ', () => {
+      
       // Assign Delivery Officer
-
       projectTaskList.selectAssignProject();
       projectAssignment.assignProject(deliveryOfficer)
       projectTaskList.getNotificationMessage().should('contain.text', 'Project is assigned');
       projectTaskList.getAssignedUser().should('contain.text', deliveryOfficer);
       projectList.filterProjectList(projectName);
       projectList.getNthProjectDeliveryOfficer().should('contain.text', deliveryOfficer);
+
+      // School Overview
+      projectList.selectProject(projectName);
+      projectTaskList.selectSchoolOverview();
+      schoolOverview.changePanNumber('98765');
+      schoolOverview.getPan().should('contain.text', '98765');
+      //Viability issues
+      schoolOverview.changeViabilityIssues(true);
+      schoolOverview.getViabilityIssues().should('contain.text', 'Yes');
+      //Financial deficit
+      
+      //PFI + details
+      //Distance plus details
+      //MP
+      
    })
 })
