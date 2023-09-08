26acd59eebf8540dca5106cebeda8ba6b01c70bb
Nick Warms <Nicholas.WARMS@education.gov.uk>
2023-09-08 16:26:00 +0100

    Switch to using static

--- Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/pages/projectList.js
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/pages/projectList.js
@@ -1,40 +1,40 @@
 /// <reference types ='Cypress'/>
 
-export const path = 'project-list';
+export default class ProjectList {
 
-class ProjectList {
+    static path = 'project-list';
 
-    checkProjectListPage() {
-        cy.url().should('include', path);
+    static checkProjectListPage() {
+        cy.url().should('include', this.path);
     }
 
-    getNthProject(n = 0){
+    static getNthProject(n = 0) {
         this.checkProjectListPage();
         return cy.get(`[id="school-name-${n}"]`);
     }
 
-    getNthProjectDeliveryOfficer(n = 0){
+    static getNthProjectDeliveryOfficer(n = 0) {
         this.checkProjectListPage();
         return cy.get(`[id="delivery-officer-${n}"]`);
     }
 
-    filterProjectList( titleFilter ) {
+    static filterProjectList(titleFilter) {
         const filterQuery = `?Title=${encodeURIComponent(titleFilter)}`;
-        cy.visit(`${Cypress.env('url')}/${path}${filterQuery}`)
+        cy.visit(`${Cypress.env('url')}/${this.path}${filterQuery}`)
     };
 
-    selectFirstItem(){
+    static selectFirstItem() {
         this.checkProjectListPage();
         this.getNthProject().click();
     }
 
-    selectProject(projectName = 'Gloucester school') {
+    static selectProject(projectName = 'Gloucester school') {
         this.filterProjectList(projectName);
         this.selectFirstItem();
         return cy.url().then(url => this.getIdFromUrl(url));
     };
 
-    getIdFromUrl(url) {
+    static getIdFromUrl(url) {
         const urlSplit = url.toString().split('/');
         for (let i = urlSplit.length - 1; i > 0; i--) {
             const potentialId = parseInt(urlSplit[i]);
@@ -45,5 +45,3 @@ class ProjectList {
         return '';
     };
 };
-
-export default new ProjectList();
