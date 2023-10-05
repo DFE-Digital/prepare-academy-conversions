860d9468a8d998bf18f69e97525b0fca50283248
Chris Sherlock <chris.sherlock@nimbleapproach.com>
2023-10-05 15:32:17 +0100

    First basic working Pact consumer test

--- Dfe.PrepareConversions/Dfe.PrepareConversions.Contracts.Tests/AcademisationApiContractTests.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.Contracts.Tests/AcademisationApiContractTests.cs
@@ -7,7 +7,11 @@ using Dfe.PrepareConversions.Data.Services;
 using Dfe.PrepareConversions.Data.Tests.TestDoubles;
 using RichardSzalay.MockHttp;
 using Dfe.Academisation.CorrelationIdMiddleware;
-using static System.Net.WebRequestMethods;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using Xunit.Abstractions;
+using PactNet.Infrastructure.Outputters;
 
 namespace Dfe.PrepareConversions.Contracts.Tests
 {
@@ -16,17 +20,35 @@ namespace Dfe.PrepareConversions.Contracts.Tests
       private readonly IPactBuilderV3 pactBuilder;
       private readonly Mock<IFeatureManager> featureManager;
       private readonly PathFor pathFor;
+      private readonly ITestOutputHelper _output;
 
-      public AcademisationApiContractTests()
+      public AcademisationApiContractTests(ITestOutputHelper output)
       {
          featureManager = new();
          featureManager.Setup(m => m.IsEnabledAsync("UseAcademisationApplication")).ReturnsAsync(true);
          pathFor = new PathFor(featureManager.Object);
+         _output = output;
+
+         var config = new PactConfig
+         {
+            PactDir = @"../../../pacts",
+            Outputters = new List<IOutput>
+            { 
+               new XunitOutput(_output),
+               new ConsoleOutput()
+            },
+            DefaultJsonSettings = new JsonSerializerSettings
+            {
+               ContractResolver = new CamelCasePropertyNamesContractResolver(),
+               Converters = new JsonConverter[] { new StringEnumConverter() }
+            },
+            LogLevel = PactLogLevel.Debug
+         };
 
          var pact = Pact.V3(
             "prepare-academy-conversions",
             "academies-academisation-api",
-            new PactConfig());
+            config);
 
          pactBuilder = pact.WithHttpInteractions();
       }
@@ -34,33 +56,36 @@ namespace Dfe.PrepareConversions.Contracts.Tests
       [Fact]
       public async Task GetApplicationByReference_ShouldReturnApplication()
       {
-         var mockHttpClientFactory = Mock.Of<IHttpClientFactory>(x => x.CreateClient(DfeHttpClientFactory.AcademisationClientName) == new MockHttpClientFactory(new MockHttpMessageHandler(BackendDefinitionBehavior.Always)).CreateClient(DfeHttpClientFactory.AcademisationClientName));
-         var mockCorrelationId = Guid.NewGuid();
-         var mockCorrelationContext = Mock.Of<ICorrelationContext>(x => x.CorrelationId == mockCorrelationId);
-
-         var sut = new DfeHttpClientFactory(mockHttpClientFactory, mockCorrelationContext);
+         var responseBody = new List<object>()
+         {
+            new { foo = "bar" }
+         };
 
          // Arrange
          pactBuilder
-            .UponReceiving("A GET request for an Application by Reference")
-            .Given("An application exists")
+            .UponReceiving("A GET request for an Application Reference by ID")
+               .Given("An application with ID 999999 exists")
                .WithRequest(HttpMethod.Get, "/application/999999/applicationReference")
                //.WithHeader("x-api-key", "foobar")
-               .WithHeader("x-correlationid", mockCorrelationId.ToString())
-               .WithHeader("Accept", PactNet.Matchers.Match.Regex(".*", "application/json"))
+               //.WithHeader("x-correlationid", PactNet.Matchers.Match.Include(mockCorrelationId.ToString()))
+               //.WithHeader("Accept", PactNet.Matchers.Match.Regex(".*", "application/json"))
                //.WithHeader("Content-Type", "application/json; charset=utf-8")
             .WillRespond()
                .WithStatus(HttpStatusCode.OK)
                .WithHeader("Content-Type", "application/json; charset=utf-8")
-               .WithJsonBody(new
-               {
-                  foo = "bar"
-               });
+               .WithJsonBody(responseBody[0]);
 
          await pactBuilder.VerifyAsync(async ctx =>
          {
+            var mockHttpClientFactory = new Mock<IHttpClientFactory>();
+            mockHttpClientFactory.Setup(x => x.CreateClient(DfeHttpClientFactory.AcademisationClientName)).Returns(Mock.Of<HttpClient>(x => x.BaseAddress == ctx.MockServerUri));
+            
+            var mockCorrelationId = Guid.NewGuid();
+            var mockCorrelationContext = Mock.Of<ICorrelationContext>(x => x.CorrelationId == mockCorrelationId);
+
+            var sut = new DfeHttpClientFactory(mockHttpClientFactory.Object, mockCorrelationContext);
+
             // Act
-            sut.CreateAcademisationClient().BaseAddress = ctx.MockServerUri;
             var client = new ApiClient(sut, featureManager.Object, pathFor);
             var res = await client.GetApplicationByReferenceAsync("999999");
             
---
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.Contracts.Tests/XunitOutput.cs
@@ -0,0 +1,26 @@
+﻿using PactNet.Infrastructure.Outputters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Dfe.PrepareConversions.Contracts.Tests
+{
+   internal class XunitOutput : IOutput
+   {
+      private readonly ITestOutputHelper _output;
+
+      public XunitOutput(ITestOutputHelper output)
+      {
+         _output = output;
+      }
+
+
+      public void WriteLine(string line)
+      {
+         _output.WriteLine(line);
+      }
+   }
+}
