7cb8d386409035b34f0c4b6e6765d0156e9315bd
Dominic NEED <Dominic.NEED@EDUCATION.GOV.UK>
2023-11-16 14:32:15 +0000

    Implemented DfE frontend

--- Dfe.PrepareConversions/Dfe.PrepareConversions/wwwroot/webpack.assets.config.js
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/wwwroot/webpack.assets.config.js
@@ -7,6 +7,7 @@ module.exports = {
 			patterns: [
 				{ from: path.join(__dirname, 'node_modules/govuk-frontend/govuk/assets'), to: path.join(__dirname, 'assets') },
 				{ from: path.join(__dirname, 'node_modules/accessible-autocomplete/dist'), to: path.join(__dirname, 'dist') },
+            { from: path.join(__dirname, 'node_modules/dfe-frontend-alpha/dist'), to: path.join(__dirname, 'dist') },
 				{ from: path.resolve(__dirname, 'node_modules/@ministryofjustice/frontend/moj/assets'), to: path.resolve(__dirname, 'assets') },
 			],
 		})
