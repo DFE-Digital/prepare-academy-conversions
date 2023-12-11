9ffa86a9b010abf280057ac25d1eabf993d4c72a
Dominic NEED <Dominic.NEED@EDUCATION.GOV.UK>
2023-12-11 15:59:03 +0000

    Remove API calls in cypress

--- Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/Core-journeys/voluntary-conversion.cy.js
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/Core-journeys/voluntary-conversion.cy.js
@@ -5,11 +5,8 @@ import voluntaryProjectTaskList from '../../pages/voluntaryProjectTaskList'
 var projectName = 'Voluntary Cypress Project'
 
 describe('Voluntary conversion', { tags: ['@dev', '@stage'] }, () => {
-   beforeEach(() => {
-      cy.callAcademisationApi('POST', `cypress-data/add-voluntary-project.cy`, "{}")
-         .then(() => {
-          projectList.selectProject(projectName)
-         });
+   before(() => {
+      // Do the spin up a project journey
    })
 
    it('TC01: Voluntary conversion journey ', () => {
