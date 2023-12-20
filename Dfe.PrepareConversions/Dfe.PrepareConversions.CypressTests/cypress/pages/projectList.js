f59d063e166714e53d6680a25fff86823a8d2235
Dominic NEED <Dominic.NEED@EDUCATION.GOV.UK>
2023-12-20 16:39:54 +0000

    Commented ESlint

--- Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/pages/projectList.js
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/pages/projectList.js
@@ -3,53 +3,53 @@ import BasePage from './BasePage'
 
 export default class projectList extends BasePage {
 
-    static path = 'project-list'
+   static path = 'project-list'
 
-    static checkProjectListPage() {
-        cy.url().should('include', this.path)
-    }
+   static checkProjectListPage() {
+      cy.url().should('include', this.path)
+   }
 
-    static getNthProject(n = 0) {
-        this.checkProjectListPage()
-        return cy.get(`[id="school-name-${n}"]`)
-    }
+   static getNthProject(n = 0) {
+      this.checkProjectListPage()
+      return cy.get(`[id="school-name-${n}"]`)
+   }
 
-    static getNthProjectDeliveryOfficer(n = 0) {
-        this.checkProjectListPage()
-        return cy.get(`[id="delivery-officer-${n}"]`)
-    }
+   static getNthProjectDeliveryOfficer(deliveryOfficer) {
+      this.checkProjectListPage()
+      return cy.get(`[data-cy="select-projectlist-filter-officer-${deliveryOfficer}"]`)
+   }
 
-    static filterProjectList(titleFilter) {
-        const filterQuery = `?Title=${encodeURIComponent(titleFilter)}`
-        cy.visit(`${Cypress.env('url')}/${this.path}${filterQuery}`)
-    }
+   static filterProjectList(titleFilter) {
+      const filterQuery = `?Title=${encodeURIComponent(titleFilter)}`
+      cy.visit(`${Cypress.env('url')}/${this.path}${filterQuery}`)
+   }
 
-    static selectFirstItem() {
-        this.checkProjectListPage()
-        this.getNthProject().click()
-    }
+   static selectFirstItem() {
+      this.checkProjectListPage()
+      this.getNthProject().click()
+   }
 
-    static selectProject(projectName = 'Gloucester school') {
-        this.filterProjectList(projectName)
-        this.selectFirstItem()
-        return cy.url().then(url => this.getIdFromUrl(url))
-    }
+   static selectProject(projectName = 'Gloucester school') {
+      this.filterProjectList(projectName)
+      this.selectFirstItem()
+      return cy.url().then(url => this.getIdFromUrl(url))
+   }
 
-    static selectVoluntaryProject() {
-        cy.login({titleFilter: 'Voluntary Cypress Project'})
-        cy.get('[id="school-name-0"]').click()
+   static selectVoluntaryProject() {
+      cy.login({ titleFilter: 'Voluntary Cypress Project' })
+      cy.get('[id="school-name-0"]').click()
 
-        return cy.url().then(url => this.getIdFromUrl(url))
-    }
+      return cy.url().then(url => this.getIdFromUrl(url))
+   }
 
-    static getIdFromUrl(url) {
-        const urlSplit = url.toString().split('/')
-        for (let i = urlSplit.length - 1; i > 0; i--) {
-            const potentialId = parseInt(urlSplit[i])
+   static getIdFromUrl(url) {
+      const urlSplit = url.toString().split('/')
+      for (let i = urlSplit.length - 1; i > 0; i--) {
+         const potentialId = parseInt(urlSplit[i])
 
-            if (!isNaN(potentialId)) return potentialId
-        }
+         if (!isNaN(potentialId)) return potentialId
+      }
 
-        return ''
-    }
+      return ''
+   }
 }
