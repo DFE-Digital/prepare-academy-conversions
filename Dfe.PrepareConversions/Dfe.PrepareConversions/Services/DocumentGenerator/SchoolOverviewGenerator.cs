0ce3b2f11e9da0d3cd2b673491f7c77335151187
Dominic NEED <Dominic.NEED@EDUCATION.GOV.UK>
2024-01-03 10:59:27 +0000

    Added SEN specific document generation

--- Dfe.PrepareConversions/Dfe.PrepareConversions/Services/DocumentGenerator/SchoolOverviewGenerator.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/Services/DocumentGenerator/SchoolOverviewGenerator.cs
@@ -12,35 +12,46 @@ namespace Dfe.PrepareConversions.Services.DocumentGenerator
          builder.ReplacePlaceholderWithContent("SchoolOverview", build =>
          {
             build.AddHeading("School overview", HeadingLevel.One);
-            build.AddTable(new List<TextElement[]>
-         {
-            new[] { new TextElement { Value = "School type", Bold = true }, new TextElement { Value = document.SchoolType } },
-            new[] { new TextElement { Value = "School phase", Bold = true }, new TextElement { Value = document.SchoolPhase } },
-            new[] { new TextElement { Value = "Age range", Bold = true }, new TextElement { Value = document.AgeRange } },
-            new[] { new TextElement { Value = "Capacity", Bold = true }, new TextElement { Value = document.SchoolCapacity } },
-            new[] { new TextElement { Value = "Published admission number (PAN)", Bold = true }, new TextElement { Value = document.PublishedAdmissionNumber } },
-            new[] { new TextElement { Value = "Number on roll (NOR)", Bold = true }, new TextElement { Value = document.NumberOnRoll } },
-            new[] { new TextElement { Value = "Percentage of the school is full", Bold = true }, new TextElement { Value = document.PercentageSchoolFull } },
-            new[]
+            var schoolOverviewTable = new List<TextElement[]>();
+            schoolOverviewTable.AddRange(new List<TextElement[]> {
+               new[] { new TextElement { Value = "School type", Bold = true }, new TextElement { Value = document.SchoolType } },
+               new[] { new TextElement { Value = "School phase", Bold = true }, new TextElement { Value = document.SchoolPhase } },
+               new[] { new TextElement { Value = "Age range", Bold = true }, new TextElement { Value = document.AgeRange } },
+               new[] { new TextElement { Value = "Capacity", Bold = true }, new TextElement { Value = document.SchoolCapacity } },
+               new[] { new TextElement { Value = "Published admission number (PAN)", Bold = true }, new TextElement { Value = document.PublishedAdmissionNumber } },
+               new[] { new TextElement { Value = "Number on roll (NOR)", Bold = true }, new TextElement { Value = document.NumberOnRoll } },
+               new[] { new TextElement { Value = "Percentage of the school is full", Bold = true }, new TextElement { Value = document.PercentageSchoolFull } },
+               new[]
+               {
+                  new TextElement { Value = "Percentage of free school meals at the school (%FSM)", Bold = true },
+                  new TextElement { Value = document.PercentageFreeSchoolMeals }
+               },
+               new[] { new TextElement { Value = "Viability issues", Bold = true }, new TextElement { Value = document.ViabilityIssues } }});
+
+            // Specific SEN fields
+            if (document.SchoolType.ToLower().Contains("special"))
             {
-               new TextElement { Value = "Percentage of free school meals at the school (%FSM)", Bold = true }, new TextElement { Value = document.PercentageFreeSchoolMeals }
-            },
-            new[] { new TextElement { Value = "Viability issues", Bold = true }, new TextElement { Value = document.ViabilityIssues } },
-            // TODO: Only if SEN
-            new[] { new TextElement { Value = "Number of places funded for", Bold = true }, new TextElement { Value = document.NumberOfPlacesFundedFor } },
-            new[] { new TextElement { Value = "Number of residential places", Bold = true }, new TextElement { Value = document.NumberOfResidentialPlaces } },
-            new[] { new TextElement { Value = "Number of funded residential places", Bold = true }, new TextElement { Value = document.NumberOfFundedResidentialPlaces } },
-            new[] { new TextElement { Value = "Financial deficit", Bold = true }, new TextElement { Value = document.FinancialDeficit } },
-            new[] { new TextElement { Value = "Private finance initiative (PFI) scheme", Bold = true }, new TextElement { Value = document.PartOfPfiScheme } },
-            new[] { new TextElement { Value = "Is the school linked to a diocese?", Bold = true }, new TextElement { Value = document.IsSchoolLinkedToADiocese } },
-            new[]
-            {
-               new TextElement { Value = "Distance from the converting school to the trust or other schools in the trust", Bold = true },
-               new TextElement { Value = $"{document.DistanceFromSchoolToTrustHeadquarters} {document.DistanceFromSchoolToTrustHeadquartersAdditionalInformation}" }
-            },
-            new[] { new TextElement { Value = "Parliamentary constituency", Bold = true }, new TextElement { Value = document.ParliamentaryConstituency } },
-            new[] { new TextElement { Value = "MP name and political party", Bold = true }, new TextElement { Value = document.MemberOfParliamentNameAndParty } }
-         });
+               schoolOverviewTable.AddRange(new List<TextElement[]> {
+               new[] { new TextElement { Value = "Number of places funded for", Bold = true }, new TextElement { Value = document.NumberOfPlacesFundedFor } },
+               new[] { new TextElement { Value = "Number of residential places", Bold = true }, new TextElement { Value = document.NumberOfResidentialPlaces } },
+               new[] { new TextElement { Value = "Number of funded residential places", Bold = true }, new TextElement { Value = document.NumberOfFundedResidentialPlaces } },
+            });
+            }
+
+            schoolOverviewTable.AddRange(new List<TextElement[]> {
+              new[] { new TextElement { Value = "Financial deficit", Bold = true }, new TextElement { Value = document.FinancialDeficit } },
+               new[] { new TextElement { Value = "Private finance initiative (PFI) scheme", Bold = true }, new TextElement { Value = document.PartOfPfiScheme } },
+               new[] { new TextElement { Value = "Is the school linked to a diocese?", Bold = true }, new TextElement { Value = document.IsSchoolLinkedToADiocese } },
+               new[]
+               {
+                  new TextElement { Value = "Distance from the converting school to the trust or other schools in the trust", Bold = true },
+                  new TextElement { Value = $"{document.DistanceFromSchoolToTrustHeadquarters} {document.DistanceFromSchoolToTrustHeadquartersAdditionalInformation}" }
+               },
+               new[] { new TextElement { Value = "Parliamentary constituency", Bold = true }, new TextElement { Value = document.ParliamentaryConstituency } },
+               new[] { new TextElement { Value = "MP name and political party", Bold = true }, new TextElement { Value = document.MemberOfParliamentNameAndParty } }
+            });
+
+            build.AddTable(schoolOverviewTable);
          });
       }
    }
