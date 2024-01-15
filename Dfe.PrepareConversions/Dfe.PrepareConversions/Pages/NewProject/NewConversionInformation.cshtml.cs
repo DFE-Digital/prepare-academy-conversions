a75bd23020b614d1713bee997505e3070ae5c257
Dominic NEED <Dominic.NEED@EDUCATION.GOV.UK>
2024-01-15 14:01:53 +0000

    Removed Async on NewConversionInformation model as it is sync

--- Dfe.PrepareConversions/Dfe.PrepareConversions/Pages/NewProject/NewConversionInformation.cshtml.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/Pages/NewProject/NewConversionInformation.cshtml.cs
@@ -2,21 +2,20 @@ using Dfe.PrepareConversions.Models;
 using Dfe.PrepareConversions.Models.ProjectList;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Threading.Tasks;
 
 namespace Dfe.PrepareConversions.Pages.SponsoredProject;
 
 public class NewConversionInformation : PageModel
 {
 
-   public async Task<IActionResult> OnGet()
+   public IActionResult OnGet()
    {
       ProjectListFilters.ClearFiltersFrom(TempData);
 
       return Page();
    }
 
-   public async Task<IActionResult> OnPost(string redirect)
+   public IActionResult OnPost(string redirect)
    {
       redirect = string.IsNullOrEmpty(redirect) ? Links.NewProject.SearchSchool.Page : redirect;
 
