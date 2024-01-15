b47958108e9ede5b6b84849a768e5ba729d76aeb
plockwood <paul.lockwood@education.gov.uk>
2024-01-15 16:09:29 +0000

    new view model

---
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/Pages/TaskList/KeyStagePerformance/PerformanceDataViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Dfe.PrepareConversions.Data.Services;
+using Dfe.PrepareConversions.Models;
+using Dfe.PrepareConversions.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Dfe.PrepareConversions.Pages.TaskList.KeyStagePerformance
+{
+   public class PerformanceDataViewModel: BaseAcademyConversionProjectPageModel
+   {
+      private readonly ErrorService _errorService;
+
+      public PerformanceDataViewModel(IAcademyConversionProjectRepository repository, ErrorService errorService) : base(repository)
+      {
+         _errorService = errorService;
+      }
+
+      public override async Task<IActionResult> OnPostAsync(int id)
+      {
+         if (YesChecked is true && string.IsNullOrWhiteSpace(ExternalApplicationFormUrl))
+         {
+            ModelState.AddModelError(nameof(ExternalApplicationFormUrl), "You must enter valid link for the schools application form");
+         }
+
+         if (ModelState.IsValid)
+         {
+
+            await _repository.SetProjectExternalApplicationForm(id, YesChecked.Value, YesChecked is true ? ExternalApplicationFormUrl : default);
+
+
+            return RedirectToPage(Links.ExternalApplicationForm.Index.Page, new { id });
+         }
+
+         _errorService.AddErrors(ModelState.Keys, ModelState);
+         return await base.OnGetAsync(id);
+      }
+   }
+}
