de01c3be29255c19fa4a7abf078fbdb7ec5a420e
Dominic NEED <Dominic.NEED@EDUCATION.GOV.UK>
2024-03-15 10:06:08 +0000

    Add string sanitisation to strings being exported to word

---
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/Services/DocumentGenerator/DocumentGeneratorStringSanitiser.cs
@@ -0,0 +1,51 @@
+﻿using Dfe.PrepareConversions.DocumentGeneration.Elements;
+using System.Linq;
+using System.Xml;
+
+namespace Dfe.PrepareConversions.Services.DocumentGenerator
+{
+   public static class DocumentGeneratorStringSanitiser
+   {
+      public static string SanitizeString(string input)
+      {
+         if (string.IsNullOrEmpty(input)) return input;
+
+         string output = input.Replace("<", "&lt;")
+                              .Replace(">", "&gt;")
+                              .Replace("&", "&amp;")
+                              .Replace("\"", "&quot;")
+                              .Replace("\'", "&apos;")
+                              .Replace("&amp;amp;", "&amp;");
+         output = new string(output.Where(ch => XmlConvert.IsXmlChar(ch)).ToArray());
+         return output;
+      }
+
+      // SanitizeTextElements method as provided
+      public static TextElement[] SanitizeTextElements(TextElement[] elements)
+      {
+         return elements.Select(element => new TextElement
+         {
+            Value = SanitizeString(element.Value),
+            Bold = element.Bold
+         }).ToArray();
+      }
+
+      // Utility method to create and sanitize TextElement arrays
+      public static TextElement[] CreateTextElements(string label, string value)
+      {
+         return SanitizeTextElements(new[]
+         {
+                new TextElement { Value = label, Bold = true },
+                new TextElement { Value = value }
+            });
+      }
+      public static TextElement[] CreateSingleTextElement(string value)
+      {
+         return new TextElement[]
+         {
+               new() { Value = SanitizeString(value) }
+         };
+      }
+
+   }
+}
