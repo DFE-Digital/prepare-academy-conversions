e3a87afdbe8d58100489347e59b80b17f8deddc1
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-07-24 13:28:40 +0100

    Set custom CDN waf rules (#1159)
    * this will be used for specific cases where we want requests to bypass the rate limit policy

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -28,6 +28,7 @@ locals {
   cdn_frontdoor_origin_host_header_override       = var.cdn_frontdoor_origin_host_header_override
   cdn_frontdoor_forwarding_protocol               = var.cdn_frontdoor_forwarding_protocol
   cdn_frontdoor_enable_rate_limiting              = var.cdn_frontdoor_enable_rate_limiting
+  cdn_frontdoor_waf_custom_rules                  = var.cdn_frontdoor_waf_custom_rules
   cdn_frontdoor_rate_limiting_threshold           = var.cdn_frontdoor_rate_limiting_threshold
   cdn_frontdoor_rate_limiting_duration_in_minutes = var.cdn_frontdoor_rate_limiting_duration_in_minutes
   cdn_frontdoor_host_add_response_headers         = var.cdn_frontdoor_host_add_response_headers
