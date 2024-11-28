5fb6489699131d382adec853bb9437ca69e4e2bf
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-11-28 14:00:08 +0000

    Abstract App Insights JS SDK
    * Making it a shared partial keeps the _layout.cshtml cleaner
    
    * Adds missing configuration for click tracking and user identification
    
    * Extract App Insights connection string into Options

--- Dfe.PrepareConversions/Dfe.PrepareConversions/wwwroot/src/application-insights.js
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/wwwroot/src/application-insights.js
@@ -4,6 +4,7 @@ import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 const clickPluginInstance = new Microsoft.ApplicationInsights.ClickAnalyticsPlugin();
 const appInsights = new ApplicationInsights({ config: {
    connectionString: window.appInsights.connectionString,
+   instrumentationKey: window.appInsights.instrumentationKey,
    extensions: [ clickPluginInstance ],
    extensionConfig: { [clickPluginInstance.identifier] : {
       autoCapture : true,
