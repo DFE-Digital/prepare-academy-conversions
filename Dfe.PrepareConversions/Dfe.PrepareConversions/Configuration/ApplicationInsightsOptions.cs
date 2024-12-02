59df364af944d07cadde943f70a994ea6b1e37a4
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-12-02 11:58:38 +0000

    Updated Tests

--- Dfe.PrepareConversions/Dfe.PrepareConversions/Configuration/ApplicationInsightsOptions.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/Configuration/ApplicationInsightsOptions.cs
@@ -3,6 +3,7 @@
 public class ApplicationInsightsOptions
 {
    public const string ConfigurationSection = "ApplicationInsights";
-   public string ConnectionString { get; set; }
-   public string InstrumentationKey { get; set; }
+   public string ConnectionString { get; init; }
+   public string InstrumentationKey { get; init; }
+   public string EnableBrowserAnalytics { get; init; }
 }
