5430458813a5124f07acc10e733d0a4799c533d1
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-12-02 12:11:44 +0000

    Abstract App Insights JS SDK (#1226)
    * Making it a shared partial keeps the _layout.cshtml cleaner
    
    * Adds missing configuration for click tracking and user identification
    
    * Extract App Insights connection string into Options

---
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/Configuration/ApplicationInsightsOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Dfe.PrepareConversions.Configuration;
+
+public class ApplicationInsightsOptions
+{
+   public const string ConfigurationSection = "ApplicationInsights";
+   public string ConnectionString { get; init; }
+   public string InstrumentationKey { get; init; }
+   public string EnableBrowserAnalytics { get; init; }
+}
