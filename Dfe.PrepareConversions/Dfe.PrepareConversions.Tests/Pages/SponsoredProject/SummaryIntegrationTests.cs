49eeae971d9946b8e1c737ed4ea62ece84aa8a4e
AZIA, Stephane <Stephane.AZIA@EDUCATION.GOV.UK>
2025-04-14 15:19:48 +0100

    Trying to sort out broken test in SummaryIntegrationTests

--- Dfe.PrepareConversions/Dfe.PrepareConversions.Tests/Pages/SponsoredProject/SummaryIntegrationTests.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.Tests/Pages/SponsoredProject/SummaryIntegrationTests.cs
@@ -20,20 +20,17 @@ public class SummaryIntegrationTests : BaseIntegrationTests
    [AutoMoqData]
    public async Task Should_render_summary_page_with_school_and_trust(EstablishmentDto establishment, TrustDtoResponse TrustDtoResponse)
    {
-      //establishment.OfstedLastInspection = DateTime.Now.ToString("dd-mm-yyyy");
-      //string ukprn = TrustDtoResponse.Data[0].Ukprn;
-      //_factory.AddGetWithJsonResponse($"/v4/establishment/urn/{establishment.Urn}", establishment);
-      //_factory.AddGetWithJsonResponse("/v4/trusts*", TrustDtoResponse);
+      establishment.OfstedLastInspection = DateTime.Now.ToString("dd-mm-yyyy");
+      string ukprn = TrustDtoResponse.Data[0].Ukprn;
+      _factory.AddGetWithJsonResponse($"/v4/establishment/urn/{establishment.Urn}", establishment);
+      _factory.AddGetWithJsonResponse("/v4/trusts*", TrustDtoResponse);
 
-      //await OpenAndConfirmPathAsync($"/start-new-project/check-school-trust-details?ukprn={ukprn}&urn={establishment.Urn}&hasSchoolApplied=true&hasPreferredTrust=yes");
+      await OpenAndConfirmPathAsync($"/start-new-project/check-school-trust-details?ukprn={ukprn}&urn={establishment.Urn}&hasSchoolApplied=true&hasPreferredTrust=yes");
 
-      //Document.QuerySelector<IHtmlElement>("[data-cy=school-name]")!.Text().Trim().Should()
-      //   .Be(establishment.Name);
-      //Document.QuerySelector<IHtmlElement>("[data-cy=trust-name]")!.Text().Trim().Should()
-      //   .Be(TrustDtoResponse.Data[0].Name);
-
-      // Sonar cloud did not allow me to skip this test, so I comment it out for now till a fix is provided for this test
-      Assert.True(true);
+      Document.QuerySelector<IHtmlElement>("[data-cy=school-name]")!.Text().Trim().Should()
+         .Be(establishment.Name);
+      Document.QuerySelector<IHtmlElement>("[data-cy=trust-name]")!.Text().Trim().Should()
+         .Be(TrustDtoResponse.Data[0].Name);
    }
 
 }
