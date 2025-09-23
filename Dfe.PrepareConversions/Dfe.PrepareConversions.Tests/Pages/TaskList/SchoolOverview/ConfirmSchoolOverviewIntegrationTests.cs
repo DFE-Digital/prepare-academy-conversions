ea023d5a245d24e12c2d8faa5a2f35a9681357b9
saziadfe <Stephane.AZIA@education.gov.uk>
2025-09-23 11:27:58 +0100

    Performance data references TRAMS incorrectly (#1395)

--- Dfe.PrepareConversions/Dfe.PrepareConversions.Tests/Pages/TaskList/SchoolOverview/ConfirmSchoolOverviewIntegrationTests.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.Tests/Pages/TaskList/SchoolOverview/ConfirmSchoolOverviewIntegrationTests.cs
@@ -67,7 +67,7 @@ public class ConfirmSchoolOverviewIntegrationTests(IntegrationTestingWebApplicat
 
       await NavigateAsync("School overview");
       var test = Document.QuerySelector("[data-test=annex-b-help]").TextContent;
-      Document.QuerySelector("[data-test=annex-b-help]").TextContent.Should().Be("You can add more from the Annex B form.");
+      Document.QuerySelector("[data-test=annex-b-help]").TextContent.Should().Be("Some of the information has come from school's application form.");
    }
    [Fact]
    public async Task Should_display_change_on_pfi_scheme()
