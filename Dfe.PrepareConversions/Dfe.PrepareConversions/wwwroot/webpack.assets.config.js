345199fba49d347474a41049d37dc467e2a01b86
Stephen Bradbury <bradbury.ste@googlemail.com>
2025-10-07 11:49:57 +0100

    changes to use the new .gov designs

--- Dfe.PrepareConversions/Dfe.PrepareConversions/wwwroot/webpack.assets.config.js
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/wwwroot/webpack.assets.config.js
@@ -5,16 +5,15 @@ module.exports = {
 	plugins: [
 		new CopyPlugin({
 			patterns: [
-				{ from: path.join(__dirname, 'node_modules/govuk-frontend/govuk/assets'), to: path.join(__dirname, 'assets') },
+				{ from: path.join(__dirname, 'node_modules/govuk-frontend/dist/govuk/assets'), to: path.join(__dirname, 'assets') },
 				{ from: path.join(__dirname, 'node_modules/accessible-autocomplete/dist'), to: path.join(__dirname, 'dist') },
-            { from: path.join(__dirname, 'node_modules/dfe-frontend/dist'), to: path.join(__dirname, 'dist') },
-            { from: path.join(__dirname, 'node_modules/dfe-frontend/packages/assets'), to: path.join(__dirname, 'assets') },
 				{ from: path.resolve(__dirname, 'node_modules/@ministryofjustice/frontend/moj/assets'), to: path.resolve(__dirname, 'assets') },
 			],
 		})
 	],
 	output: {
-		path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
 		filename: 'site.js',
 	}
 };
--- Dfe.PrepareConversions/Dfe.PrepareConversions/wwwroot/webpack.config.js
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/wwwroot/webpack.config.js
@@ -1,52 +1,45 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-	entry: ["./src/index.js", "./src/index.scss"],
-	plugins: [
-		new MiniCssExtractPlugin({ filename: 'site.css' }),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					// Creates `style` nodes from JS strings
-					MiniCssExtractPlugin.loader,
-					// Translates CSS into CommonJS
-					"css-loader",
-					// Compiles Sass to CSS
-					"sass-loader",
-				],
-			},
-			{ test: /\.css$/, use: ['style-loader', 'css-loader'] },
-			{
-				test: /\.(woff2?)$/i,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							emitFile: false,
-							name: '/assets/fonts/[name].[ext]'
-						}
-					}
-				]
-			},
-			{
-				test: /\.(jpe?g|png|gif|svg)$/i,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							emitFile: false,
-							name: '/assets/images/[name].[ext]'
-						}
-					}
-				]
-			},
-		]
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'site.js',
-	}
+   entry: ["./src/index.js", "./src/index.scss"],
+   plugins: [
+      new MiniCssExtractPlugin({ filename: 'site.css' }),
+   ],
+   module: {
+      rules: [
+         {
+            test: /\.s[ac]ss$/i,
+            use: [
+               // Creates `style` nodes from JS strings
+               MiniCssExtractPlugin.loader,
+               // Translates CSS into CommonJS
+               "css-loader",
+               // Compiles Sass to CSS
+               "sass-loader",
+            ],
+         },
+         { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+         {
+            test: /\.(woff2?)$/i,
+            type: 'asset/resource', // Replaces file-loader
+            generator: {
+               filename: '../assets/fonts/[name][ext]', // Matches your old file-loader name pattern
+               publicPath: '/assets/',
+            },
+         },
+         {
+            test: /\.(jpe?g|png|gif|svg)$/i, // Match image files
+            type: 'asset/resource', // Replaces file-loader
+            generator: {
+               filename: '../assets/images/[name][ext]', // Matches your old file-loader name pattern
+               publicPath: '/assets/',
+            },
+         },
+      ]
+   },
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/', // Ensures correct URL references
+      filename: 'site.js',
+   }
 };
