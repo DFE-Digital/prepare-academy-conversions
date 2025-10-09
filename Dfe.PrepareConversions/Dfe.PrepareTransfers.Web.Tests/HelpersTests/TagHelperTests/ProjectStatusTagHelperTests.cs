b08881c7b04a47179858ab14c2d445770514acc1
Stephen Bradbury <bradbury.ste@googlemail.com>
2025-10-09 14:02:35 +0100

    changes to use title case on statuses

--- Dfe.PrepareConversions/Dfe.PrepareTransfers.Web.Tests/HelpersTests/TagHelperTests/ProjectStatusTagHelperTests.cs
+++ Dfe.PrepareConversions/Dfe.PrepareTransfers.Web.Tests/HelpersTests/TagHelperTests/ProjectStatusTagHelperTests.cs
@@ -6,43 +6,39 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace Dfe.PrepareTransfers.Web.Tests.HelpersTests.TagHelperTests
+namespace Dfe.PrepareTransfers.Web.Tests.HelpersTests.TagHelperTests;
+
+public class ProjectStatusTagHelperTests
 {
-    public class ProjectStatusTagHelperTests
-    {
-        [Theory]
-        [InlineData(ProjectStatuses.NotStarted, "NOT STARTED", "govuk-tag--grey")]
-        [InlineData(ProjectStatuses.InProgress, "IN PROGRESS", "govuk-tag--blue")]
-        [InlineData(ProjectStatuses.Completed, "COMPLETED", null)]
-        [InlineData(ProjectStatuses.Empty, "", null)]
-        public void GivenNotStartedStatus_ReturnsRedNotStartedTag(ProjectStatuses projectStatus,
-            string expectedStatusText, string expectedCssClass)
-        {
-            // Arrange
-            var projectStatusTagHelper = new ProjectStatusTagHelper
-            {
-                Status = projectStatus
-            };
-            var tagHelperContext = new TagHelperContext(
-                new TagHelperAttributeList() {{"id", "elementId"}},
-                new Dictionary<object, object>(),
-                Guid.NewGuid().ToString("N"));
-            var tagHelperOutput = new TagHelperOutput("projectstatus",
-                new TagHelperAttributeList(),
-                (result, encoder) =>
-                {
-                    var tagHelperContent = new DefaultTagHelperContent();
-                    tagHelperContent.SetHtmlContent(string.Empty);
-                    return Task.FromResult<TagHelperContent>(tagHelperContent);
-                });
+   [Theory]
+   [InlineData(ProjectStatuses.NotStarted, "Not Started", "govuk-tag--grey")]
+   [InlineData(ProjectStatuses.InProgress, "In Progress", "govuk-tag--blue")]
+   [InlineData(ProjectStatuses.Completed, "Completed", null)]
+   [InlineData(ProjectStatuses.Empty, "", null)]
+   public void GivenNotStartedStatus_ReturnsRedNotStartedTag(ProjectStatuses projectStatus,
+      string expectedStatusText, string expectedCssClass)
+   {
+      // Arrange
+      ProjectStatusTagHelper projectStatusTagHelper = new() { Status = projectStatus };
+      TagHelperContext tagHelperContext = new(
+         new TagHelperAttributeList { { "id", "elementId" } },
+         new Dictionary<object, object>(),
+         Guid.NewGuid().ToString("N"));
+      TagHelperOutput tagHelperOutput = new("projectstatus",
+         new TagHelperAttributeList(),
+         (result, encoder) =>
+         {
+            DefaultTagHelperContent tagHelperContent = new();
+            tagHelperContent.SetHtmlContent(string.Empty);
+            return Task.FromResult<TagHelperContent>(tagHelperContent);
+         });
 
-            // Act
-            projectStatusTagHelper.Process(tagHelperContext, tagHelperOutput);
+      // Act
+      projectStatusTagHelper.Process(tagHelperContext, tagHelperOutput);
 
-            // Assert
-            Assert.Equal("strong", tagHelperOutput.TagName);
-            Assert.Equal(expectedStatusText, tagHelperOutput.Content.GetContent());
-            Assert.Equal($"govuk-tag {expectedCssClass} moj-task-list__tag", tagHelperOutput.Attributes["class"].Value);
-        }
-    }
+      // Assert
+      Assert.Equal("strong", tagHelperOutput.TagName);
+      Assert.Equal(expectedStatusText, tagHelperOutput.Content.GetContent());
+      Assert.Equal($"govuk-tag {expectedCssClass} moj-task-list__tag", tagHelperOutput.Attributes["class"].Value);
+   }
 }
