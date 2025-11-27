81db25d7c14070d6e74b4ef3645ef68732e894d1
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-11-27 20:09:52 +0000

    Update configuration to load from env based config

--- Dfe.PrepareConversions/Dfe.PrepareConversions/Program.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/Program.cs
@@ -25,19 +25,6 @@ public static class Program
    {
       return Host.CreateDefaultBuilder(args)
          .UseSerilog()
-         .ConfigureAppConfiguration((context, configuration) =>
-         {
-            var env = context.HostingEnvironment;
-            
-            configuration.Sources.Clear();
-            
-            // Load configuration in order of precedence (later sources override earlier ones)
-            configuration
-               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-               .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
-               .AddEnvironmentVariables()
-               .AddCommandLine(args);
-         })
          .ConfigureWebHostDefaults(webBuilder =>
          {
             webBuilder.UseStartup<Startup>();
