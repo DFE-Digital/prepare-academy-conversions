3dc642e1b2bf5ebbd9bb564e884a1e6882ceb754
paddy-clark <patrick.clark@education.gov.uk>
2026-01-26 14:34:41 +0000

    add lint formatting files

--- Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/eslint.config.cjs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/eslint.config.cjs
@@ -1,29 +1,63 @@
+const eslintPluginCypress = require('eslint-plugin-cypress/flat');
+const eslintPluginPrettier = require('eslint-plugin-prettier');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
 module.exports = [
+  eslintPluginCypress.configs.recommended,
+  eslintConfigPrettier,
   {
-    files: [
-      ".js", ".jsx", ".ts", ".tsx"
-    ],
-    plugins: [
-      "cypress"
-    ],
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        cy: 'readonly',
+        Cypress: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        before: 'readonly',
+        beforeEach: 'readonly',
+        after: 'readonly',
+        afterEach: 'readonly',
+        expect: 'readonly',
+        context: 'readonly',
+        process: 'readonly',
+        require: 'readonly',
+        module: 'readonly',
+        __dirname: 'readonly',
+        console: 'readonly',
+      },
+    },
     rules: {
-      "cypress/no-assigning-return-values": "error",
-      "cypress/no-unnecessary-waiting": "error",
-      "cypress/assertion-before-screenshot": "error",
-      "cypress/no-force": "error",
-      "cypress/no-async-tests": "error",
-      "cypress/no-pause": "error"
+      // Cypress specific rules
+      'cypress/no-assigning-return-values': 'error',
+      'cypress/no-unnecessary-waiting': 'error',
+      'cypress/assertion-before-screenshot': 'error',
+      'cypress/no-force': 'error',
+      'cypress/no-async-tests': 'error',
+      'cypress/no-pause': 'error',
+
+      // Prettier integration - shows formatting issues as ESLint errors
+      'prettier/prettier': 'error',
+
+      // General best practices
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      eqeqeq: ['error', 'always'],
     },
-    env: {
-      "cypress/globals": true,
-      "node": true
-    },
-    extends: [
-      "plugin:cypress/recommended",
-      "eslint:recommended"
+  },
+  {
+    ignores: [
+      'node_modules/**',
+      'cypress/reports/**',
+      'cypress/screenshots/**',
+      'cypress/videos/**',
+      'cypress/downloads/**',
     ],
-    parserOptions: {
-      "ecmaVersion": 2022
-    }
-  }
-]
+  },
+];
