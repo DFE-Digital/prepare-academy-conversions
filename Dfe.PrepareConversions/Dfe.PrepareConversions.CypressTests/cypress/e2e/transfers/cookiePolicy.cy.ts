a0eba4b69a2805e2948b9bb9358c745053be26a5
paddy-clark <patrick.clark@education.gov.uk>
2026-01-27 14:45:17 +0000

    refactor cookie tests to use page object model

--- Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/transfers/cookiePolicy.cy.ts
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/transfers/cookiePolicy.cy.ts
@@ -1,50 +1,51 @@
+import cookiePolicyPage from 'cypress/pages/cookiePolicyPage';
+import { Logger } from '../../support/logger';
+
 describe('Cookie Policy', () => {
     beforeEach(() => {
+        Logger.log('Visit the homepage before each test');
         cy.visit('/');
     });
 
     it('Should show cookie banner when no preference set', () => {
-        cy.get('[data-test="cookie-banner"]').should('be.visible');
+        cookiePolicyPage.cookieBannerShouldBeVisible();
     });
 
     context('When cookie banner clicked', () => {
         beforeEach(() => {
-            cy.get('[data-test="cookie-banner-accept"]').click();
+            Logger.log('Accept cookies from banner');
+            cookiePolicyPage.acceptCookiesFromBanner();
         });
 
         it('Should consent to cookies from cookie header button', () => {
-            cy.getCookie('.ManageAnAcademyTransfer.Consent').should('exist').should('have.property', 'value', 'True');
+            cookiePolicyPage.verifyCookieConsentIsSet('True');
         });
 
         it('Should hide the cookie banner when consent has been given', () => {
-            cy.get('#acceptCookieBanner').should('be.visible');
+            cookiePolicyPage.acceptedBannerShouldBeVisible();
         });
     });
 
     context('When cookie link in footer clicked', () => {
         beforeEach(() => {
-            cy.get("[data-test='cookie-preferences']").click();
+            Logger.log('Click cookie preferences link in footer');
+            cookiePolicyPage.clickCookiePreferencesLink();
         });
 
         it('Should navigate to cookies page', () => {
-            cy.url().then((href) => {
-                expect(href).includes('cookie-preferences');
-            });
+            cookiePolicyPage.verifyOnCookiePreferencesPage();
         });
 
         it('Should set cookie preferences', () => {
-            cy.get('#cookie-consent-deny').click();
-            cy.get("[data-qa='submit']").click();
-            cy.getCookie('.ManageAnAcademyTransfer.Consent').should('have.property', 'value', 'False');
+            cookiePolicyPage.selectDenyCookies().submitCookiePreferences().verifyCookieConsentIsSet('False');
         });
 
-        it('Should return show success banner', () => {
-            cy.get('#cookie-consent-deny').click();
-            cy.get("[data-qa='submit']").click();
-            cy.get('[data-test="success-banner-return-link"]').click();
-            cy.url().then((href) => {
-                expect(href).includes('/project-list');
-            });
+        it('Should show success banner and return to project list', () => {
+            cookiePolicyPage
+                .selectDenyCookies()
+                .submitCookiePreferences()
+                .clickSuccessBannerReturnLink()
+                .verifyRedirectedToProjectList();
         });
     });
 });
---
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/pages/cookiePolicyPage.ts
@@ -0,0 +1,69 @@
+class CookiePolicyPage {
+    public slug = 'cookie-preferences';
+
+    private cookieName = '.ManageAnAcademyTransfer.Consent';
+
+    public cookieBannerShouldBeVisible(): this {
+        cy.getByDataTest('cookie-banner').should('be.visible');
+        return this;
+    }
+
+    public cookieBannerShouldNotBeVisible(): this {
+        cy.getByDataTest('cookie-banner').should('not.exist');
+        return this;
+    }
+
+    public acceptCookiesFromBanner(): this {
+        cy.getByDataTest('cookie-banner-accept').click();
+        return this;
+    }
+
+    public verifyCookieConsentIsSet(value: string): this {
+        cy.getCookie(this.cookieName).should('exist').should('have.property', 'value', value);
+        return this;
+    }
+
+    public acceptedBannerShouldBeVisible(): this {
+        cy.getById('acceptCookieBanner').should('be.visible');
+        return this;
+    }
+
+    public clickCookiePreferencesLink(): this {
+        cy.getByDataTest('cookie-preferences').click();
+        return this;
+    }
+
+    public verifyOnCookiePreferencesPage(): this {
+        cy.checkPath(this.slug);
+        return this;
+    }
+
+    public selectDenyCookies(): this {
+        cy.getById('cookie-consent-deny').click();
+        return this;
+    }
+
+    public selectAcceptCookies(): this {
+        cy.getById('cookie-consent-accept').click();
+        return this;
+    }
+
+    public submitCookiePreferences(): this {
+        cy.get('[data-qa="submit"]').click();
+        return this;
+    }
+
+    public clickSuccessBannerReturnLink(): this {
+        cy.getByDataTest('success-banner-return-link').click();
+        return this;
+    }
+
+    public verifyRedirectedToProjectList(): this {
+        cy.checkPath('/project-list');
+        return this;
+    }
+}
+
+const cookiePolicyPage = new CookiePolicyPage();
+
+export default cookiePolicyPage;
