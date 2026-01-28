ecbe1440d6dab4b9611b5e83300100a3291b24d2
paddy-clark <patrick.clark@education.gov.uk>
2026-01-28 16:20:08 +0000

    fix and refactor filtration test

--- Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/conversions/filteration-test.cy.ts
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/conversions/filteration-test.cy.ts
@@ -1,45 +1,8 @@
 /// <reference types="cypress" />
-import projectList from '../../pages/projectList';
 import { filterPage } from '../../pages/filterPage';
-import { currentDate, nextYearDate } from '../../constants/testConstants';
 import { Logger } from '../../support/logger';
 
 describe('Filtration Tests', {}, () => {
-    const testData = {
-        projectName: 'Deanshanger Primary School',
-        completedText: 'Completed',
-        projectAssignment: {
-            deliveryOfficer: 'Chris Sherlock',
-            assignedOfficerMessage: 'Project is assigned',
-        },
-        schoolOverview: {
-            pan: '999',
-            pfiDescription: 'PFI Description',
-            distance: '15',
-            distanceDecription: 'Distance description',
-            mp: 'Important Politician, Independent',
-        },
-        budget: {
-            endOfFinanicalYear: currentDate,
-            forecastedRevenueCurrentYear: 20,
-            forecastedCapitalCurrentYear: 10,
-            endOfNextFinancialYear: nextYearDate,
-            forecastedRevenueNextYear: 15,
-            forecastedCapitalNextYear: 12,
-        },
-        pupilForecast: {
-            additionalInfomation: 'Pupil Forecast Additional Information',
-        },
-        rationale: 'This is why this school should become an academy',
-        risksAndIssues: 'Here are the risks and issues for this conversion',
-        localAuthority: {
-            comment: 'Comment',
-            sharepointLink: 'https://sharepoint.com',
-        },
-        performanceInfo: 'Additional Information',
-        keyStages: [2],
-    };
-
     const regionFilterConfig = {
         regionIds: ['east-midlands', 'east-of-england', 'london', 'north-east'],
         expectedRegionNames: ['East Midlands', 'East of England', 'London', 'North East'],
@@ -54,7 +17,10 @@ describe('Filtration Tests', {}, () => {
         Logger.log('Visit the homepage before each test');
         cy.visit('/');
         cy.acceptCookies();
-        projectList.filterProject(testData.projectName);
+    });
+
+    afterEach(() => {
+        filterPage.clearFilters();
     });
 
     it('Should filter projects by region', () => {
@@ -62,8 +28,7 @@ describe('Filtration Tests', {}, () => {
             .expandRegionFilter()
             .selectRegions(regionFilterConfig.regionIds)
             .applyFilters()
-            .verifyAllRegionsInResults(regionFilterConfig.resultCount, regionFilterConfig.expectedRegionNames)
-            .clearFilters();
+            .verifyAllRegionsInResults(regionFilterConfig.resultCount, regionFilterConfig.expectedRegionNames);
     });
 
     projectStatuses.forEach((statusToFilter) => {
@@ -72,8 +37,7 @@ describe('Filtration Tests', {}, () => {
                 .expandProjectStatusFilter()
                 .selectProjectStatus(statusToFilter)
                 .applyFilters()
-                .verifyProjectStatusInResults(statusToFilter)
-                .clearFilters();
+                .verifyProjectStatusInResults(statusToFilter);
         });
     });
 
