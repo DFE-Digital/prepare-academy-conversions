68e1dfaf20c81a39803932558f3457a2a3dcade9
Tristan Collen <tristan.collen@nimbleapproach.com>
2026-05-20 11:37:16 +0100

    resolve tests

--- Dfe.PrepareConversions/Dfe.PrepareTransfers.Web.Tests/PagesTests/Projects/GeneralInformation/IndexTests.cs
+++ Dfe.PrepareConversions/Dfe.PrepareTransfers.Web.Tests/PagesTests/Projects/GeneralInformation/IndexTests.cs
@@ -1,54 +1,72 @@
-using System.Collections.Generic;
-using System.Linq;
 using AutoFixture;
+using Dfe.PrepareConversions.Data.Services.Person;
+using Dfe.PrepareConversions.Services;
 using Dfe.PrepareTransfers.Data.Models;
 using Dfe.PrepareTransfers.Web.Pages.Projects.GeneralInformation;
+using GovUK.Dfe.CoreLibs.Contracts.ExternalApplications.Models.Response;
+using GovUK.Dfe.PersonsApi.Client.Contracts;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Moq;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Dfe.PrepareTransfers.Web.Tests.PagesTests.Projects.GeneralInformation
 {
-    public class IndexTests : BaseTests
-    {
-        private readonly Index _subject;
-        
-        public IndexTests()
-        {
-            _subject = new Index(GetInformationForProject.Object)
-            {
-                AcademyUkprn = AcademyUkprn
-            };
-        }
-        
-        [Fact]
-        public async void GivenUrn_FetchesProjectFromTheRepository()
-        {
-            await _subject.OnGetAsync(ProjectUrn0001);
-        
-            GetInformationForProject.Verify(r => r.Execute(ProjectUrn0001), Times.Once);
-        }
-        
-        [Fact]
-        public async void GivenExistingAcademy_AssignsTheAcademyToTheViewModel()
-        {
-            var ukprn = "7689";
-            var fixture = new Fixture();
-            var outgoingAcademy = fixture.Create<Academy>();
-            outgoingAcademy.Ukprn = ukprn;
-            outgoingAcademy.LastChangedDate = "22-10-2021";
-            FoundInformationForProject.OutgoingAcademies = new List<Academy>
-            {
-                outgoingAcademy
-            };
+   public class IndexTests : BaseTests
+   {
+      private readonly Index _subject;
+      private readonly Mock<IPersonApiEstablishmentsService> _personApiEstablishmentsService = new();
+      private readonly Mock<ErrorService> _errorService = new();
+      private readonly Academy _outgoingAcademy;
 
-            _subject.AcademyUkprn = ukprn;
-            var response = await _subject.OnGetAsync(ProjectUrn0001);
+      public IndexTests()
+      {
+         var expectedMp = new MemberOfParliament
+         {
+            DisplayName = "MP Name",
+            ConstituencyPartyName = "MP Party"
+         };
+         var mpResult = new Result<MemberOfParliament>(expectedMp, true, null, null);
 
-            var expectedGeneralInformation = FoundInformationForProject.OutgoingAcademies.First().GeneralInformation;
-            Assert.IsType<PageResult>(response);
-            Assert.Equal(outgoingAcademy.Name, _subject.AcademyName);
-            Assert.Equal(expectedGeneralInformation.SchoolPhase, _subject.SchoolPhase);
-        }
-    }
+         _personApiEstablishmentsService
+            .Setup(x => x.GetMemberOfParliamentBySchoolUrnAsync(It.IsAny<int>()))
+            .ReturnsAsync(mpResult);
+
+         _subject = new Index(GetInformationForProject.Object, _personApiEstablishmentsService.Object, ProjectRepository.Object, _errorService.Object)
+         {
+            AcademyUkprn = AcademyUkprn
+         };
+
+         var ukprn = "7689";
+         var fixture = new Fixture();
+         var outgoingAcademy = fixture.Create<Academy>();
+         outgoingAcademy.Ukprn = ukprn;
+         outgoingAcademy.Urn = "123456";
+         outgoingAcademy.LastChangedDate = "22-10-2021";
+         FoundInformationForProject.OutgoingAcademies = [outgoingAcademy];
+         FoundInformationForProject.Project.TransferringAcademies.First().OutgoingAcademyUkprn = ukprn;
+         _subject.AcademyUkprn = ukprn;
+         _outgoingAcademy = outgoingAcademy;
+      }
+
+      [Fact]
+      public async void GivenUrn_FetchesProjectFromTheRepository()
+      {
+         await _subject.OnGetAsync(ProjectUrn0001);
+
+         GetInformationForProject.Verify(r => r.Execute(ProjectUrn0001), Times.Once);
+      }
+
+      [Fact]
+      public async Task GivenExistingAcademy_AssignsTheAcademyToTheViewModel()
+      {
+         var response = await _subject.OnGetAsync(ProjectUrn0001);
+
+         var expectedGeneralInformation = FoundInformationForProject.OutgoingAcademies.First().GeneralInformation;
+         Assert.IsType<PageResult>(response);
+         Assert.Equal(_outgoingAcademy.Name, _subject.AcademyName);
+         Assert.Equal(expectedGeneralInformation.SchoolPhase, _subject.SchoolPhase);
+      }
+   }
 }
