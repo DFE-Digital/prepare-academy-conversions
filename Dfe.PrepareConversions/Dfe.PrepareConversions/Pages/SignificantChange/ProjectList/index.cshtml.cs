c4c409a9d3d2a4b1d80d8c763b6c817a5443690f
Tristan Collen <tristan.collen@nimbleapproach.com>
2026-07-21 16:44:50 +0100

    create and link repository

--- Dfe.PrepareConversions/Dfe.PrepareConversions/Pages/SignificantChange/ProjectList/index.cshtml.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/Pages/SignificantChange/ProjectList/index.cshtml.cs
@@ -1,19 +1,25 @@
+using Dfe.PrepareConversions.Data.Services.Interfaces;
+using Dfe.PrepareConversions.Utils;
 using Dfe.PrepareConversions.ViewModels;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Dfe.PrepareConversions.Pages.SignificantChange.ProjectList;
 
-public class IndexModel : PageModel
+public class IndexModel(ISignificantChangeProjectRepository significantChangeProjectRepository) : PageModel
 {
+   public ISignificantChangeProjectRepository SignificantChangeProjectRepository { get; } = significantChangeProjectRepository;
    public List<SignificantChangeProjectListViewModel> Projects { get; private set; } = [];
 
    public int ProjectCount => Projects.Count;
 
    public int TotalProjects => ProjectCount;
-
-   public void OnGet()
+   
+   public async Task OnGetAsync()
    {
-      Projects = [];
+      var response = await SignificantChangeProjectRepository.GetAllProjects(1, 100);
+      Projects = response.Body?.Data.Select(SignificantChangeProjectListHelper.Build).ToList();
    }
 }
---
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/Utils/SignificantChangeProjectListHelper.cs
@@ -0,0 +1,21 @@
+﻿using Dfe.PrepareConversions.Data.Models.SignificantChange;
+using Dfe.PrepareConversions.ViewModels;
+
+namespace Dfe.PrepareConversions.Utils;
+
+public static class SignificantChangeProjectListHelper
+{
+   public static SignificantChangeProjectListViewModel Build(SignificantChangeProjectResponse significantChangeProject)
+   {
+      return new SignificantChangeProjectListViewModel
+      {
+         Id = significantChangeProject.Id,
+         Urn = significantChangeProject.Urn,
+         Tier = significantChangeProject.Tier,
+         TrustName = significantChangeProject.TrustName,
+         TrustUkprn = significantChangeProject.TrustUkprn,
+         TypeOfSignificantChange = significantChangeProject.TypeOfSignificantChange,
+         Status = significantChangeProject.Status,
+      };
+   }
+}
