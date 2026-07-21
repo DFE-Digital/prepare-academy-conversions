6de31d3c556df5877d6e7367106407eb66e25098
Tristan Collen <tristan.collen@nimbleapproach.com>
2026-07-21 17:00:17 +0100

    add pagination

--- Dfe.PrepareConversions/Dfe.PrepareConversions/Pages/SignificantChange/ProjectList/index.cshtml.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions/Pages/SignificantChange/ProjectList/index.cshtml.cs
@@ -1,25 +1,30 @@
 using Dfe.PrepareConversions.Data.Services.Interfaces;
 using Dfe.PrepareConversions.Utils;
 using Dfe.PrepareConversions.ViewModels;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Dfe.PrepareConversions.Data.Services;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dfe.PrepareConversions.Models;
 
 namespace Dfe.PrepareConversions.Pages.SignificantChange.ProjectList;
 
-public class IndexModel(ISignificantChangeProjectRepository significantChangeProjectRepository) : PageModel
+public class IndexModel(ISignificantChangeProjectRepository significantChangeProjectRepository) : PaginatedPageModel
 {
    public ISignificantChangeProjectRepository SignificantChangeProjectRepository { get; } = significantChangeProjectRepository;
+   protected override ApiV2PagingInfo Paging { get; set; }
    public List<SignificantChangeProjectListViewModel> Projects { get; private set; } = [];
 
    public int ProjectCount => Projects.Count;
 
-   public int TotalProjects => ProjectCount;
+   public int TotalProjects => Paging?.RecordCount ?? ProjectCount;
    
    public async Task OnGetAsync()
    {
-      var response = await SignificantChangeProjectRepository.GetAllProjects(1, 100);
-      Projects = response.Body?.Data.Select(SignificantChangeProjectListHelper.Build).ToList();
+      PagePath = Links.SignificantChange.ListView.Page;
+
+      var response = await SignificantChangeProjectRepository.GetAllProjects(CurrentPage, PageSize);
+      Paging = response.Body?.Paging ?? new ApiV2PagingInfo { Page = CurrentPage, RecordCount = 0, NextPageUrl = null };
+      Projects = response.Body?.Data?.Select(SignificantChangeProjectListHelper.Build).ToList() ?? [];
    }
 }
