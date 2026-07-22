6555f449c3273ac0f1887fb436444b062eb6da07
Tristan Collen <tristan.collen@nimbleapproach.com>
2026-07-22 17:02:41 +0100

    add new fields to the project row summary

--- Dfe.PrepareConversions/Dfe.PrepareConversions.Data/Models/SignificantChange/SignificantChangeProjectResponse.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.Data/Models/SignificantChange/SignificantChangeProjectResponse.cs
@@ -4,9 +4,11 @@ public class SignificantChangeProjectResponse
 {
    public int Id { get; set; }
    public int Urn { get; set; }
+   public string SchoolName { get; set; }
    public byte Tier { get; set; }
    public required string TrustName { get; set; }
    public required string TrustUkprn { get; set; }
+   public User AssignedUser { get; set; }
    public required string TypeOfSignificantChange { get; set; }
    public required string Status { get; set; }
 }
