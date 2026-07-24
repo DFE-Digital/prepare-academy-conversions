1ba84f767e68aa00cbc4663d54bb5cb719a61b0d
Tristan Collen <tristan.collen@nimbleapproach.com>
2026-07-24 10:35:03 +0100

    add test coverage

---
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/pages/significantChangeTaskList.ts
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+import BasePage from './basePage';
+
+class SignificantChangeTaskList extends BasePage {
+    public path = 'significant-change/task-list';
+
+    public verifyHeaderAndSubNavigation(): this {
+        cy.checkPath(this.path);
+
+        cy.getByDataCy('select-heading').should('be.visible');
+        cy.get('.govuk-caption-xl').should('contain.text', 'URN:');
+        cy.contains('p', 'Route:').should('be.visible');
+        cy.contains('p', 'Project owner:').should('be.visible');
+
+        cy.get('strong[id^="project-status-"]')
+            .should('be.visible')
+            .invoke('attr', 'class')
+            .should('match', /govuk-tag--(green|yellow|orange|red|purple)/);
+
+        cy.contains('a.moj-sub-navigation__link', 'Project details')
+            .should('be.visible')
+            .and('have.attr', 'aria-current', 'page');
+
+        return this;
+    }
+}
+
+const significantChangeTaskList = new SignificantChangeTaskList();
+
+export default significantChangeTaskList;
