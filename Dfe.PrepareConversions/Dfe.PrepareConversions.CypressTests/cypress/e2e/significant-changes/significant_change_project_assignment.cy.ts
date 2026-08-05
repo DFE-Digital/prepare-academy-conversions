d0756b0400e48a4e570c0d801e23409562a138fc
Ali <Muhammad.ALI-SHAH@education.gov.uk>
2026-08-05 15:16:45 +0100

    cypress fixes - 2

--- Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/significant-changes/significant_change_project_assignment.cy.ts
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.CypressTests/cypress/e2e/significant-changes/significant_change_project_assignment.cy.ts
@@ -2,6 +2,11 @@
 import significantChangeProjectAssignment from '../../pages/significantChangeProjectAssignment';
 import { Logger } from '../../support/logger';
 
+// The app appends a query string (e.g. ?returnToFormAMatMenu=False) to these URLs.
+const TASK_LIST_URL = /\/significant-change\/task-list\/\d+(\?.*)?$/;
+
+const projectIdFrom = (url: string): string => url.split('?')[0].split('/').pop() as string;
+
 describe('Significant change project assignment', () => {
     beforeEach(() => {
         cy.login();
@@ -23,8 +28,7 @@ describe('Significant change project assignment', () => {
                     cy.getById('school-name-0').click();
 
                     cy.url().then((taskListUrl) => {
-                        const projectId = taskListUrl.split('/').pop();
-                        cy.visit(`/significant-change/project-assignment/${projectId}`);
+                        cy.visit(`/significant-change/project-assignment/${projectIdFrom(taskListUrl)}`);
 
                         significantChangeProjectAssignment.verifyPageIsVisible();
                         cy.getById('unassign-link').should('be.visible');
@@ -48,21 +52,21 @@ describe('Significant change project assignment', () => {
                     cy.getById('school-name-0').click();
 
                     cy.url().then((taskListUrl) => {
-                        const projectId = taskListUrl.split('/').pop() as string;
+                        const projectId = projectIdFrom(taskListUrl);
 
                         cy.visit(`/significant-change/project-assignment/${projectId}`);
 
                         significantChangeProjectAssignment
                             .assignFirstAvailableDeliveryOfficer()
                             .then((deliveryOfficer) => {
-                                cy.url().should('match', /\/significant-change\/task-list\/\d+$/);
+                                cy.url().should('match', TASK_LIST_URL);
                                 cy.getById('notification-message').should('contain.text', 'Project is assigned');
                                 cy.getByDataId('assigned-user').should('contain.text', deliveryOfficer);
 
                                 cy.visit(`/significant-change/project-assignment/${projectId}`);
                                 significantChangeProjectAssignment.unassignDeliveryOfficer();
 
-                                cy.url().should('match', /\/significant-change\/task-list\/\d+$/);
+                                cy.url().should('match', TASK_LIST_URL);
                                 cy.getById('notification-message').should('contain.text', 'Project is unassigned');
                                 cy.getByDataId('assigned-user').should('contain.text', 'Empty');
                             });
