6864f3c31e0f98bd32afc01e3dd895ef56c15f55
Stephen Bradbury <bradbury.ste@googlemail.com>
2026-08-19 15:38:40 +0100

    fixed whitespace

--- Dfe.PrepareConversions/Dfe.PrepareConversions.Data/Models/SignificantChange/SignificantChangeAdmissionVariationConsultationResponse.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.Data/Models/SignificantChange/SignificantChangeAdmissionVariationConsultationResponse.cs
@@ -1,9 +1,9 @@
 ﻿
 namespace Dfe.PrepareConversions.Data.Models.SignificantChange
 {
-	public class SignificantChangeAdmissionVariationConsultationResponse
-	{
-		public bool? ConsultationIncludeAdmissionVariation { get; set; }
+   public class SignificantChangeAdmissionVariationConsultationResponse
+   {
+      public bool? ConsultationIncludeAdmissionVariation { get; set; }
       public string ConsultationNoAdmissionVariationReason { get; set; } = string.Empty;
       public SignificantChangeTaskStatus Status { get; set; } = SignificantChangeTaskStatus.NotStarted;
    }
