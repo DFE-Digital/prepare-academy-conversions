b8e720be8f75d78f176cdbd14240ffa84a3ecb9a
Adam Forrest <Adam.Forrest@Education.gov.uk>
2026-09-22 11:24:09 +0100

    [296588] - fixed a test

--- Dfe.PrepareConversions/Dfe.PrepareConversions.Tests/Pages/SignificantChange/TaskList/SignificantChangeTaskListModelTests.cs
+++ Dfe.PrepareConversions/Dfe.PrepareConversions.Tests/Pages/SignificantChange/TaskList/SignificantChangeTaskListModelTests.cs
@@ -30,7 +30,7 @@ public class SignificantChangeTaskListModelTests
       Assert.IsType<PageResult>(result);
       Assert.NotNull(model.TaskList);
       Assert.Equal(3, model.TaskList.Sections.Count);
-      Assert.Equal(4, model.TaskList.Sections[0].Tasks.Count);
+      Assert.Equal(5, model.TaskList.Sections[0].Tasks.Count);
       repository.Verify(r => r.GetProjectById(id), Times.Once);
    }
 
